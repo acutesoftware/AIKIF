77ba08ad8b5251e2a6c61aabba957d6172a46f00
Duncan Murray <djmurray@acutesoftware.com.au>
2014-03-23 19:59:55 +1030

    simple command menu for quick navigation

--- AI/view.py
+++ AI/view.py
@@ -17,18 +17,44 @@ if len(sys.argv) == 2:
 
 def view():
 	# main function 
-
 	aikif.LogProcess('Starting view',  'view.py') # sys.modules[self.__module__].__file__)
+	AIKIF_FileList = aikif.build_AIKIF_structure()
+		
+	command = ''
+	ShowMenu()
+	while True:
+		command = input('Enter command: (m=menu, q=quit, [string]=view entries for string ')
+		if command.upper() == 'Q': 
+			quit()
+		if command.upper() == 'M': 
+			ShowMenu()
+		if command.upper() == 'S':
+			ShowStatistics()
+		if command == '1':
+			aikif.printFileList(AIKIF_FileList)
+		if command == '2':
+			aikif.showColumnStructures(AIKIF_FileList)
+		if command == '3':
+			aikif.debugPrintFileStructures(AIKIF_FileList)
+		if command == '4':
+			filemap.ShowListOfPhysicalFiles()
+
+def ShowMenu():
 	print('---------------------')
 	print('AIKIF Data Structures')
 	print('---------------------')	
-
-	AIKIF_FileList = aikif.build_AIKIF_structure()
-	#aikif.printFileList(AIKIF_FileList)
-	aikif.showColumnStructures(AIKIF_FileList)
-	#aikif.debugPrintFileStructures(AIKIF_FileList)
-	filemap.ShowListOfPhysicalFiles()
-
+	print('1 - aikif.printFileList(AIKIF_FileList)')
+	print('2 - aikif.showColumnStructures(AIKIF_FileList)')
+	print('3 - aikif.debugPrintFileStructures(AIKIF_FileList)')
+	print('4 - filemap.ShowListOfPhysicalFiles()')
+	print('m - show this menu')
+	print('s - statistics')
+	print('q - quit')
+	
+	
+def ShowStatistics():
+	print(' ----=== Statistics for AIKIF ===----')
+	
 	
 if __name__ == '__main__':
     view()	
