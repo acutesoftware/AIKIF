88a02b1ac9896f1dea3f7247e28cb5255af5f39e
Duncan Murray <djmurray@acutesoftware.com.au>
2014-03-26 14:39:32 +1030

    added testing for local file sizes

--- AI/review_ontology.py
+++ AI/review_ontology.py
@@ -12,82 +12,104 @@
 ontologyList = [  # links to various upper ontologies - http://en.wikipedia.org/wiki/Upper_ontology
 	{'name': 'WordNet', 
 	 'url': 'http://wordnet.princeton.edu/wordnet/download/', 
-	 'rating': 'qualifies as an upper ontology by including the most general concepts as well as more specialized concepts, related to each other not only by the subsumption relations, but by other semantic relations as well, such as part-of and cause. However, unlike Cyc, it has not been formally axiomatized so as to make the logical relations between the concepts precise. It has been widely used in Natural language processing research', 
+	 'data': 'http://wordnetcode.princeton.edu/wn3.1.dict.tar.gz', 	 
+	 'localFile': 'S:\\DATA\\opendata\\ontology\\WordNet\\dict\\data.noun', 
+	 'rating': 'Best word list, not in OWL format, though qualifies as an upper ontology by including the most general concepts as well as more specialized concepts, related to each other not only by the subsumption relations, but by other semantic relations as well, such as part-of and cause. However, unlike Cyc, it has not been formally axiomatized so as to make the logical relations between the concepts precise. It has been widely used in Natural language processing research', 
 	 'tested': 'Untested'},
 	{'name': 'SUMO - Suggested Upper Merged Ontology', 
 	 'url': 'http://www.ontologyportal.org/', 
-	 'rating': 'includes an upper ontology, created by the IEEE working group P1600.1', 
+	 'data': 'http://sigmakee.cvs.sourceforge.net/viewvc/sigmakee/KBs/?view=tar', 
+	 'localFile': 'S:\\DATA\\opendata\\ontology\\SUMO\\KBs\\Mid-level-ontology.kif', 
+	 'rating': 'Created by the IEEE working group P1600.1 - has multiple files by subject area which includes an upper ontology (which file?)', 
 	 'tested': 'Untested'}, 
 	{'name': 'DOLCE - Descriptive Ontology for Linguistic and Cognitive Engineering ', 
-	 'url': 'http://www.loa-cnr.it/ontologies/DOLCE-Lite.owl', 
-	 'rating': 'has a clear cognitive bias, in that it aims at capturing the ontological categories underlying natural language and human common sense', 
+	 'url': 'http://www.loa.istc.cnr.it/', 
+	 'data': 'http://www.loa-cnr.it/ontologies/DOLCE-Lite.owl', 
+	 'localFile': 'S:\\DATA\\opendata\\ontology\\DOLCE\\DOLCE-Lite.owl', 
+	 'rating': 'Not an active project on website, but has a clear cognitive bias, in that it aims at capturing the ontological categories underlying natural language and human common sense', 
 	 'tested': 'Untested'}, 
 	{'name': 'DBPedia', 
 	 'url': 'http://wiki.dbpedia.org/Datasets', 
+	 'data': 'http://wiki.dbpedia.org/Downloads39', 
+	 'localFile': 'S:\\DATA\\opendata\\ontology\\wikipedia_categories\\dbpedia-ontology.owl.bz2.owl.bz2.owl', 
 	 'rating': 'The most comprehensive set of data based on Wikipedia (470M facts)', 
 	 'tested': 'Untested'}, 
 	{'name': 'BFO - Basic Formal Ontology', 
 	 'url': 'http://www.ifomis.org/bfo', 
+	 'data': 'http://www.ifomis.org/bfo/1.1', 
+	 'localFile': 'S:\\DATA\\opendata\\ontology\\BFO\\bfo-1.1.owl', 
 	 'rating': 'Incorporates both three-dimensionalist and four-dimensionalist perspectives on reality within a single framework. Has over 100 other ontologies build based on this', 
 	 'tested': 'Untested'},
 	{'name': 'UMBEL', 
 	 'url': 'http://structureddynamics.com/resources.html#Ontologies', 
+	 'data': 'https://github.com/structureddynamics/UMBEL/blob/master/Ontology/umbel.n3', 
+	 'localFile': 'S:\\DATA\\opendata\\ontology\\UMBEL\\umbel.n3', 
 	 'rating': 'Maps to a simplified subset of the OpenCyc ontology (28,000 entries)', 
 	 'tested': 'Untested'},
-	{'name': 'DnS - Descriptions and Situations', 
+	{'name': 'DnS - Descriptions and Situations (implementation of DOLCE+DnS-Ultralite abbreviated to DUL) ', 
 	 'url': 'http://stlab.istc.cnr.it/stlab/The_Semantic_Technology_Laboratory_%28STLab%29', 
+	 'data': 'http://www.ontologydesignpatterns.org/ont/dul/DUL.owl', 
+	 'localFile': 'S:\\DATA\\opendata\\ontology\\DnS\\DUL.owl', 
 	 'rating': 'constructivist ontology that pushes DOLCEs descriptive stance even further allowing for context-sensitive redescriptions of the types and relations postulated by other given ontologies', 
 	 'tested': 'Untested'},
 	{'name': 'GFO - General Formal Ontology', 
-	 'url': 'http://www.onto-med.de/', 
-	 'rating': 'Unrated', 
+	 'url': 'http://www.onto-med.de/ontologies/index.jsp', 
+	 'data': 'http://www.onto-med.de/ontologies/gfo.owl', 
+	 'localFile': 'S:\\DATA\\opendata\\ontology\\GFO\\gfo-ato.owl', 
+	 'rating': 'have developed a top level ontology and a biological core ontology. OWL file is copyright, but redistribution allowed', 
 	 'tested': 'Untested'},
 	{'name': 'UFO - Unified Foundation Ontology', 
-	 'url': 'http://', 
-	 'rating': 'new, pretty good. tested for complex domains, combines DOLCE and GFO', 
+	 'url': 'https://oxygen.informatik.tu-cottbus.de/drupal7/ufo/', 
+	 'data': '', 
+	 'localFile': '', 
+	 'rating': 'new, pretty good. tested for complex domains, combines DOLCE and GFO. Count not find single download OWL file', 
 	 'tested': 'Untested'},
 	{'name': 'CIDOC Conceptual Reference Model', 
 	 'url': 'http://en.wikipedia.org/wiki/CIDOC_Conceptual_Reference_Model', 
-	 'rating': 'domain ontology, but includes its own version of an upper ontology in its core classes', 
+	 'data': 'http://www.cidoc-crm.org/rdfs/cidoc_crm_v5.0.4_official_release.rdfs', 
+	 'localFile': 'S:\\DATA\\opendata\\ontology\\CIDOC\\cidoc_crm_v5.0.4_official_release.rdfs', 
+	 'rating': 'provides an extensible ontology for concepts and information in cultural heritage and museum documentation. Includes its own version of an upper ontology in its core classes', 
 	 'tested': 'Untested'}, 
 	{'name': 'COSMO - COmmon Semantic MOdel', 
-	 'url': 'http://404', 
+	 'url': 'http://ontolog.cim3.net/cgi-bin/wiki.pl?COSMO', 
+	 'data': 'http://www.micra.com/COSMO/COSMO.owl', 
+	 'localFile': 'S:\\DATA\\opendata\\ontology\\COSMO\\COSMO.owl', 
 	 'rating': 'The current (May 2009) OWL version of COSMO has over 6400 types (OWL classes), over 700 relations, and over 1400 restrictions', 
-	 'tested': 'Untested'}, 
-	{'name': 'OCHRE', 
-	 'url': 'http://en.wikipedia.org/wiki/Object-centered_high-level_reference_ontology', 
-	 'rating': 'has a focus on conceptual simplicity, so that the number of basic (primitive) concepts and relations is as small as possible in order to simplify the theory', 
 	 'tested': 'Untested'}, 
 	{'name': 'YAMATO - Yet Another More Advanced Top Ontology', 
 	 'url': 'http://www.ei.sanken.osaka-u.ac.jp/hozo/onto_library/upperOnto.htm', 
+	 'data': 'http://www.ei.sanken.osaka-u.ac.jp/hozo/onto_library/download.php?filename=YAMATO20120714owl.zip', 
+	 'localFile': 'S:\\DATA\\opendata\\ontology\\YAMATO\\YAMATO20120714.owl', 
 	 'rating': 'complex but very advanced', 
 	 'tested': 'Untested'}, 
-	{'name': 'KSL Omtonlingua', 
-	 'url' : 'http://www.ksl.stanford.edu/software/ontolingua/', 
-	 'rating': 'Unrated', 
-	 'tested': 'Untested'},
-	{'name': 'DAML Ontology Library', 
-	 'url': 'http://www.daml.org/ontologies/', 
-	 'rating': 'Unrated', 
-	 'tested': 'Untested'},
 	{'name': 'OpenCyc', 
-	 'url': 'http://en.wikipedia.org/wiki/Cyc#ResearchCyc', 
+	 'url': 'http://en.wikipedia.org/wiki/Cyc#OpenCyc', 
+	 'data': 'http://sourceforge.net/projects/texai/files/open-cyc-rdf/1.1/open-cyc.rdf.ZIP/download', 
+	 'localFile': 'S:\\DATA\\opendata\\ontology\\OpenCyc\\open-cyc.rdf', 
 	 'rating': 'Proprietry but fairly precise', 
 	 'tested': 'Untested'},
 	{'name': 'PROTON', 
-	 'url': 'http://', 
+	 'url': 'http://www.ontotext.com/proton-ontology', 
+	 'data': 'http://www.ontotext.com/sites/default/files/proton/protontop.ttl', 
+	 'localFile': 'S:\\DATA\\opendata\\ontology\\PROTON\\protontop.ttl', 
 	 'rating': 'basic subsumption hierarchy which provides coverage of most of the upper-level concepts necessary for semantic annotation, indexing, and retrieval', 
 	 'tested': 'Untested'}, 
 	{'name': 'IDEAS', 
-	 'url': 'http://', 
-	 'rating': 'not intended for reasoning and inference purposes; its purpose is to be a precise model of business', 
+	 'url': 'http://www.ideasgroup.org/7Documents/', 
+	 'data': 'http://www.ideasgroup.org/file_download/5/IDEAS+Foundation+v1_0+Released+2009-04-24.xmi.zip', 
+	 'localFile': 'S:\\DATA\\opendata\\ontology\\IDEAS\\IDEAS Foundation v1_0 Released 2009-04-24.xmi', 
+	 'rating': 'The most common usage of IDEAS will be in direct exchange of information between architectural modelling tools are repositories', 
 	 'tested': 'Untested'},
 	{'name': 'MarineTLO', 
 	 'url': 'http://www.ics.forth.gr/isl/MarineTLO/', 
-	 'rating': 'Low - aimed for Marine domain', 
+	 'data': 'http://www.ics.forth.gr/isl/MarineTLO/v3/core_v3.owl', 
+	 'localFile': 'S:\\DATA\\opendata\\ontology\\MarineTLO\\core_v3.owl', 
+	 'rating': 'MarineTLO is a top-level ontology for the marine domain (also applicable to the terrestrial domain)', 
 	 'tested': 'Untested'}, 
 	{'name': 'DIY - eg build your own Ontology', 
 	 'url': 'http://localhost', 
+	 'data': '', 
+	 'localFile': '', 
 	 'rating': 'Not ideal - better to use existing sets, but best method to get a concise set of tuples', 
 	 'tested': 'Untested'}
 ]
@@ -116,6 +138,11 @@ documentList = [  # links to various documents, tools and general ontology relat
 		'author': 'Robert Hoehndorf', 'year': '2010', 
 		'localSavedFile': 'ontogenesis.html', 
 		'comment': 'basic overview of ontologies with descriptions of time/space and objects/processes'},
+	{'title': 'DAML Ontology Library', 
+	 'url': 'http://www.daml.org/ontologies/', 
+	 'author': 'http://www.daml.org/ontologies/uri.html', 'year':'', 
+	 'localSavedFile': 'S:\\DATA\\opendata\\ontology\\DAML\\uri.html', 
+	 'comment': 'Not a single ontology, but has links to other ontologies of various grains'},
 	{'title': 'Toward the Use of an Upper Ontology for U.S. Government and U.S. Military Domains: An Evaluation', 
 		'url': 'http://oai.dtic.mil/oai/oai?verb=getRecord&metadataPrefix=html&identifier=ADA459575',
 		'author': 'MITRE Corp report for US Government', 'year': '2004', 
@@ -136,6 +163,18 @@ documentList = [  # links to various documents, tools and general ontology relat
 		'url': 'http://protege.stanford.edu/publications/ontology_development/ontology101-noy-mcguinness.html',
 		'localSavedFile': 'ontology101-noy-mcguinness.html', 
 		'comment': 'Good introduction and examples of building an ontology - key points: reuse parts if possible, but build yourself to keep it short and valid'},
+	{'title': 'KSL Ontonlingua', 
+	 'url' : 'http://www.ksl.stanford.edu/software/ontolingua/', 
+	 'author': 'Standford', 
+	 'year': '2012', 
+	 'localSavedFile': '', 
+	 'comment': 'Ontolingua provides a distributed collaborative environment to browse, create, edit, modify, and use ontologies. The server supports over 150 active users, some of whom have provided us with descriptions of their projects. '},
+	{'title': 'OCHRE', 
+	 'url': 'http://en.wikipedia.org/wiki/Object-centered_high-level_reference_ontology', 
+		'author'		: '', 
+		'year'			: '', 
+		'localSavedFile': 'S:\\DATA\\opendata\\ontology\\OCHRE\\ki2003epaper.pdf', 
+	    'comment': 'Descriptive document, not an actual ontology - has a focus on conceptual simplicity, so that the number of basic (primitive) concepts and relations is as small as possible in order to simplify the theory'}, 
 	{'title'			: 'Onto-Med Report Nr. 8', 
 		'url'			: 'http://www.onto-med.de/publications/2010/gfo-basic-principles.pdf',
 		'author'		: 'Onto-Med in  Leipzig', 
@@ -151,16 +190,12 @@ commentsList = [
 
 import os
 import sys
-sys.path.append('..//..//aspytk')
-import lib_file as fle
-import lib_net as net
-import lib_data as dat
 
 
 def main():
 	ShowStatistics()
 	SaveHTML_Review('review_ontology.html')
-	fle.LaunchFile('review_ontology.html')
+	os.system('start review_ontology.html')
 
 	#ShowConclusion()
 
@@ -197,30 +232,102 @@ def ShowData():
 
 def SaveHTML_Review(htmlFile):
 	#print(' saving results to ' + htmlFile)
-	fle.deleteFile(htmlFile)
-	fle.AppendToFile(htmlFile, net.BuildHTMLHeader('Ontology Review', '\r\n', '0'))
-	fle.AppendToFile(htmlFile, '</TABLE>')
-	fle.AppendToFile(htmlFile, 'Updated 25/3/2014 - list of upper ontologies with comments/ratings for possible use in AI applications.<BR><BR>\r\n')
-	fle.AppendToFile(htmlFile, '<H2>Ontology Datasets</h2>\r\n')
+	deleteFile(htmlFile)
+	AppendToFile(htmlFile, BuildHTMLHeader('Ontology Review', '\r\n', '0'))
+	AppendToFile(htmlFile, '</TABLE>')
+	AppendToFile(htmlFile, 'Updated 25/3/2014 - list of upper ontologies with comments/ratings for possible use in AI applications.<BR><BR>\r\n')
+	AppendToFile(htmlFile, '<H2>Ontology Datasets</h2>\r\n')
 	for i in ontologyList:
-		fle.AppendToFile(htmlFile, '<B>' + i['name']  + '</B><BR><a href=' + i['url'] + '>' + i['url'] + '</a><BR>\r\n')
-		fle.AppendToFile(htmlFile, i['rating'] + '<BR><BR>\r\n')
-	fle.AppendToFile(htmlFile, '<BR><BR>\r\n')
+		AppendToFile(htmlFile, '<B>' + i['name']  + '</B><BR>')
+		AppendToFile(htmlFile, 'page = <a href=' + i['url'] + '>' + i['url'] + '</a><BR>')
+		AppendToFile(htmlFile, 'data = <a href=' + i['data'] + '>' + i['data'] + '</a><BR>\r\n')
+		AppendToFile(htmlFile, i['rating'] + '<BR>\r\n')
+		AppendToFile(htmlFile,  TestLocalFile(i['localFile']) + '<BR><BR>\r\n')
+	AppendToFile(htmlFile, '<BR><BR>\r\n')
 	
 	# show document list of RHS
-	fle.AppendToFile(htmlFile, '<H2>Useful Links for Ontological development</H2>\r\n')
+	AppendToFile(htmlFile, '<H2>Useful Links for Ontological development</H2>\r\n')
 	for i in documentList:
-		fle.AppendToFile(htmlFile, '<B>' + i['title'] + '</B><BR>' + '<a href=' + i['url'] + '>' + i['url'] + '</a><BR>\r\n')
-		fle.AppendToFile(htmlFile, i['comment'] + '<BR><BR>\r\n')
-	fle.AppendToFile(htmlFile, '<BR><BR></BODY></HTML>')
+		AppendToFile(htmlFile, '<B>' + i['title'] + '</B><BR>' + '<a href=' + i['url'] + '>' + i['url'] + '</a><BR>\r\n')
+		AppendToFile(htmlFile, i['comment'] + '<BR><BR>\r\n')
+	AppendToFile(htmlFile, '<BR><BR></BODY></HTML>')
 
+def BuildHTMLHeader(title, linefeed='\n', border='1'):
+    res = "<HTML><HEAD><title>" + linefeed
+    res = res + title + "</title>" + linefeed
+    res = res + CreateCssString("Arial", "10pt", linefeed ) + linefeed
+    res = res + "</HEAD><BODY><H1>"
+    res = res + title + "</H1><TABLE border=" + border + ">"
+    return res
+	
+def CreateCssString(fontFamily, baseFontSize, linefeed='\n'):
+    css = "<STYLE>" + linefeed
+    css = css + "BODY {      font-size:" + baseFontSize + "; FONT-FAMILY:" + fontFamily + "; }" + linefeed
+    css = css + "A:link {    font-size:" + baseFontSize + "; COLOR: blue;TEXT-DECORATION:none}" + linefeed
+    css = css + "A:visited { color: #003399; font-size:" + baseFontSize + ";TEXT-DECORATION:none }" + linefeed
+    css = css + "A:hover {   color:#FF3300;TEXT-DECORATION:underline}" + linefeed
+    css = css + "TD {        font-size:" + baseFontSize + "; valign=top; FONT-FAMILY:Arial; padding: 1px 2px 2px 1px;  }" + linefeed
+    css = css + "H1 {        font-size:200%; padding: 1px 0px 0px 0px; margin:0px; }" + linefeed
+    css = css + "H2 {        font-size:160%; FONT-WEIGHT:NORMAL; margin:0px 0px 0px 0px; padding:0px; }" + linefeed
+    css = css + "H3 {        font-size:100%; FONT-WEIGHT:BOLD; padding:1px; letter-spacing:0.1em; }" + linefeed
+    css = css + "H4 {        font-size:140%; FONT-WEIGHT:NORMAL; margin:0px 0px 0px 0px; padding:1px; }" + linefeed
+    css = css + "</STYLE>" + linefeed
+    return css
+  
+	
+def deleteFile(f):
+    if f == "":
+        pass
+    else:
+        try:
+            os.remove(f)
+        except:
+            print("Cant delete ",f)
+
+def AppendToFile(fname, txt):
+    with open(fname, "a") as myfile:
+        myfile.write(txt)
+	
+def GetFileSize(localFile):
+	return os.path.getsize(localFile)
+	
+def GetTotalNumFiles(localFile):
+	fldr = os.path.dirname(localFile)
+	fileList = os.listdir(fldr)
+	numfiles = len([name for name in fileList if os.path.isfile(fldr + '\\' + name)])
+	return numfiles
+	
+def GetTotalFileSizesForFolder(localFile):
+	fldr = os.path.dirname(localFile)
+	fileList = os.listdir(fldr)
+	num = sum(os.path.getsize(fldr + '\\' + f) for f in fileList if os.path.isfile(fldr + '\\' + f))
+	return num
+
+def DoesFileExist(localFile):	
+	success = False
+	try:
+		if os.path.isfile(localFile):
+			success = True
+	except:
+		pass
+	return success
+	
+def TestLocalFile(localFile):
+	result = 'Not downloaded'
+	if DoesFileExist(localFile):
+			print('Collecting stats for ' + localFile)
+			result = '<I>Sample file saved to ' + localFile + ' (' + format(GetFileSize(localFile), ',d') + ' bytes)<BR>'
+			result = result + '' + format(GetTotalNumFiles(localFile), ',d')
+			result = result + ' files in folder, totalling ' + format(GetTotalFileSizesForFolder(localFile), ',d') + ' bytes</I>'
+	return result
+	
 def SaveHTML_Review_as_table(htmlFile):
 	#print(' saving results to ' + htmlFile)
-	fle.deleteFile(htmlFile)
-	fle.AppendToFile(htmlFile, net.BuildHTMLHeader('Ontology Review', '\r\n', '2'))
-	fle.AppendToFile(htmlFile, '<TR>\r\n')
-	fle.AppendToFile(htmlFile, '<TD valign=top><TABLE border=1 valign=top width=96%>\r\n')
-	fle.AppendToFile(htmlFile, '<TH>Ontology</TH><TH>Test Comments</th><TH>Rating for AI</TH></TR>\r\n')
+	deleteFile(htmlFile)
+	AppendToFile(htmlFile, BuildHTMLHeader('Ontology Review', '\r\n', '2'))
+	AppendToFile(htmlFile, '<TR>\r\n')
+	AppendToFile(htmlFile, '<TD valign=top><TABLE border=1 valign=top width=96%>\r\n')
+	AppendToFile(htmlFile, '<TH>Ontology</TH><TH>Test Comments</th><TH>Rating for AI</TH></TR>\r\n')
 	for i in ontologyList:
 		txtRow = '<TR>'
 		#print(i['name'], i['url'])
@@ -229,22 +336,22 @@ def SaveHTML_Review_as_table(htmlFile):
 		txtRow = txtRow + '<TD>' + i['rating'] + '</TD>\r\n'
 		txtRow = txtRow + '</TR>\r\n'	
 		#fle.AppendToFile(htmlFile, net.FormatListAsHTMLTableRow(dat.dict2list(i)))
-		fle.AppendToFile(htmlFile, txtRow)
-	fle.AppendToFile(htmlFile, '</TABLE></TD valign=top><TD>\r\n')
+		AppendToFile(htmlFile, txtRow)
+	AppendToFile(htmlFile, '</TABLE></TD valign=top><TD>\r\n')
 	
 	# show document list of RHS
-	fle.AppendToFile(htmlFile, '<TABLE border=1 valign=top width=96%>\r\n')
-	fle.AppendToFile(htmlFile, '<TH>Document Title</TH><TH>Comments</th></TR>\r\n')
+	AppendToFile(htmlFile, '<TABLE border=1 valign=top width=96%>\r\n')
+	AppendToFile(htmlFile, '<TH>Document Title</TH><TH>Comments</th></TR>\r\n')
 	for i in documentList:
 		txtRow = '<TR>'
 		#print(i['name'], i['url'])
 		txtRow = txtRow + '<TD><a href=' + i['url'] + '>' + i['title'] + '</a></TD>\r\n'
 		txtRow = txtRow + '<TD>' + i['comment'] + '</TD>\r\n'
 		txtRow = txtRow + '</TR>\r\n'	
-		#fle.AppendToFile(htmlFile, net.FormatListAsHTMLTableRow(dat.dict2list(i)))
-		fle.AppendToFile(htmlFile, txtRow)
-	fle.AppendToFile(htmlFile, '</TABLE></TD><TD>\r\n')
-	fle.AppendToFile(htmlFile, '</TD></TR></TABLE><BR><BR><BR><BR></BODY></HTML>')
+		#AppendToFile(htmlFile, net.FormatListAsHTMLTableRow(dat.dict2list(i)))
+		AppendToFile(htmlFile, txtRow)
+	AppendToFile(htmlFile, '</TABLE></TD><TD>\r\n')
+	AppendToFile(htmlFile, '</TD></TR></TABLE><BR><BR><BR><BR></BODY></HTML>')
 
 	
 		
