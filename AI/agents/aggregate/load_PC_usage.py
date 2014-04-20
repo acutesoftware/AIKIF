0e75428c8f9707fbc62bdab49c40946903d4c2db
Duncan Murray <djmurray@acutesoftware.com.au>
2014-04-20 10:00:51 +0930

    empty stub for aggregate program

---
+++ AI/agents/aggregate/load_PC_usage.py
@@ -0,0 +1,8 @@
+# load_PC_usage.py	written by Duncan Murray  19/4/2014
+# reads a text file containing PC usage collected from daily agent
+# and exports to the AIKIF structures (in personal folder)
+
+ipFile = 'T:\\user\\docs\\AIKIF\\pc_usage.txt'
+opFile = 'T:\\user\\docs\\AIKIF\\diary.csv'
+
+print('aggregating ', ipFile, ' to ' , opFile)
