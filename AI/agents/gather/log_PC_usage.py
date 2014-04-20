587f2817c315fb7ec797a19c2ae89be03364e092
Duncan Murray <djmurray@acutesoftware.com.au>
2014-04-20 09:58:44 +0930

    collection functions for agents

---
+++ AI/agents/gather/log_PC_usage.py
@@ -0,0 +1,51 @@
+# log_usage.py  written by Duncan Murray 13/1/2014
+
+from win32gui import GetWindowText, GetForegroundWindow
+import sys
+import time
+sys.path.append('S://duncan//C//user//dev//src//python//aspytk')
+import lib_file as fle
+fname = 'T:\\user\\docs\\AIKIF\\pc_usage.txt'
+
+def main():
+	lstRaw = []
+	prevText = ''
+	startTime = fle.TodayAsString()
+	tot_seconds = 1
+	try:
+		while True:
+			txt = GetWindowText(GetForegroundWindow())
+			#print(txt)
+			#fle.AppendToFile(fname, fle.TodayAsString() + ' ' + txt + '\n')
+			if txt == prevText:
+				tot_seconds = tot_seconds + 1
+			else:
+				lstRaw.append(startTime + ',' + format(tot_seconds, "03d") + ',' + txt)
+				prevText = txt
+				tot_seconds = 1
+				startTime = fle.TodayAsString()
+			time.sleep(1)
+			if fle.TodayAsString()[-3:] == ':00':
+				lstRaw.append(startTime + ',' + format(tot_seconds, "03d") + ',' + txt)
+				print('Recording data')
+				tot_seconds = 1
+				startTime = fle.TodayAsString()
+				record(lstRaw)
+				lstRaw = []
+
+	except KeyboardInterrupt:	
+		print("logging halted")
+		lstRaw.append(startTime + ',' + format(tot_seconds, "03d") + ',' + txt)   # save the latest record
+		record(lstRaw)
+
+	
+def record(lst):
+	with open(fname, "a") as f:
+		for txt in lst:
+			f.write(txt + '\n')
+	tot_seconds = 1
+		
+if __name__ == '__main__':
+	main()
+	
+	
---
+++ AI/agents/gather/log_browser_history.py
@@ -0,0 +1,69 @@
+# d:\py>GetBrowserHistoryChrome.py		written by Duncan Murray 13/1/2014 
+# references = http://computer-forensics.sans.org/blog/2010/01/21/google-chrome-forensics/
+#			 = http://www.forensicswiki.org/wiki/Google_Chrome
+#			 = http://sumtxt.wordpress.com/2011/07/02/chrome-browser-history-in-r/
+
+#OUTPUT:
+# Reading Chrome history from C:\Users\murraydj\AppData\Local\Google\Chrome\User Data\Default
+# Exported 44868 records to chrome_history.csv
+
+
+import os
+import datetime
+import sqlite3
+import codecs, re
+import time
+from os.path import expanduser
+
+#from datetime import datetime
+
+pattern = "(((http)|(https))(://)(www.)|().*?)\.[a-z]*/"
+SQL_STATEMENT = 'SELECT urls.url, urls.title, urls.visit_count, urls.typed_count, urls.last_visit_time, visits.visit_time, urls.hidden, visits.from_visit, urls.id, visits.transition  FROM urls, visits WHERE urls.id = visits.url;'
+
+opFile = 'T:\\user\\docs\\AIKIF\\chrome_history.csv'
+storage = codecs.open(opFile, 'w', 'utf-8')
+
+def DateConv(webkit_timestamp):
+	return date_from_webkit(webkit_timestamp)
+ 
+def date_from_webkit_ORIG_RETURNS_GMT_TIME(webkit_timestamp):
+    epoch_start = datetime.datetime(1601,1,1)
+    delta = datetime.timedelta(microseconds=int(webkit_timestamp))
+    return epoch_start + delta
+	
+def date_from_webkit(webkit_timestamp):
+	UTC_OFFSET_TIMEDELTA = datetime.datetime.utcnow() - datetime.datetime.now()
+	epoch_start = datetime.datetime(1601,1,1)
+	delta = datetime.timedelta(microseconds=int(webkit_timestamp))
+	return epoch_start + delta - UTC_OFFSET_TIMEDELTA
+
+def datetime_from_utc_to_local(utc_datetime):
+    now_timestamp = time.time()
+    offset = datetime.fromtimestamp(now_timestamp) - datetime.utcfromtimestamp(now_timestamp)
+    return utc_datetime + offset
+
+home = expanduser("~")
+#print(home)	# C:\users\murraydj
+#basePath = "C://Users//murraydj//AppData//Local//Google//Chrome//User Data//Default//" 
+basePath = home + r"\AppData\Local\Google\Chrome\User Data\Default" 
+print ("Reading Chrome history from " + basePath)
+paths = [basePath + "\Archived History", basePath + "\History"] 
+
+
+def GetBrowserHistoryChrome():
+	numRecs = 0
+	storage.write('"url","visit_count","typed_count","last_visit_time","visit_time","hidden","from_visit","id","transition","title"\n')
+	for path in paths:
+		c = sqlite3.connect(path)
+		for row in c.execute(SQL_STATEMENT):
+			#storage.write( row[0] + ", " + row[1] + ", " + str(row[2])+ ", ")
+			storage.write( '"' + row[0] + '","' + str(row[2]) + '","' + str(row[3]) + '","' + str(DateConv(row[4]))[0:21]  + '","' + str(DateConv(row[5]))[0:21] + '","' + str(row[6]) + '","' + str(row[7]) + '","' + str(row[8]) + '","' + str(row[9]) + '","' + row[1] + '"\n'  )
+			numRecs = numRecs + 1
+			#date_time = date_from_webkit(row[1])
+			#url = re.search(pattern, row[0])
+			#try: urlc = url.group(0)
+			#except: urlc = "ERROR"
+			#storage.write(str(date_time)[0:19] + "\t" + urlc + "\n")
+	print('Exported ' + str(numRecs) + ' records to ' + opFile)		
+# Main
+GetBrowserHistoryChrome()			
