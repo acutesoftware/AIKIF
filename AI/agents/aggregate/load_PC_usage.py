904042624e3ecb30e28809b1d05dba5c3a431ce8
Duncan Murray <djmurray@acutesoftware.com.au>
2014-04-27 22:08:37 +0930

    setup file checking for multiple source files

--- AI/agents/aggregate/load_PC_usage.py
+++ AI/agents/aggregate/load_PC_usage.py
@@ -2,7 +2,65 @@
 # reads a text file containing PC usage collected from daily agent
 # and exports to the AIKIF structures (in personal folder)
 
-ipFile = 'T:\\user\\AIKIF\\pc_usage.txt'
-opFile = 'T:\\user\\AIKIF\\diary.csv'
+import sys
+import os
+import time
 
-print('aggregating ', ipFile, ' to ' , opFile)
+def GetUser():
+	try:
+		import getpass
+		usr = getpass.getuser()
+	except:
+		usr = 'username'
+	return usr
+
+def GetPCName():
+	try:
+		import socket
+		pcname = socket.gethostname()
+	except:
+		pcname = 'computer'
+	return pcname
+
+try:
+	ipFolder = sys.argv[1] 
+	opFolder = sys.argv[2] 
+except:
+	print ('load_PC_usage.py - processes logged PC usage data to diary files')
+	print('Usage:')
+	print('  load_PC_usage.py [input_folder] [output_folder]')
+	exit(1)
+	
+	
+def main():	
+	#ipFile = ipFolder + '\\pc_usage_' + GetPCName() + '_' + GetUser() + '.txt'
+	#opFile = opFolder + '\\diary.csv'
+	print('aggregating ', ipFolder, ' to ' , opFolder)
+	rawFiles = GetRawFiles(ipFolder)
+	for f in rawFiles:
+		processRawFile(f, opFolder)
+	
+def GetRawFiles(ipFolder):
+	fl = []
+	#print(ipFolder)
+	for root, dirs, files in os.walk(ipFolder):
+		#print('root = ', root)
+		#print('dirs = ', dirs)
+		#print('files= ', files)
+		for basename in files:
+			filename = os.path.join(root, basename)
+			if basename[0:9] == 'pc_usage_':
+				fl.append(filename)
+		#print(filename)
+		#print(basename[0:9])
+	
+	return fl
+
+
+def processRawFile(f, opFolder):
+	print ('Processing: ', f)
+	
+	
+if __name__ == '__main__':
+    main()	
+	
