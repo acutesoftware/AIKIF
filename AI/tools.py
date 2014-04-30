6d003caed35bc9b2748421d783a89168f89fb931
Duncan Murray <djmurray@acutesoftware.com.au>
2014-05-01 00:27:51 +0930

    additional tests for toolbox

--- AI/tools.py
+++ AI/tools.py
@@ -32,18 +32,25 @@ def main():
 
 	
 	tl.save('tools.txt')
-	print(tl.lstTools[0])
-	print(tl.run(tl.lstTools[0], [1,2,3,4,5], 'N'))
-	run_multiple(tl, tl.lstTools[0], 5000000)
+	args = [1,2,3,4,5,6,7]
+	for ndx in range(0,2):
+		testResult = tl.run(tl.lstTools[ndx], args, 'N')
+		print('Ran test on ', os.path.basename(tl.lstTools[ndx]['file']) + '->' + tl.lstTools[ndx]['function'], ' Result = ', testResult)
+
+#	testResult = tl.run(tl.lstTools[1], args, 'N')
+#	print('Ran test on ', os.path.basename(tl.lstTools[1]['file']) + '->' + tl.lstTools[0]['function'], ' Result = ', testResult)
+	
+	
+	run_multiple(tl, tl.lstTools[0], 500)
 	
 
-def run_multiple(t1, tool, numIterations):
+def run_multiple(t1, tool, numIterations, silent='Y'):
 	results = []
 	start_time = time.time()		
 	for i in range(0,numIterations):
 		args = [randint(10,99) for j in range(1,randint(2,5))]
 		testname = tool['file'] + '.' + tool['function']
-		answer = t1.run(tool, args, 'Y')
+		answer = t1.run(tool, args, silent)
 		results.append({'tool':testname, 'args':args, 'result':answer})
 	print("Method1 = ", time.time() - start_time, "seconds")
 	print('Done processing ' + str(len(results)) + ' calculations')
