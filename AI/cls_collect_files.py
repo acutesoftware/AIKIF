5d487c9d017b35bd197264d16885daa475b05c02
Duncan Murray <djmurray@acutesoftware.com.au>
2014-05-20 23:48:22 +0930

    collects files

--- AI/cls_collect_files.py
+++ AI/cls_collect_files.py
@@ -2,6 +2,8 @@
 # Base class for collection classes for AIKIF
 
 import cls_collect
+import os
+import fnmatch
 
 class clsCollectFiles(cls_collect.clsCollect):
 	"""
@@ -10,14 +12,21 @@ class clsCollectFiles(cls_collect.clsCollect):
 	def __init__(self, name=None,  fldr=None):
 		self.name = name
 		self.fldr = fldr
+		self.tot_bytes = 0
+		self.tot_files = 0
+		self.tot_fldrs = 0
 		self.filelist = []
 
 	def collect_filelist(self):
 		print("collecting files from " , self.fldr)	
 		self.filelist = []
-		
+		for root, dirs, files in os.walk(self.fldr):
+			self.tot_fldrs += 1
+			for filename in fnmatch.filter(files, "*.*"):
+				#file_size = GetFileSize(os.path.join(root, filename)) 
+				self.filelist.append(os.path.join(root, filename))
 	
-	def get_filelist(self):
+	def get_filelist(self, fldr):
 		print("there are " , str(len(self.filelist)), " files found")
 		return self.filelist
 		
