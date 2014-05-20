ddea791c696fbece5e0c0be0c1462d873f169563
Duncan Murray <djmurray@acutesoftware.com.au>
2014-05-21 00:30:12 +0930

    added byte counting

--- AI/cls_collect_files.py
+++ AI/cls_collect_files.py
@@ -9,9 +9,9 @@ class clsCollectFiles(cls_collect.clsCollect):
 	"""
 	Collects file info for AIKIF
 	"""
-	def __init__(self, name=None,  fldr=None):
-		self.name = name
+	def __init__(self, fldr=None, pattern="*.*"):
 		self.fldr = fldr
+		self.pattern = pattern
 		self.tot_bytes = 0
 		self.tot_files = 0
 		self.tot_fldrs = 0
@@ -22,11 +22,23 @@ class clsCollectFiles(cls_collect.clsCollect):
 		self.filelist = []
 		for root, dirs, files in os.walk(self.fldr):
 			self.tot_fldrs += 1
-			for filename in fnmatch.filter(files, "*.*"):
-				#file_size = GetFileSize(os.path.join(root, filename)) 
+			for filename in fnmatch.filter(files, self.pattern):
+				file_size = os.path.getsize((os.path.join(root, filename))) 
+				self.tot_bytes += file_size
+				self.tot_files += 1
 				self.filelist.append(os.path.join(root, filename))
+				print(filename)
 	
-	def get_filelist(self, fldr):
+	def get_fldrs(self):
+		return self.tot_fldrs
+		
+	def get_files(self):
+		return self.tot_files
+		
+	def get_bytes(self):
+		return self.tot_bytes
+		
+	def get_filelist(self):
 		print("there are " , str(len(self.filelist)), " files found")
 		return self.filelist
 		
