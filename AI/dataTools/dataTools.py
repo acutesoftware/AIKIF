2537dea4d049735493db17ebf06640b1ffc24d4b
Duncan Murray <djmurray@acutesoftware.com.au>
2014-05-21 22:49:20 +0930

    added Generate SQL command

--- AI/dataTools/dataTools.py
+++ AI/dataTools/dataTools.py
@@ -36,7 +36,7 @@ import lib_data as dat
 import lib_file as fle
 import lib_net as net
 try:
-	import lib_excel as xl
+	import xlrd as xl        # NOTE - xlrd imports fine from python shell, but this line cant find it
 except:
 	print('you need to install xlrd')
 
@@ -46,17 +46,39 @@ def TEST():
 	print('Data tools test...')
 	url = 'http://www.abs.gov.au/AUSSTATS/subscriber.nsf/log?openagent&standard australian classification of countries, 2011, version 2.2.xls&1269.0&Data Cubes&EE21444EE8F2C99CCA257BF30012B66F&0&2011&01.10.2013&Latest'
 	fname = fldr + 'test.xlsx'
-	DownloadFile(url, fname)
-	#xl.csv_from_excel(fname , os.getcwd())
+	dl_fname = fldr + 'test-download.xlsx'
+	DownloadFile(url, dl_fname)
+	#csv_from_excel(fname , os.getcwd())
 	testFile = fldr + 'test.csv'
 	CreateRandomCSVFile(testFile)
-	GenerateSQL(testFile, headerRow=1)
+	GenerateSQL(testFile, 'MY_TABLE', testFile + '.SQL', headerRow=1)
 
 	CreateRandomIndentedCSVFile(fldr + 'indented.csv')
 #	ExtractTable(f, tmpFile, extractList[1]['colList'], 8, 1, 52, 9)
 	AutoFillCSV(fldr + 'indented.csv', fldr + 'indented-fixed.csv', ['grouping', 'code', 'desc'],  ['grouping'])   # autofill FIRST col based on prev values
 	RemoveBlankRecs(fldr + 'indented-fixed.csv', fldr + 'indented-fixed-and-no-blanks.csv', 2)
 
+
+def csv_from_excel(excel_file, pth):
+	opFname = ''
+	print('converting file ' + excel_file + '  to folder ' + pth)
+	workbook = xl.open_workbook(pth + '\\' + excel_file)
+	all_worksheets = workbook.sheet_names()
+	for worksheet_name in all_worksheets:
+		if worksheet_name != 'Pivot':
+			print('converting - ' + worksheet_name)
+			worksheet = workbook.sheet_by_name(worksheet_name)
+			opFname = pth + '\\' + os.path.splitext(excel_file)[0] + '_' + worksheet_name + '.csv'
+			print('SAVING - ' + opFname)
+			csv_file = open(opFname, 'wb')
+			#csv_file = open(pth + ''.join([worksheet_name,'.csv']), 'wb')
+			wr = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+
+			for rownum in xrange(worksheet.nrows):
+				wr.writerow([unicode(entry).encode("utf-8") for entry in worksheet.row_values(rownum)])
+			csv_file.close()
+		else:
+			print('ignoring tab - ' + worksheet_name)
 	
 
 def DownloadFile(url, fname):
@@ -173,42 +195,77 @@ def RemoveBlankRecs(fname, opFile, masterCol):
 
 	
 		
-def GenerateSQL(csvFile, headerRow=1):
-	# top level function
-	tbl = os.path.basename(csvFile).split('.')[0]
-	opFile = fldr + os.path.basename(csvFile).split('.')[0] + '.SQL'
+def GenerateSQL(csvFile, tblName, opFile, headerRow=1):
+	""" Generates the SQL command to create the table and
+	insert the data. Output of test.csv.sql is below:
+		DROP TABLE MY_TABLE  CASCADE CONSTRAINTS;
+		CREATE TABLE MY_TABLE ( 
+		ID VARCHAR2(2000), 
+		CODE VARCHAR2(2000), 
+		DESC VARCHAR2(2000), 
+		REC_EXTRACT_DATE DATE
+		);
+
+		INSERT INTO MY_TABLE (ID, CODE, DESC, REC_EXTRACT_DATE) VALUES (
+		'id', 'code', 'desc',  sysdate ); 
+		INSERT INTO MY_TABLE (ID, CODE, DESC, REC_EXTRACT_DATE) VALUES (
+		'1', 'S', 'AAA',  sysdate ); 
+		INSERT INTO MY_TABLE (ID, CODE, DESC, REC_EXTRACT_DATE) VALUES (
+		'2', 'B', 'BBB',  sysdate ); 
+		INSERT INTO MY_TABLE (ID, CODE, DESC, REC_EXTRACT_DATE) VALUES (
+		'3', 'X', 'Long description',  sysdate ); 
+		COMMIT;
+	
+	"""
+	
+	import re
+	if tblName == '':
+		tbl = str(os.path.basename(csvFile).split('.')[0])
+	else:
+		tbl = tblName
+	if opFile == '':	
+		opFile = str(os.path.basename(csvFile).split('.')[0] + '.SQL')
+		
+	print("Generating SQL for table " + tbl + " via " + opFile)
 	# read in the CSV file header
 	cols = []
-	txt = ''
-	SQL_file = open(opFile, 'w')
+	SQL_file = open(opFile, 'w')   # Note - with one version of Python this needs wb
 	with open(csvFile) as input_file:
 		rowNum = 0
 		for row in csv.reader(input_file, delimiter=','):
 			rowNum = rowNum + 1
 			if rowNum == headerRow:
 				for col in row:
-					# Format column name to remove unwanted chars
-					#txt = col.replace(' ', '_')
-					cols.append(col)
-		txt = GenerateSQL_CreateTable(tbl, cols)
-		print ( txt)
-		#dat.appendToFile(opFile, txt)
-		SQL_file.write(txt)
+					cols.append(clean_column_heading(col))
+				sql = GenerateSQL_CreateTable(tbl, cols)
+				#print(tbl, cols, sql)
+				SQL_file.write(sql)
 		
 	# now generate the inserts
 	with open(csvFile) as input_file:
 		for row in csv.reader(input_file, delimiter=','):
 			SQL_file.write(GenerateSQL_Insert(tbl, row, cols))
 		SQL_file.write('COMMIT;')
-	
+
+def clean_column_heading(txt):
+	""" terrible code, but will be fixed later - TODO """
+	c1 = txt.strip().replace(' ', '_').replace('.', '_').upper().strip()
+	c2 = ''
+	try:  # difference in earlier versions of python
+		c2 = re.sub('[^0-9a-zA-Z]+', '_', c1) 
+	except:
+		c2 =  "".join([ c if c.isalnum() else "" for c in c1 ])
+	#print('c1=', c2, 'c2=',c2)
+	return c2.strip('_')
 		
 def GenerateSQL_CreateTable(tbl, cols):
-	txt = 'DROP TABLE ' + tbl + ' CASCADE CONSTRAINTS;\n'
+	txt = 'DROP TABLE ' + tbl + '  CASCADE CONSTRAINTS;\n'
 	txt = txt + 'CREATE TABLE ' + tbl + ' ( \n'
 	for c in cols:
 		if c != '':
-			txt = txt + '    ' + c + ' VARCHAR2(200), \n'
+			txt = txt + '    ' + c + ' VARCHAR2(2000), \n'
 	txt = txt + '    REC_EXTRACT_DATE DATE\n);\n\n'
+	#print (txt)
 	return txt
 
 def GenerateSQL_Insert(tbl, row, cols):
@@ -218,12 +275,16 @@ def GenerateSQL_Insert(tbl, row, cols):
 			txt = txt + c + ', '
 	txt = txt + 'REC_EXTRACT_DATE) VALUES (\n'
 	for d in row:
+		if 'Rahman, M.M.' in row:
+			print (d)
 		if d != '':
-			txt = txt + '\'' + d.strip().replace('\'','\'\'') + '\'' + ', '
+			txt = txt + '\'' + d.strip().replace('\'','\'\'').replace('"', '') + '\'' + ', '
+		else:
+			txt = txt + 'NULL, '
 	txt = txt + ' sysdate ); \n'
 	return txt
-
 	
+
 if __name__ == '__main__':
     TEST()	
 	
