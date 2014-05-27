6e59e4d1df64962848e3bedf9d4036f786ab196f
Duncan Murray <djmurray@acutesoftware.com.au>
2014-05-28 00:11:47 +0930

    page data shows CSV file contents as well as list

--- AI/web_app/page_data.py
+++ AI/web_app/page_data.py
@@ -1,7 +1,7 @@
 # page_data.py	written by Duncan Murray 26/5/2014
 # handles the data display page for AIKIF web interface
 
-import sys
+import sys, os
 import web_utils as web
 
 sys.path.append('..\\..\\AI')
@@ -10,17 +10,28 @@ import fileMapping as filemap
 
 #from .AI import AIKIF_utils as aikif
 #from ...AI import fileMapping as filemap
-  
+
+dataFolder = '..\\data\\core\\'  
+dataFolder = 'T:\user\dev\src\python\AI\data\core'
 
 def get_page(dataFile=''):
 	txt = ''
 	if dataFile:
-		txt = "SHOWING FILE - " + dataFile
+		fullName = os.path.join(dataFolder , dataFile)
+		print("\nREADING " + fullName + '\n')
+		txt += showDataFile(fullName)
 	else:
 		txt += get_aikif_structure()
-		txt += get_ontology()
+		#txt += get_ontology()   # no - this is the proposed file list
+		txt += get_list_data_files()
 	return txt
 	
+def showDataFile(fname):
+	""" shows a data file in CSV format - all files live in CORE folder """
+	txt = '<H2>' + fname + '</H2>'
+	txt += web.read_csv_to_html_table(fname)
+	return txt
+		
 	
 def get_aikif_structure():
 	txt = '<H3>AIKIF Data Structure</H3>\n<TABLE width=100% border=0 align=centre>\n'
@@ -38,6 +49,14 @@ def get_aikif_structure():
 					flds +=  fld + ', '
 		txt += '<TR><TD>' + nme + '</TD><TD>' + flds + '</TD></TR>\n'
 	txt += '</TABLE><BR><BR>'
+	return txt
+	
+def get_list_data_files():
+	txt = '<H3>Master File Mapping</H3>\n<TABLE width=100% border=0 align=centre>\n'
+	pth = os.path.abspath('..\\data\\core\\')
+	print( pth )
+	lst = web.GetFileList(pth, '*.CSV')
+	txt += web.filelist2html(lst, pth)
 	return txt
 	
 def get_ontology():
--- AI/web_app/web_utils.py
+++ AI/web_app/web_utils.py
@@ -1,5 +1,9 @@
 # web_utils.py	written by Duncan Murray 26/5/2014
 # functions to convert data to HTML, etc for web dev
+import csv
+import os
+import glob
+import fnmatch
 
 def list2html(lst):
 	txt = '<TABLE width=100% border=0>'
@@ -18,6 +22,25 @@ def list2html(lst):
 	txt += '</TABLE><BR>\n'
 	return txt
 
+def GetFileList(rootPath, lstXtn, shortNameOnly='Y'):
+	# written by Duncan Murray 7/8/2013 (C) Acute Software
+	# builds a list of files and returns as a list 
+	numFiles = 0    
+	opFileList = []
+	for root, dirs, files in os.walk(rootPath):
+		for basename in files:
+			for xtn in lstXtn:
+				if fnmatch.fnmatch(basename, xtn):
+					filename = os.path.join(root, basename)
+					numFiles = numFiles + 1
+					if shortNameOnly == 'Y':
+						opFileList.append( os.path.basename(filename))
+					else:
+						opFileList.append(filename)
+	return opFileList
+
+	
+	
 def filelist2html(lst, fldr):
 	txt = '<TABLE width=100% border=0>'
 	for l in lst:
@@ -37,7 +60,12 @@ def filelist2html(lst, fldr):
 
 def link_file(f, fldr):
 	""" creates a html link for a file using folder fldr """
-	return '<a href="data/' + f + '">' + f + '</a>'
+	fname = os.path.join(fldr,f)
+	print(fname)
+	if os.path.isfile(fname):
+		return '<a href="data/' + f + '">' + f + '</a>'
+	else:
+		return f
 	#return '<a href="?' + f + '">' + f + '</a>'
 	
 
@@ -51,4 +79,17 @@ def dict_to_htmlrow(d):
 	#print res
 	res += '</TR>\n'
 	return res
+
+def read_csv_to_html_table(csvFile):
+	txt = '<TABLE width=100% border=1>'
+	with open(csvFile) as csv_file:
+		for row in csv.reader(csv_file, delimiter=','):
+			txt += "<TR>"
+			for col in row:
+				txt += "<TD>"
+				txt += col
+				txt += "</TD>"
+			txt += "</TR>"
+		txt += "</TABLE>"
+	return txt
 	
