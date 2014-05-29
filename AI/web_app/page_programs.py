e31990e5509f3d10ba6f697421a758da08fe5276
Duncan Murray <djmurray@acutesoftware.com.au>
2014-05-29 23:37:29 +0930

    collects details of python programs and displays in html

--- AI/web_app/page_programs.py
+++ AI/web_app/page_programs.py
@@ -1,7 +1,8 @@
+# coding: utf-8
 # page_programs.py	written by Duncan Murray 28/5/2014
 # handles the programs display page for AIKIF web interface
 
-import sys
+import sys, os
 import web_utils as web
 sys.path.append('..\\..\\AI')
 
@@ -12,9 +13,80 @@ def get_page():
 	return txt
 	
 def get_program_list():
-	txt = '<TABLE width=100% border=0>'
-	#print(agt.agent_list)
-	txt += '<TR><TD>Program List</TD></TR>'
+	#colList = ['FullName', 'Path', 'FileName','FileSize','Functions', 'Imports']
+	colList = ['FileName','FileSize','Functions', 'Imports']
+
+	txt = '<TABLE width=90% border=0>'
+	txt += format_file_table_header(colList)
+	fl = web.GetFileList('..\\..\\AI', ['*.py'], 'N')
+	for f in fl:
+		txt += format_file_to_html_row(f, colList)
 	txt += '</TABLE>\n\n'
 	return txt
+
 	
+def format_file_table_header(lstCols):
+	txt = '<TR>'
+	if 'FullName' in lstCols: 
+		txt += '<TD>Full Path and Filename</TD>' 
+	if 'FileName' in lstCols: 
+		txt += '<TD>File Name</TD>' 
+	if 'Path' in lstCols: 
+		txt += '<TD>Path</TD>' 
+	if 'FileSize' in lstCols: 
+		txt += '<TD>Size</TD>' 
+	if 'Imports' in lstCols:	
+		txt += '<TD>Imports</TD>' 
+	if 'Functions' in lstCols: 
+		txt += '<TD>List of Functions</TD>' 
+	txt += '</TR>\n'
+	return txt
+
+	
+def format_file_to_html_row(fname, lstCols):
+	txt = '<TR>'
+	if 'FullName' in lstCols: 
+		txt += '<TD>' + fname + '</TD>' 
+	if 'FileName' in lstCols: 
+		txt += '<TD>' + os.path.basename(fname) + '</TD>' 
+	if 'Path' in lstCols: 
+		txt += '<TD>' + os.path.abspath(fname) + '</TD>' 
+	if 'FileSize' in lstCols: 	
+		txt += '<TD>' + format(os.path.getsize(fname), ",d") + '</TD>' 
+	if 'Imports' in lstCols:
+		txt += '<TD>' + get_imports(fname) + '</TD>' 
+	if 'Functions' in lstCols:
+		txt += '<TD>' + get_functions(fname) + '</TD>' 
+	txt += '</TR>\n'
+	return txt
+
+def get_functions(fname):
+	""" get a list of functions from a Python program """
+	txt = '' # os.path.abspath(fname)
+	numLines = 0
+	with open(fname, 'r') as f:
+		for line in f:
+			numLines += 1
+			#if numLines < 15:
+			#	txt += line + '<BR>\n' 
+			
+			if line.strip()[0:4] == 'def ':
+				txt += '<PRE>' + strip_text_after_string(strip_text_after_string(line, '#')[4:], ':') + '</PRE>\n'
+			if line[0:5] == 'class':
+				txt += '<PRE>' + strip_text_after_string(strip_text_after_string(line, '#'), ':') + '</PRE>\n'
+	return txt + '<BR>'
+
+def strip_text_after_string(txt, junk):
+	if junk in txt:
+		return txt[:txt.find(junk)]
+	else:
+		return txt
+	
+def get_imports(fname):
+	""" get a list of functions from a Python program """
+	txt = ''
+	with open(fname, 'r') as f:
+		for line in f:
+			if line[0:6] == 'import':
+				txt += '<PRE>' + strip_text_after_string(line[7:], ' as ') + '</PRE>\n'
+	return txt + '<BR>'
