e3c3455b191515e96e88f9b304f8208485218620
Duncan Murray <djmurray@acutesoftware.com.au>
2014-06-07 00:51:07 +0930

    aggregating logged PC usage to diary format

--- AI/agents/aggregate/load_PC_usage.py
+++ AI/agents/aggregate/load_PC_usage.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 # load_PC_usage.py	written by Duncan Murray  19/4/2014
 # reads a text file containing PC usage collected from daily agent
 # and exports to the AIKIF structures (in personal folder)
@@ -38,53 +39,48 @@ def main():
 	print('aggregating ', ipFolder, ' to ' , opFolder)
 	rawFiles = GetRawFiles(ipFolder)
 	for f in rawFiles:
+		print('rawfile = ' + f)
 		processRawFile(f, opFolder)
 	
 def GetRawFiles(ipFolder):
 	fl = []
-	#print(ipFolder)
+	print(ipFolder)
 	for root, dirs, files in os.walk(ipFolder):
-		#print('root = ', root)
-		#print('dirs = ', dirs)
-		#print('files= ', files)
+		print('root = ', root)
+		print('dirs = ', dirs)
+		print('files= ', files)
 		for basename in files:
 			filename = os.path.join(root, basename)
-			if basename[0:9] == 'pc_usage_':
+			if basename[0:6] == 'diary_':
 				fl.append(filename)
 		#print(filename)
 		#print(basename[0:9])
-	
+	print(fl)
 	return fl
 
 
 def processRawFile(f, opFolder):
 	print ('Processing: ', f)
 	opFile = opFolder + '\\diary.csv'
+	print('Saving diary data to ' + opFile)
 	curText = ''
 	oldText = ''
 	usage = []
 	with open(opFile,'a') as op:
 		with open(f, 'r') as ip:
 			for line in ip:
-				print('line=====', line)
+				det = ''
+				#print('line=====', line)
 				cols = line.split(',')
-				dte = cols[0].split(' ')[0]
-				tme = cols[0].split(' ')[1]
-				amt = cols[1]
-				det = cols[2]
-				print('\ndte = ' , dte, '\ntme = ', tme, '\namt = ',amt,'\ndet = ', det)
-				if oldText == curText:
-					print('same')
-				usage.append(det)
-
-		uniqueUsage = set(usage)
-		
-		for res in uniqueUsage:
-			# sum amt for usage
-			# TODO
-		
-		
-			op.write(res)
+				dte = cols[0]
+				tme = cols[1]
+				amt = cols[2]
+				for pos in range(3, len(cols)):
+					det += cols[pos]   # cols[2]
+				#print('\ndte = ' , dte, '\ntme = ', tme, '\namt = ',amt,'\ndet = ', det)
+				usage.append([dte,tme,amt,det])
+		for res in usage:
+			op.write(chr(31).join(res))
 	
 if __name__ == '__main__':
     main()	
