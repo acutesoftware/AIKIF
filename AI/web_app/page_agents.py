1793795a94c7193c5071dcd9d9b4fe4e39950498
Duncan Murray <djmurray@acutesoftware.com.au>
2014-06-14 13:49:55 +0930

    adding text to page agents page showing where to update list

--- AI/web_app/page_agents.py
+++ AI/web_app/page_agents.py
@@ -6,17 +6,17 @@ import web_utils as web
 sys.path.append('..\\..\\AI')
 import run_agents as agt
 
-
 def get_page():
 	txt = ''
+	txt += '<p>Page to show agents currently setup in AIKIF.</p>'
+	txt += '<p>Modify run_agents.py to update agent list.</p>'
+	txt += '<BR><BR>'
 	txt += get_agent_list()
 	return txt
 	
 def get_agent_list():
 	txt = '<TABLE width=100% border=0>'
-	#print(agt.agent_list)
 	for a in agt.get_agent_list():
-		#txt += web.list2html(a)
 		txt += web.dict_to_htmlrow(a) 
 	txt += '</TABLE>\n\n'
 	return txt
