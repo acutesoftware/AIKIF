556944b04105de4227e92236a327023759a07fc3
Duncan Murray <djmurray@acutesoftware.com.au>
2014-06-15 22:08:54 +0930

    builds blank boards for 2048 and checkers

--- AI/toolbox/game_board_utils.py
+++ AI/toolbox/game_board_utils.py
@@ -5,9 +5,49 @@
 # assist testing usage of functions within Toolbox class.
 # 
 
+from cls_grid import Grid
+
+def build_board_2048():
+	""" builds a 2048 starting board 
+	Printing Grid
+     0     0     0     2
+     0     0     4     0
+     0     0     0     0
+     0     0     0     0
+
+	"""
+	grd = Grid(4,4, [2,4])
+	grd.new_tile()
+	grd.new_tile()
+	print(grd)
+	return grd
+	
+def build_board_checkers():
+	""" builds a checkers starting board 
+	Printing Grid
+     0     B     0     B     0     B     0     B
+     B     0     B     0     B     0     B     0
+     0     0     0     0     0     0     0     0
+     0     0     0     0     0     0     0     0
+     0     0     0     0     0     0     0     0
+     0     0     0     0     0     0     0     0
+     0     W     0     W     0     W     0     W
+     W     0     W     0     W     0     W     0
+	"""
+	grd = Grid(8,8, ["B","W"])
+	for c in range(4):
+		grd.set_tile(0,(c*2) - 1, "B")
+		grd.set_tile(1,(c*2) - 0, "B")
+		grd.set_tile(6,(c*2) + 1, "W")
+		grd.set_tile(7,(c*2) - 0, "W")
+	print(grd)
+	
+	return grd
+	
+
 def TEST():
 	""" tests for this module """
-	from cls_grid import Grid
+	
 	
 	grd = Grid(4,4, [2,4])
 	grd.new_tile()
@@ -18,9 +58,10 @@ def TEST():
 	grd2 = Grid(5,5, ['A','B'])
 	grd2.new_tile(26)
 	print(grd2)
-	
+	build_board_checkers()
 	
 	print("There are ", grd2.count_blank_positions(), " blanks in grid 2")
 
-
-TEST()
+#build_board_checkers()
+build_board_2048()
+#TEST()
