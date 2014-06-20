5149f86b5492fa18d72dc8116ac05a01eb8141f3
Duncan Murray <djmurray@acutesoftware.com.au>
2014-06-20 21:56:58 +0930

    additional tests for clean columns

--- tests/test_dataTools.py
+++ tests/test_dataTools.py
@@ -8,15 +8,24 @@ import csv
 sys.path.append("..\\AI")
 from dataTools import dataTools as cl
 					
-class TestClassCollect(unittest.TestCase):
+class TestClassDataTools(unittest.TestCase):
  
 	def setUp(self):
 		pass
 
-	def test_collect(self):
+	def test_clean_col_upper(self):
 		self.assertEqual(cl.clean_column_heading('date'), 'DATE')  # should be one file (this one)
-		self.assertEqual(cl.clean_column_heading('Country'), 'COUNTRY')  # should be one file (this one)
+		self.assertEqual(cl.clean_column_heading('Date'), 'DATE')  # should be one file (this one)
+		
+	def test_clean_col_bad_chars(self):
+		self.assertEqual(cl.clean_column_heading('__Country__%^%^%'), 'COUNTRY')  # should be one file (this one)
+		self.assertEqual(cl.clean_column_heading('phone"""__'), 'PHONE')  # should be one file (this one)
+		self.assertEqual(cl.clean_column_heading('_First_name?*__'), 'FIRST_NAME')  # should be one file (this one)
+		
+	def test_clean_col_spaces(self):
 		self.assertEqual(cl.clean_column_heading('country of birth'), 'COUNTRY_OF_BIRTH')  # should be one file (this one)
+		self.assertEqual(cl.clean_column_heading('country of birth '), 'COUNTRY_OF_BIRTH')  # should be one file (this one)
+		self.assertEqual(cl.clean_column_heading(' country of birth'), 'COUNTRY_OF_BIRTH')  # should be one file (this one)
 		
 		
 if __name__ == '__main__':
