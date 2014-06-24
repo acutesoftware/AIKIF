544844a480697ea28889d330fd871c23cb91be75
Duncan Murray <djmurray@acutesoftware.com.au>
2014-06-25 00:11:37 +0930

    convert to Python 3.4

--- AI/go_web_aikif.bat
+++ AI/go_web_aikif.bat
@@ -3,4 +3,4 @@ echo Starting web server for AIKIF
 echo call this from AIKIF folder, eg. T:\user\dev\src\python\AI\AI
 rem echo "TODO - get flask working with Python 3.3"
 start http://127.0.0.1:5000
-C:\python27\python.exe web_app\web_aikif.py
+C:\python34\python.exe web_app\web_aikif.py
