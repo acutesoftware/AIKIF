f80cfc73def4e1288f21bf4112100d2aa3ebdd36
Duncan Murray <djmurray@acutesoftware.com.au>
2014-06-24 23:19:27 +0930

    added docstrings

--- AI/lib/cls_file.py
+++ AI/lib/cls_file.py
@@ -2,6 +2,11 @@
 import os
         
 class File(object):
+	"""
+	handles various file conversions, reading, writing 
+	as well as	general file operations (delete, copy, launch)
+	"""
+	
 	def __init__(self, fname):
 		self.fullname = fname
 		self.name = fname  # convert to short name
@@ -19,23 +24,24 @@ class File(object):
 
 
 	def append_text(self, txt):
+		""" adds a line of text to a file """
 		with open(self.fullname, "a") as myfile:
 			myfile.write(txt)
 
 	def convert_to_csv(self, op_csv_file, delim):
-		# function to simply convert the diary files to csv - testing
+		""" function to simply convert the diary files to csv - testing """
 		in_txt = csv.reader(open(self.fullname, "r"), delimiter = delim)
-		#out_csv = csv.writer(open(csv_file, 'w')
 		ofile  = open(op_csv_file, 'w', newline='')
 		out_csv = csv.writer(ofile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-		#print (in_txt)
 		if in_txt != "":
 			out_csv.writerows(in_txt)
 
 	def launch(self, params=''):
+		""" launch a file using os.system() - used for starting html pages """
 		os.system("start " + self.fullname)    
 
 	def delete(self):
+		""" delete a file, dont really care if it doesnt exist """
 		if self.fullname == "":
 			pass
 		else:
@@ -45,11 +51,13 @@ class File(object):
 				print("Cant delete ",self.fullname)
 
 	def load_file_to_string(self):
+		""" load a file to a string """
 		with open(self.fullname, 'r') as f:
 			txt = f.read()
 		return txt
 		
 	def load_file_to_list(self):
+		""" load a file to a list """
 		lst = []
 		with open(self.fullname, 'r') as f:
 			for line in f:
