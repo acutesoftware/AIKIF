98c395afc0dcc3c5aa1ae3eb208c9650b5b4df0e
Duncan Murray <djmurray@acutesoftware.com.au>
2014-06-25 00:22:20 +0930

    fixed BAT file to use Python 3.4

--- AI/go_web_aikif.bat
+++ AI/go_web_aikif.bat
@@ -1,6 +1,7 @@
 @echo off
 echo Starting web server for AIKIF
-echo call this from AIKIF folder, eg. T:\user\dev\src\python\AI\AI
-rem echo "TODO - get flask working with Python 3.3"
-start http://127.0.0.1:5000
-C:\python34\python.exe web_app\web_aikif.py
+
+start /min C:\python34\python.exe web_app\web_aikif.py
+echo Server started - press SPACE to start browser
+pause
+start  http://127.0.0.1:5000
