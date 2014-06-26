8c492cd5f496643af4031cc809d3565d5d91d4dc
Duncan Murray <djmurray@acutesoftware.com.au>
2014-06-27 00:38:25 +0930

    tests for SQL aggregate function

--- tests/test_cls_sql_code_generator.py
+++ tests/test_cls_sql_code_generator.py
@@ -6,30 +6,37 @@ sys.path.append("..\\AI\\dataTools")
 from cls_sql_code_generator import SQLCodeGenerator
 class TestClassDataSet(unittest.TestCase):
  
-	def setUp(self):
-		pass
+    def setUp(self):
+        pass
 
-	def test_full_sql_generation(self):
-		
-		tst = SQLCodeGenerator('C_FACT_TABLE')
-		tst.set_column_list(['DATE', 'PRODUCT', 'CUSTOMER_NAME', 'AMOUNT'])
+    def test_full_sql_generation(self):
+        
+        tst = SQLCodeGenerator('C_FACT_TABLE')
+        tst.set_column_list(['DATE', 'PRODUCT', 'CUSTOMER_NAME', 'AMOUNT'])
 
-		tst.comment_block('Insert staging records to Fact')
-		tst.trunc_fact_table()
-		tst.populate_from_staging('RAW_DATA_TABLE', ['DATE', 'PRODUCT', 'CUSTOMER_NAME', 'AMOUNT'])
+        tst.comment_block('Insert staging records to Fact')
+        tst.trunc_fact_table()
+        tst.populate_from_staging('RAW_DATA_TABLE', ['DATE', 'PRODUCT', 'CUSTOMER_NAME', 'AMOUNT'])
 
-		tst.comment_block('DQ Fixes')
-		tst.update_set_where("PRODUCT = 'new name'", "PRODUCT = 'old name'")
-		tst.update_old_to_new("CUSTOMER_NAME", "wRong Name", "Corrected Name")
-		tst.commit()
-		tst.comment_block('Key to Dimensions')
-		tst.key_to_dimension('PRODUCT_KEY', 'substr(op.PRODUCT, 1,10)', 'U_PRODUCT_LIST', 'product_name', 'PRODUCT_KEY')
-		tst.commit()
-		tst.save('test.sql')
-		#print(tst.get_sql())
-		self.assertEqual(len(tst.get_sql()), 1112) 		
-		
-		
-		
+        tst.comment_block('DQ Fixes')
+        tst.update_set_where("PRODUCT = 'new name'", "PRODUCT = 'old name'")
+        tst.update_old_to_new("CUSTOMER_NAME", "wRong Name", "Corrected Name")
+        tst.commit()
+        tst.comment_block('Key to Dimensions')
+        tst.key_to_dimension('PRODUCT_KEY', 'substr(op.PRODUCT, 1,10)', 'U_PRODUCT_LIST', 'product_name', 'PRODUCT_KEY')
+        tst.commit()
+        tst.save('test.sql')
+        #print(tst.get_sql())
+        self.assertEqual(len(tst.get_sql()), 1112) 		
+    
+
+    def test_aggregate_1(self):
+        t2 = SQLCodeGenerator('C_SALES')
+        t2.set_column_list(['DATE', 'PRODUCT', 'CUSTOMER_NAME', 'AMOUNT'])
+        t2.aggregate('C_AGG_PRODUCT', 'PRODUCT', 'sum(AMOUNT)')
+        t2.save('test_agg1.sql')
+        self.assertEqual(len(t2.get_sql()), 138) 		
+
+
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
