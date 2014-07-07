4fb29c845d4fe7db7d072813d827158b77e3571b
Duncan Murray <djmurray@acutesoftware.com.au>
2014-07-07 23:45:52 +0930

    add short summary showing first 40 lines from files

--- AI/ontology/read_opencyc.py
+++ AI/ontology/read_opencyc.py
@@ -4,31 +4,57 @@
 import os
 import sys
 
-aikif_dir = os.path.dirname(os.path.abspath(__file__))
-fldr = os.path.abspath(aikif_dir + os.sep + "toolbox" )
+#ip_folder = os.path.dirname(os.path.abspath(__file__))  # leave src out of git - too big
+ip_folder =  'S:\\DATA\\opendata\\ontology\\OpenCyc'
+op_folder = os.path.abspath(ip_folder + os.sep + ".." + os.sep + ".." + os.sep + "data" )
 
-ipFolder = 'S:\\DATA\\opendata\\ontology\\OpenCyc'
-opFolder = '..//..//data//'  # os.getcwd()
+
+print ('ip = ', ip_folder)
+print ('op = ', op_folder)
+
 
 files = ['open-cyc.n3', 'open-cyc.rdf', 'open-cyc.trig']
 lookup = ['gauge', 'mind', 'post']
 
 
-print(aikif_dir)
- 
 def main():
+    create_html_summary()
+    
     allWords = []
     num_lines = 0
     num_definitions = 0
+    
+    
+def create_html_summary():
+    txt = '<HTML><BODY>'
     for f in files:
-        numLines = 0
-        print('Reading OpenCyc file - ', f)
-        fullName = ipFolder + '\\' + f
-        for line in open(fullName,'r'): 
-            num_lines += 1
-            if num_lines < 4:
-                print(line)
-        print('tot_lines = ' , num_lines)
-                
+        txt += summarise_file_as_html(f)
+    txt += '</BODY></HTML>'
+    
+    with open('open_cyc_summary.html', 'w') as fop:
+        fop.write(txt)
+    print('Done')
+
+def summarise_file_as_html(fname):
+    """ 
+    takes a large data file and produces a HTML summary as html
+    """
+    txt = '<H1>' + fname + '</H1>'
+    num_lines = 0
+    print('Reading OpenCyc file - ', fname)
+    with open(ip_folder + os.sep + fname, 'r') as f:
+        txt += '<PRE>'
+        for line in f: 
+            if line.strip() != '':
+                num_lines += 1
+                if num_lines < 80:
+                    txt += str(num_lines) + ': ' + line + '<HR>'
+        txt += '</PRE>'
+        txt += 'Total lines = ' + str(num_lines) + '<BR><BR>'
+    
+    return txt
+
+
+    
 if __name__ == "__main__":
 	main()
