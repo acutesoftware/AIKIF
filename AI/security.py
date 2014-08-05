e103b0704f4f50cc7482747a78afe0f9053e7f76
Duncan Murray <djmurray@acutesoftware.com.au>
2014-08-05 23:04:01 +0930

    removing old library calls

--- AI/security.py
+++ AI/security.py
@@ -1,69 +1,68 @@
 # coding: utf-8
 # security.py	written by Duncan Murray 23/3/2014	(C) Acute Software
-# searches AIKIF
+# security and privacy settings for AIKIF
 
 import os
 import sys
-sys.path.append('..//..//aspytk')
-import lib_data as dat
-import lib_file as fle
-import lib_net as net
 import AIKIF_utils as aikif
 import fileMapping as filemap
 
 
 if len(sys.argv) == 2:
-	viewStructure = sys.argv[1]
+    viewStructure = sys.argv[1]
 
 securityLevels = [
-	'NONE', 	# anyone
-	'NETWORK',  # all users on any device in the network
-	'WORK',		# your work group (customise this - have as many as desired)
-	'FAMILY',	# your family
-	'PARTNER',	# your partner
-	'SELF'		# yourself
-	]
-	
+    'NONE', 	# anyone
+    'NETWORK',  # all users on any device in the network
+    'WORK',		# your work group (customise this - have as many as desired)
+    'FAMILY',	# your family
+    'PARTNER',	# your partner
+    'SELF'		# yourself
+    ]
+    
+
+
+privateFiles = []
+privateFiles.append(filemap.GetFullFilename(filemap.FindType('thing'), filemap.FindOntology('shopping')[0]))
+privateFiles.append(filemap.GetFullFilename(filemap.FindType('event'), filemap.FindOntology('shopping')[0]))
+
+workFiles = []
+workFiles.append(filemap.GetFullFilename(filemap.FindType('location'), filemap.FindOntology('file')[0]))
+
 
 securityMappings = []
-	
+    
 def MapSecurityLevel(files, security):   
-	# call this function when adding sensitive data
-	for file in files:
-		securityMappings.append([file, security])
+    # call this function when adding sensitive data
+    for file in files:
+        securityMappings.append([file, security])
 
-	
+    
 def ShowSecurity():
-	num = 0
-	print('map\tfile\t\t\t\tlevel')
-	for map in securityMappings:
-		num = num+ 1
-		print(str(num) + '\t' + fle.GetShortFileName(map[0]) + '\t' + map[1])
+    num = 0
+    print('map\tfile\t\t\t\tlevel')
+    for map in securityMappings:
+        num = num+ 1
+        print(str(num) + '\t' + map[0] + '\t' + map[1])
 
-		
+        
 def main():
-	# main function 
+    # main function 
+    MapSecurityLevel(workFiles, 'WORK')
+    MapSecurityLevel(privateFiles, 'FAMILY')
 
-	aikif.LogProcess('Starting Security',  'view.py') # sys.modules[self.__module__].__file__)
-	print('--------------')
-	print('AIKIF Security')
-	print('--------------')	
-	
-	privateFiles = []
-	privateFiles.append(filemap.GetFullFilename(filemap.FindType('thing'), filemap.FindOntology('shopping')[0]))
-	privateFiles.append(filemap.GetFullFilename(filemap.FindType('event'), filemap.FindOntology('shopping')[0]))
-	
-	workFiles = []
-	workFiles.append(filemap.GetFullFilename(filemap.FindType('location'), filemap.FindOntology('file')[0]))
-	
-	#print(privateFiles)
-	MapSecurityLevel(workFiles, 'WORK')
-	MapSecurityLevel(privateFiles, 'FAMILY')
-	ShowSecurity()
+    aikif.LogProcess('Starting Security',  'view.py') 
+    print('--------------')
+    print('AIKIF Security Settings')
+    print('--------------')	
+    
+    
+    #print(privateFiles)
+    ShowSecurity()
 
-	
+    
 if __name__ == '__main__':
     main()	
-	
-		
-		
+    
+        
+        
