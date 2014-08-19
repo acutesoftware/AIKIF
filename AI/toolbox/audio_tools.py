9ab3eb4db4313d45c244eeaaf49b9b00c583e785
Duncan Murray <djmurray@acutesoftware.com.au>
2014-08-19 22:44:33 +0930

    trying Easy mode - still no metadata

--- AI/toolbox/audio_tools.py
+++ AI/toolbox/audio_tools.py
@@ -13,10 +13,54 @@ def TEST():
     """ local test to demo usage - see unittests for full functionality """
     print("Local test of audio_tools.py")
     fname = r"E:\backup\music\Music\_Rock\Angels\Red Back Fever\07 Red Back Fever.mp3"
-    res = get_audio_metadata(fname)
+    fname = '07 Red Back Fever.mp3'
+    #res = get_audio_metadata(fname)
+    #print(res)
+
+    res = get_easy_metadata(fname)
     print(res)
 
+    
+    
 
+def get_easy_metadata(fname):
+    """ 2nd attempt - trying easy mode """
+    from mutagen.easyid3 import EasyID3
+    #m = mutagen.File(fname, easy=True)
+    audio = EasyID3(fname)
+    audio_dict = {}
+    
+    try:
+        artist = audio["artist"]
+        print (artist)
+    except:
+        print("Cant get artist")
+        artist = ''
+        
+    try:    
+        title = audio["title"]
+        print (title)
+    except:
+        print("Cant get title")
+        title = ''
+        
+    try:
+        album = audio["album"]
+        print (album)
+    except:
+        print("Cant get album")
+        album = ''
+        
+    #print(m['length'])
+    #m['artist']
+    #m['album'] 
+    
+    audio_dict['album'] = album
+    audio_dict['title'] = title
+    audio_dict['artist'] = artist
+    
+    return audio_dict
+        
 def get_audio_metadata(fname):
     """ retrieve the metadata from an MP3 file """
     audio_dict = {}
