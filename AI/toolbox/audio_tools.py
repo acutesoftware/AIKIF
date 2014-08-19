7e9cf9ef9c42e01b425f3081bf05ef71b273d2ef
Duncan Murray <djmurray@acutesoftware.com.au>
2014-08-19 22:58:54 +0930

    Correctly finds MP3 metadata once mutagenx installed

--- AI/toolbox/audio_tools.py
+++ AI/toolbox/audio_tools.py
@@ -1,31 +1,43 @@
 # audio_tools.py  written by Duncan Murray 19/8/2014
-
+# sources:
+# https://mutagen.readthedocs.org/en/latest/api/id3.html#mutagen.mp3.MPEGInfo
+# https://bitbucket.org/lazka/mutagen/issue/162/python3-no-tags-visible-in-mp3-file
+#   (above shows bug in python 3 - suggests
+#    https://pypi.python.org/pypi/mutagenx
+# https://musicbrainz.org/doc/MusicBrainz_Picard/Tags/Mapping
+# 
 import sys
 import os
 
 try:
-    import mutagen
-    import mutagen.id3
+    import mutagenx
+    import mutagenx.id3
 except:
     print("Error: cant import mutagen")
     
 def TEST():
     """ local test to demo usage - see unittests for full functionality """
+    
     print("Local test of audio_tools.py")
     fname = r"E:\backup\music\Music\_Rock\Angels\Red Back Fever\07 Red Back Fever.mp3"
-    fname = '07 Red Back Fever.mp3'
-    #res = get_audio_metadata(fname)
-    #print(res)
-
-    res = get_easy_metadata(fname)
+    res = get_audio_metadata(fname)
     print(res)
+
+    #res = get_easy_metadata(fname)
+    #print(res)
 
     
     
 
 def get_easy_metadata(fname):
-    """ 2nd attempt - trying easy mode """
-    from mutagen.easyid3 import EasyID3
+    """ collects basic MP3 metadata
+    Works, once you use mutagenx (buried deep in issues page)
+    ['Angels']
+    ['Red Back Fever']
+    ['Red Back Fever']
+    {'album': ['Red Back Fever'], 'title': ['Red Back Fever'], 'artist': ['Angels']}    
+    """
+    from mutagenx.easyid3 import EasyID3
     #m = mutagen.File(fname, easy=True)
     audio = EasyID3(fname)
     audio_dict = {}
@@ -66,7 +78,7 @@ def get_audio_metadata(fname):
     audio_dict = {}
     print("IDv2 tag info for %s:" % fname)
     try:
-        audio = mutagen.id3.ID3(fname, translate=False)
+        audio = mutagenx.id3.ID3(fname, translate=False)
     except StandardError as err:
         print("ERROR = " + str(err))
     else:
