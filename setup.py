59d1a1f29825657dd145a487d849b64c24a5a9eb
Duncan Murray <djmurray@acutesoftware.com.au>
2014-08-21 00:18:57 +0930

    pointing url to github

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name='AIKIF',
-    version='0.0.1',
+    version='0.0.2',
     author='Duncan Murray',
     author_email='djmurray@acutesoftware.com.au',
     packages=['AI', 'tests', 'AI.agents','AI.dataTools','AI.environments','AI.lib','AI.ontology','AI.toolbox'],
-    url='http://pypi.python.org/pypi/aikif/',
+    url='https://github.com/acutesoftware/AIKIF',
     license='LICENSE.txt',
     description='Artificial Intelligence Knowledge Information Framework',
     long_description=open('README.txt').read(),
