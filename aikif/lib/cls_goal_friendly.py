43d4fa0ca4b398d6d74d4dff936eae71a06c98ab
Duncan Murray <djmurray@acutesoftware.com.au>
2014-08-21 19:30:06 +0930

    fixing package setup

--- aikif/lib/cls_goal_friendly.py
+++ aikif/lib/cls_goal_friendly.py
@@ -1,5 +1,7 @@
 # cls_goal_friendly.py
 
+from cls_goal import Goal
+
 class GoalFriendly(Goal):
     """ 
     goals around friendliness - no idea how to implement at this stage
@@ -7,22 +9,22 @@ class GoalFriendly(Goal):
     def __init__(self, acceptable_damage=10):
 
         self.maximise = True
-		self.cost_life = 0
-		self.cost_freedom = 0
-		self.cost_damage = acceptable_damage
+        self.cost_life = 0
+        self.cost_freedom = 0
+        self.cost_damage = acceptable_damage
         self.strategy = []
         
         
     def check_for_success(self):
-		pass = True
-		if self.cost_life > 0:
-			pass = False
-		if self.cost_freedom > 0:
-			pass = False
-		if self.cost_damage > accepted_damage:
-			pass = False
-			
-        return pass
+        res = True
+        if self.cost_life > 0:
+            res = False
+        if self.cost_freedom > 0:
+            res = False
+        if self.cost_damage > accepted_damage:
+            res = False
+            
+        return res
 
     def run_plan(self, strategy):
         """ 
--- setup.py
+++ setup.py
@@ -5,9 +5,22 @@ setup(
     version='0.0.2',
     author='Duncan Murray',
     author_email='djmurray@acutesoftware.com.au',
-    packages=['AI', 'tests', 'AI.agents','AI.dataTools','AI.environments','AI.lib','AI.ontology','AI.toolbox'],
+    packages=['aikif', 'tests', 'aikif.agents','aikif.dataTools','aikif.environments','aikif.lib','aikif.ontology','aikif.toolbox', 'aikif.web_app'],
     url='https://github.com/acutesoftware/AIKIF',
+    doc_files='CHANGES.txt',
     license='LICENSE.txt',
     description='Artificial Intelligence Knowledge Information Framework',
     long_description=open('README.txt').read(),
+    classifiers = [
+    'Programming Language :: Python',
+    'Development Status :: 2 - Pre-Alpha',
+    'Natural Language :: English',
+    'Environment :: Web Environment',
+    'Intended Audience :: Developers',
+    'Operating System :: OS Independent',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+
 )
