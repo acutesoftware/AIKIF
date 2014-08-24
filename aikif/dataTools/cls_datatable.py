c27c94edee2726062a981b758330ffd63d2e6090
Duncan Murray <djmurray@acutesoftware.com.au>
2014-08-25 00:07:07 +0930

    percentile calculations added to datatable - may not be final location

--- aikif/dataTools/cls_datatable.py
+++ aikif/dataTools/cls_datatable.py
@@ -1,11 +1,36 @@
 # cls_datatable.py	written by Duncan Murray 25/6/2014
 
-from cls_dataset import DataSet
+#from cls_dataset import DataSet
+import math
 
 class DataTable(object): 
     """
     A data table is a single grid of data, such as a 
     CSV / TXT file or database view or table.
+    
+    Sample input ( test.csv)
+        TERM,GENDER,ID,tot1,tot2
+        5320,M,78,18,66
+        1310,M,78,10,12
+        1310,F,78,1,45
+        1310,F,16,0,2
+        1310,F,16,5,12
+        5320,F,16,31,40
+        1310,F,16,67,83
+    >> describe_contents()
+        ======================================================================
+        TERM      GENDER    ID        tot1      tot2
+        5320      M         78        18        66
+        1310      M         78        10        12
+        1310      F         78        1         45
+        1310      F         16        0         2
+        1310      F         16        5         12
+        5320      F         16        31        40
+        1310      F         16        67        83
+
+        Table  =  5 cols x 7 rows
+        HEADER =  ['TERM', 'GENDER', 'ID', 'tot1', 'tot2']
+        arr    =  [['5320', 'M', '78', '18', '66'], ['1310', 'M', '78', '10', '12']]    
     """
 
     def __init__(self, name, dataset_type):
@@ -22,10 +47,101 @@ class DataTable(object):
         res += '\n'
         for row in self.arr:
             for c in row:
-                res += c.ljust(15)
+                res += self.force_to_string(c).ljust(10)
             res += '\n'
         return res
-    
+
+    def describe_contents(self):
+        """ describes various contents of data table """
+        print('======================================================================')
+        print(self)
+        print('Table  = ',  str(len(self.header)) + ' cols x ' + str(len(self.arr)) + ' rows')
+        print('HEADER = ', self.get_header())
+        print('arr    = ', self.arr[0:2])
+        #for num, itm in enumerate(self.get_header()):
+        #    print('HEADER ', num, itm)
+
+    def select_where(self, where_col_list, where_value_list):
+        """ 
+        selects rows from the array where col_list == val_list
+        """
+        res = []        # list of rows to be returned
+        col_ids = []    # ids of the columns to check
+        
+        for col_id, col in enumerate(self.header):
+            if col in where_col_list:
+                col_ids.append([col_id, col])
+        #print('select_where    : col_ids = ',  col_ids)   # correctly prints [[0, 'TERM'], [2, 'ID']]
+        
+        for row_num, row in enumerate(self.arr):
+            keep_this_row = True
+            for ndx, where_col in enumerate(col_ids):
+                if row[where_col[0]] != where_value_list[ndx]:
+                    keep_this_row = False
+            if keep_this_row == True: 
+                res.append([row_num, row])
+        return res
+
+    def force_to_string(self,unknown):
+        result = ''
+        if type(unknown) is str:
+            result = unknown
+        if type(unknown) is int:
+            result = str(unknown)
+        if type(unknown) is float:
+            result = str(unknown)
+        if type(unknown) is dict:
+            result = Dict2String(unknown)
+        if type(unknown) is list:
+            result = List2String(unknown)
+        
+        return result
+        
+
+        
+    def update_where(self, col, value, where_col_list, where_value_list):
+        """ 
+        updates the array to set cell = value where col_list == val_list
+        """
+        if type(col) is str:
+            col_ndx = self.get_col_by_name(col)
+        else:
+            col_ndx = col
+        print('col_ndx = ', col_ndx    )
+        print("updating " + col + " to " , value, " where " , where_col_list , " = " , where_value_list)
+        new_arr = self.select_where(where_col_list, where_value_list)
+        
+        for r in new_arr:
+            self.arr[r[0]][col_ndx] = value
+        
+        print(self.arr)
+        
+    def calc_percentiles(self, col_name):
+        """ calculates the percentiles for the appropriate cols """
+        col_data = self.get_col_data_by_name(col_name)
+        #print('col_data = ', col_data)
+        first  = self.percentile(col_data, .25)
+        third  = self.percentile(col_data, .75)
+        median = self.percentile(col_data, .50)
+        #print('CALC_PERCENTILES =  first, third, median ',  first, third, median )
+        return  first, third, median 
+        
+    def percentile(self, lst_data, percent , key=lambda x:x):
+        """ calculates the 'num' percentile of the items in the list """
+        new_list = sorted(lst_data)
+        #print('new list = ' , new_list)
+        n = float(len(lst_data))
+        k = (len(new_list)-1) * percent
+        f = math.floor(k)
+        c = math.ceil(k)
+        if f == c:
+            #print(key(new_list[int(k)]))
+            return key(new_list[int(k)])
+        d0 = key(new_list[int(f)]) * (c-k)
+        d1 = key(new_list[int(c)]) * (k-f)
+        return d0+d1
+
+        
     def load(self, filename):
         """
         loads a dataset to memory - usually overriden but 
@@ -70,7 +186,9 @@ class DataTable(object):
         
     def add_cols(self, col_list):
         #print("BEFORE = " , self.arr)
-        self.arr.extend(col_list)
+        self.header.extend(col_list)
+        for r in self.arr:
+            r.extend(['0' for c in col_list])
         print("AFTER = " , self.arr)
         
     def load_to_array(self):
@@ -91,5 +209,17 @@ class DataTable(object):
             if c == col_name:
                 return num
         return 0
+        
+    def get_col_data_by_name(self, col_name, WHERE_Clause=''):
+        """ returns the values of col_name according to where """
+        col_key = self.get_col_by_name(col_name)
+        res = []
+        for row in self.arr:
+            res.append(int(row[col_key]))
+        return res
           
-            
+class DataStats(object):
+    """ class to do statistics on an array """
+    def __init__(self, arr):
+        self.arr = arr
+                   
