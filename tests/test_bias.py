d0dcd8f2d0588e9c68ea8ae6968d9942efb44cb3
Duncan Murray <djmurray@acutesoftware.com.au>
2014-10-16 23:51:37 +1030

    tests for getters for bias.py

--- tests/test_bias.py
+++ tests/test_bias.py
@@ -18,6 +18,28 @@ class BiasTest(unittest.TestCase):
         bias = mod_bias.Bias('gov.au', 'published_data', 'abs.gov.au', '')
         self.assertEqual(bias.get_bias_rating(), 0.863)
 
+    def test_03_get_source_area(self):
+        bias = mod_bias.Bias('AAA', 'BBB', 'CCC', 'DDD')
+        self.assertEqual(bias.get_source_area(), 'AAA')
+
+    def test_03_get_source_type(self):
+        bias = mod_bias.Bias('AAA', 'BBB', 'CCC', 'DDD')
+        self.assertEqual(bias.get_source_type(), 'BBB')
+
+    def test_03_get_source_website(self):
+        bias = mod_bias.Bias('AAA', 'BBB', 'CCC', 'DDD')
+        self.assertEqual(bias.get_source_website(), 'CCC')
+
+    def test_03_get_source_person(self):
+        bias = mod_bias.Bias('AAA', 'BBB', 'CCC', 'DDD')
+        self.assertEqual(bias.get_source_person(), 'DDD')
+
+        
+
+
+
+ 
+ 
  
 if __name__ == '__main__':
     unittest.main()
