6dc3995ea0e6ea628a3949d57123ade1c3fce83e
Duncan Murray <djmurray@acutesoftware.com.au>
2014-11-21 00:37:36 +1030

    simple test for ml algo - entropy tests

---
+++ tests/test_toolbox_maths_ml_algorithms.py
@@ -0,0 +1,22 @@
+# test_toolbox_maths_ml_algorithms.py
+
+import unittest
+import sys
+import os
+root_fldr = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.sep + ".." + os.sep + "aikif"  )
+sys.path.append(root_fldr)
+import toolbox.maths_ml_algorithms as ml
+
+class LogTest(unittest.TestCase):
+
+    def test_01_entropy(self):
+        self.assertEquals(ml.ml_entropy([3,3]), 1)
+        self.assertEquals(ml.ml_entropy([2,0]), 0)
+        self.assertEquals(ml.ml_entropy([1,3]), 0.811278)
+        self.assertEquals(ml.ml_entropy([16,0,0,0,0,0,0,0,0]), 0)
+        self.assertEquals(ml.ml_entropy([546,314]), 0.946848)
+        self.assertEquals(ml.ml_entropy([184,11]), 0.313027)
+
+      
+if __name__ == '__main__':
+    unittest.main()
