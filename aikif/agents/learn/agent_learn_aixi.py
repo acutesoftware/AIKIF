e6abf35d339bfc93cfa4a5ac5e7fe32883171099
Duncan Murray <djmurray@acutesoftware.com.au>
2014-12-14 23:57:42 +1030

    placeholder for pyaixi

---
+++ aikif/agents/learn/agent_learn_aixi.py
@@ -0,0 +1,31 @@
+# agent_learn_aixi.py		written by Duncan Murray	14/12/2014
+
+
+import os
+import sys
+import random
+root_folder = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.sep + ".." + os.sep + ".." ) 
+sys.path.append(root_folder)
+print("agent_learn_aixi : root_folder = " + root_folder)
+import aikif.agents.agent as agt
+import cls_log
+		
+
+def TEST():
+    agt = Aixi('test aixi', root_folder, True, 1)
+    print(agt)
+        
+class Aixi(agt.Agent):
+    """
+    test agent to run pyaixi
+    """
+    def __init__(self, name,  fldr, running, LOG_LEVEL):
+        agt.Agent.__init__(self, name,  fldr, running)
+        self.LOG_LEVEL = LOG_LEVEL
+        
+    def __str__(self):
+        res = ''
+        res += self.name
+        return res
+        
+TEST()
