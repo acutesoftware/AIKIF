332445d877e1bf8818a749deb4bc72bbb5642261
Duncan Murray <djmurray@acutesoftware.com.au>
2015-01-07 22:39:00 +1030

    added example of dummy API call

--- aikif/agents/learn/dummy_learn_1.py
+++ aikif/agents/learn/dummy_learn_1.py
@@ -6,9 +6,12 @@ def main(arg1=55, arg2='test', arg3=[5,6,7,5,4,]):
     """
     This is a sample program to show how a learning agent can
     be logged using AIKIF. 
-    The idea is that this main function is your algorithm, and 
-    you there are several functions you can add to allow for easy
-    logging access
+    The idea is that this main function is your algorithm, which
+    will run until it finds a successful result. The result is 
+    returned and the time taken is logged.
+    
+    There can optionally be have additional functions 
+    to call to allow for easy logging access
     """
     print('Starting dummy AI algorithm with :', arg1, arg2, arg3)
     
@@ -17,7 +20,18 @@ def main(arg1=55, arg2='test', arg3=[5,6,7,5,4,]):
     
     print('Done - returning ', result)
     return result
-    
+
+
+def API_option_1_get_best_result():
+    """
+    example API call that you would implement in your AI algorithm
+    that returns the best result so far. This will be useful for 
+    running multiple simulations with a range of parameters and 
+    tracking the result by time (so to halt progress when diminishing
+    returns occur, eg when it gets within 99.9% there is often little
+    point in continuing for another 2 days
+    """
+    return 6.00006  # example
    
 if __name__ == '__main__':   
     main()
