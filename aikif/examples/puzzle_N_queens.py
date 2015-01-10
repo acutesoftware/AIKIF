f6b4bd27914f15516c9bd90deeb346740b3dd1c1
Duncan Murray <djmurray@acutesoftware.com.au>
2015-01-11 00:14:24 +1030

    placeholder for N queens puzzle to be solved by planner

---
+++ aikif/examples/puzzle_N_queens.py
@@ -0,0 +1,21 @@
+# puzzle_N_queens.py   written by Duncan Murray  10/1/2015
+
+"""
+
+
+N-Queens Problem
+================
+Place N queens on an nxn chess board such that none of the 
+queens attacks any of the others
+
+Initial State = {}  # nothing on board
+Goal - 8 queens on the board, no attacks
+
+Actions = add a queen to the board
+cost = same for all 
+
+-- to simplify the plan -- 
+first action - can only place queen in 1st col
+- 2nd queen can only go in 2nd column
+
+"""
