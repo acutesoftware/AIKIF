77e64194901b764e839dade301267af014b1d57a
Duncan Murray <djmurray@acutesoftware.com.au>
2015-01-10 23:17:20 +1030

    placeholder for plan algorithms

---
+++ aikif/lib/cls_plan_search.py
@@ -0,0 +1,19 @@
+# cls_plan_search.py    written by Duncan Murray 10/1/2015
+
+"""
+Various algorithms to do searches
+
+"""
+
+def TEST():
+    print('testing cls_plan_search.py')
+
+class Plan():
+    """ base class """
+    pass
+    
+    
+    
+if __name__ == '__main__':
+    TEST()	
+    
