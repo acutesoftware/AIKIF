95dd8124d8a31cf62b2dc95c102db7c48f647da4
Duncan Murray <djmurray@acutesoftware.com.au>
2015-01-20 00:42:35 +1030

    initial setup for TSP

---
+++ aikif/examples/solve_travelling_salesman.py
@@ -0,0 +1,40 @@
+# solve_travelling_salesman.py   written by Duncan Murray  20/1/2015
+
+"""
+
+
+Travelling Salesman problem
+===========================
+
+
+"""
+
+city_distances_csv = 'data_travel_salesman.csv'
+     
+
+def main():
+    dist = read_distance()
+    for i in dist:
+        #print(i)
+        print(i['town'] + ' is ' + i['dist'] + ' from Bucharest')
+    
+def read_distance():
+    with open(city_distances_csv, 'r') as file:
+        d = []
+        for line in file:
+            print(line)
+            cols = line.split(',')
+            town = cols[0]
+            dist = cols[1].strip('\n')
+            d.append({'town':town, 'dist':dist})
+            try:
+                print(d)
+                pass
+            except:
+                print('cant print distance')
+    return dist
+    
+
+if __name__ == '__main__':
+    main()	
+    
