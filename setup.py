69a395e2b87b81e0a017712e18a46faf9fef8fc5
Duncan Murray <djmurray@acutesoftware.com.au>
2015-02-26 01:21:05 +1030

    v0.0.5 for readme fixes

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
     name='AIKIF',
-    version='0.0.4c',
+    version='0.0.5',
     author='Duncan Murray',
     author_email='djmurray@acutesoftware.com.au',
     packages=['aikif', 'aikif.agents','aikif.agents.aggregate','aikif.agents.explore','aikif.agents.gather','aikif.agents.learn','aikif.dataTools','aikif.environments','aikif.examples','aikif.lib', 'aikif.ontology','aikif.toolbox', 'aikif.web_app'],
     url='https://github.com/acutesoftware/AIKIF',
     license='LICENSE.txt',
     description='Artificial Intelligence Knowledge Information Framework',
-    long_description=open('README.rst').read(),
+    long_description=open('README').read(),
     classifiers = [
     'Development Status :: 2 - Pre-Alpha',
     'Environment :: Web Environment',
