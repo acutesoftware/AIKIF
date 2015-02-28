81d2b07bee5df5ad8b220cc802e11f2817c80248
Duncan Murray <djmurray@acutesoftware.com.au>
2015-02-28 19:10:43 +1030

    better log tests for individual logfiles

--- tests/test_cls_log.py
+++ tests/test_cls_log.py
@@ -3,22 +3,16 @@
 import unittest
 import sys
 import os
-root_fldr = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.sep + ".." + os.sep + "aikif" )
-test_fldr = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.sep + "test_results" + os.sep + "log")
-sys.path.append(root_fldr)
-import cls_log
+import time
+test_fldr = os.getcwd() + os.sep + 'test_results'
+import aikif.cls_log as mod_log
 
 class LogTest(unittest.TestCase):
     
     def setUp(self):
         """ Note, this gets called for EACH test """
         unittest.TestCase.setUp(self)
-        self.mylog = cls_log.Log(test_fldr)
-        self.mylog.record_process('test', 'hello - recording process')
-        self.mylog.record_command('test', 'hello - recording command')        
-        self.mylog.record_result('test', 'hello - recording result')        
-        self.mylog.record_source('test', 'hello - recording source')        
-        
+        self.mylog = mod_log.Log(test_fldr)
     def tearDown(self):
         """ called once at the end of this test class """
         unittest.TestCase.tearDown(self)
@@ -31,12 +25,37 @@ class LogTest(unittest.TestCase):
         result = self.mylog.get_folder_process()
         self.assertEqual(result, test_fldr + os.sep + 'process.log')
 
-    def test_03_logsum_init(self):
-        sum = cls_log.LogSummary(self.mylog, test_fldr)
+    def test_03_append_log_process(self):
+        self.mylog.record_process('test', 'hello - recording process')
+        self.assertEqual(self.mylog.logFileProcess, os.getcwd() + os.sep + 'test_results' + os.sep + 'process.log')
+        self.assertEqual(os.path.isfile(self.mylog.logFileProcess), True)
+
+
+    def test_04_append_log_command(self):
+        self.mylog.record_command('test', 'hello - recording command')        
+        self.assertEqual(self.mylog.logFileCommand, os.getcwd() + os.sep + 'test_results' + os.sep + 'command.log')
+        self.assertEqual(os.path.isfile(self.mylog.logFileCommand), True)
+
+
+    def test_05_append_log_result(self):
+        self.mylog.record_result('test', 'hello - recording result')        
+        self.assertEqual(self.mylog.logFileResult, os.getcwd() + os.sep + 'test_results' + os.sep + 'result.log')
+        self.assertEqual(os.path.isfile(self.mylog.logFileResult), True)
+
+
+    def test_06_append_log_source(self):
+        self.mylog.record_source('test', 'hello - recording source')        
+        self.assertEqual(self.mylog.logFileSource, os.getcwd() + os.sep + 'test_results' + os.sep + 'source.log')
+        self.assertEqual(os.path.isfile(self.mylog.logFileSource), True)
+
+        
+    def test_10_logsum_init(self):
+        time.sleep(4)  # warning - fails first time this is run if no logs exist
+        sum = mod_log.LogSummary(self.mylog, test_fldr)
         self.assertTrue(len(str(sum)) > 10)
     
-    def test_04_summarise_results(self):
-        sum = cls_log.LogSummary(self.mylog, test_fldr)
+    def test_11_summarise_results(self):
+        sum = mod_log.LogSummary(self.mylog, test_fldr)
         sum.summarise_events()
     
 if __name__ == '__main__':
