7ab9c1491718354184216f6572eba07ad9d71121
Duncan Murray <djmurray@acutesoftware.com.au>
2015-03-26 21:55:50 +1030

    network tools added from project ASPYTK

---
+++ aikif/toolbox/network_tools.py
@@ -0,0 +1,40 @@
+# network_tools.py  written by Duncan Murray 26/3/2015
+
+import os
+import sys
+
+
+try:
+	import urllib.request as request
+except:
+	import urllib2 as request
+	
+import getpass
+import socket
+
+
+	
+
+def get_user_name():
+    """
+    get the username of the person logged on
+    """
+	return getpass.getuser()
+
+def get_host_name():
+    """
+    get the computer name
+    """
+	return socket.gethostname()
+	
+def download_file_no_logon(url, filename):
+    """
+    download a file from a public website with no logon required
+    """
+	output = open(filename,'wb')
+	output.write(request.urlopen(url).read())
+	output.close()
+
+    
+if __name__ == '__main__': 
+    TEST()    
