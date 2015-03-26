b9143953b467b50c362bf6603a316ff64a591a5d
Duncan Murray <djmurray@acutesoftware.com.au>
2015-03-27 00:22:43 +1030

    added download with logon - untested

--- aikif/toolbox/network_tools.py
+++ aikif/toolbox/network_tools.py
@@ -32,3 +32,18 @@ def download_file_no_logon(url, filename):
     output.write(request.urlopen(url).read())
     output.close()
 
+def download_file(p_realm, p_url, p_op_file, p_username, p_password, p_logon_url=''):
+    # https://docs.python.org/3/library/urllib.request.html#examples
+    # Create an OpenerDirector with support for Basic HTTP Authentication...
+    auth_handler = urllib.request.HTTPBasicAuthHandler()
+    auth_handler.add_password(realm=p_realm,
+                              uri=p_url,
+                              user=p_username,
+                              passwd=p_password)
+    opener = urllib.request.build_opener(auth_handler)
+    # ...and install it globally so it can be used with urlopen.
+    urllib.request.install_opener(opener)
+    if p_logon_url == '':
+        p_logon_url = p_url
+    urllib.request.urlopen(p_logon_url)
+
