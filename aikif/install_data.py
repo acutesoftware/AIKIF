93b4a2880a70156dbe69c7cb86efb148a7c1a11e
Duncan Murray <djmurray@acutesoftware.com.au>
2015-04-21 00:30:55 +0930

    debug testing of create folder structure:

--- aikif/install_data.py
+++ aikif/install_data.py
@@ -3,13 +3,14 @@
 import os
 import aikif.config as cfg
 import aikif.cls_log as mod_log
+cur_path = os.getcwd()
 
 def main():
     """
     script to setup folder structures for AIKIF 
     and prepare data tables.
     """
-    print(' --------- AIKIF Installation --------')
+    print('\n\n --------- AIKIF Installation --------')
     print(' s. show current setup')
     print(' f. setup folder structures')
     print(' c. create sample data')
@@ -19,7 +20,7 @@ def main():
     if cmd == 's':
         show_setup()   
     elif cmd == 'f':
-        setup_folders()
+        setup_folders(cur_path)
     elif cmd == 'c':
         create_sample_data()   
     elif cmd == 'w':
@@ -32,22 +33,27 @@ def main():
 def show_setup():
     print(' -= Current Setup =- ')
     
-def setup_folders():
+def setup_folders(cur_path):
     print('creating folders..')
-    create_folder('data')
-    create_folder('data\\core')
-    create_folder('data\\index')
-    create_folder('data\\log')
-    create_folder('data\\raw')
-    create_folder('data\\ref')
-    create_folder('data\\temp')
+    create_folder(cur_path + os.sep + 'data')
+    create_folder(cur_path + os.sep + 'data' + os.sep + 'core')
+    create_folder(cur_path + os.sep + 'data' + os.sep + 'index')
+    create_folder(cur_path + os.sep + 'data' + os.sep + 'log')
+    create_folder(cur_path + os.sep + 'data' + os.sep + 'raw')
+    create_folder(cur_path + os.sep + 'data' + os.sep + 'ref')
+    create_folder(cur_path + os.sep + 'data' + os.sep + 'temp')
     
 
 def create_sample_data():
     print('creating sample data')
 
 def create_folder(fldr):
-    print('create folder : ', fldr)
+    print('creating folder ' + fldr)
+    ensure_dir(fldr)
 
- 
+def ensure_dir(f):
+    d = os.path.dirname(f)
+    #print('ensure_dir=' + d)
+    if not os.path.exists(d):
+        os.makedirs(d) 
 main()    
