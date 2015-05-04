eb4a24a62e38efd76f28ded5fad60dd2c8dc8c86
Duncan Murray <djmurray@acutesoftware.com.au>
2015-05-04 20:51:29 +0930

    Initial command line interface for AIKIF

---
+++ aikif/AI.py
@@ -0,0 +1,116 @@
+# AI.py     written by Duncan Murray 2/5/2015
+
+import os
+import sys
+
+prompt = '> '
+
+# commands are structured as [lst_VALID_PROMPTS, str_FUNCTION_TO_CALL, str_DESCRIPTION]
+all_commands = {}  
+all_commands['exit'] = [[':q', '/q', 'exit', 'quit'], 'Exit the application']
+all_commands['help'] = [[':h', '/h', 'help'], 'Show help']
+all_commands['cmd'] =  [[':!', '/!', 'cmd', 'command'], 'Return to Command mode']
+all_commands['add'] =  [[':a', '/a', 'add'], 'Enter Add mode']
+
+modes = ['COMMAND', 'ADD', 'QUERY']
+
+
+def main():
+    cmd = '?'
+    mode = 'COMMAND'
+    welcome()
+    while cmd not in all_commands['exit'][0]:
+        cmd = get_command(mode)
+        print(cmd)
+        result, mode = process(cmd, mode)
+        show_output(result)
+
+def get_command(mode):
+    """
+    Takes a raw user input, initially from command line
+    Later: 
+        - check user credentials
+        - rate bias
+        - check for malicious commands
+    """
+    if mode == 'COMMAND':
+        prompt = '> '
+    elif mode == 'ADD':
+        prompt = 'ADD > '
+    txt = input(prompt)
+    return txt 
+ 
+def show_output(txt, device='Console'):
+    """
+    Output the data to the current device (usually
+    screen via print statements but can be file or
+    network / API
+    """
+    if device == 'Console':
+        print(txt)
+    elif device == 'File':
+        raise ('show_output to File not implemented')
+
+def process(txt, mode):
+    """
+    Top level function to process the command, mainly
+    depending on mode.
+    This should work by using the function name defined
+    in all_commamnds
+    """
+    result = ''
+    if txt in all_commands['exit'][0]:
+        cmd_exit()
+    elif txt in all_commands['help'][0]:
+        cmd_help()
+    elif txt in all_commands['cmd'][0]:
+        show_output('Returning to Command mode')
+        mode = 'COMMAND'
+        prompt = '> '
+        
+    elif txt in all_commands['add'][0]:
+        if mode == 'ADD':  # already here, so add data
+            cmd_add(txt)
+        else:   # enter add mode
+            show_output('Entering Add mode')
+            mode = 'ADD'
+            prompt = 'ADD > '
+        
+    return result, mode
+    
+#####################################################
+#  COMMAND PROCESSING FUNCTIONS
+#####################################################  
+  
+def welcome():
+    show_output('Welcome to the AIKIF Command Line Interface')
+    
+def cmd_exit():
+    show_output('Bye')
+    exit(0)
+    
+
+def cmd_help():
+    txt = '\n'
+    for k,v in all_commands.items():
+        txt += k.ljust(6) + '= ' 
+        txt += v[1].ljust(25)
+        #print('v[0]= ' ,v[0])
+        for cmd in v[0]:
+            #print('cmd = ', cmd)
+            txt += cmd + ', '
+        txt += '\n'
+    show_output(txt)
+
+
+def cmd_add(txt):
+    """
+    Enter add mode - all text entered now will be 
+    processed as adding information until cancelled
+    """
+    show_output('Adding ' + txt)
+
+    
+if __name__ == '__main__':        
+    main()
+                
