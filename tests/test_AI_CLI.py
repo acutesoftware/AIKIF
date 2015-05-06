b3c359f5b4123aad28ded18be48047099658ba99
Duncan Murray <djmurray@acutesoftware.com.au>
2015-05-06 23:50:18 +0930

    tests for AI_CLI changed to suit class based module

--- tests/test_AI_CLI.py
+++ tests/test_AI_CLI.py
@@ -3,7 +3,7 @@
 import unittest
 import sys
 import os
-import aikif.AI_CLI as cli
+import aikif.AI_CLI as mod_cli
 
 test_fldr = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.sep + "test_results" + os.sep + "log")
 
@@ -11,54 +11,59 @@ class AiCliTest(unittest.TestCase):
     
     def setUp(self):
         unittest.TestCase.setUp(self)
+        self.raw = 'blah'
+        self.cli = mod_cli.AICLI('test', False)  # dont autostart CLI
         
     def tearDown(self):
         """ called once at the end of this test class """
         unittest.TestCase.tearDown(self)
 
     def test_01_params_all_commands(self):
-        self.assertEqual(len(cli.all_commands) > 4, True)  # check for at least 5 commands
+        self.assertEqual(len(self.cli.all_commands) > 4, True)  # check for at least 5 commands
+        self.assertEqual(self.raw, 'blah')
 
     def test_02_get_command(self):
-        txt = cli.get_command('ADD', 'test_02_blah')
+        txt = self.cli.get_command('ADD', 'test_02_blah')
         self.assertEqual(txt, 'test_02_blah')
         
     def test_02_show_output(self):
-        txt = cli.show_output('Dont Print This', 'Function_Return')
+        txt = self.cli.show_output('Dont Print This', 'Function_Return')
         self.assertEqual(txt, 'Dont Print This')
-        txt = cli.show_output('Print This')  # when printing to console
+        txt = self.cli.show_output('Print This')  # when printing to console
         self.assertEqual(txt, None)          # there is no return variable
       
     def test_03_show_help(self):
-        result, mode = cli.process('help', 'COMMAND')
+        result, mode = self.cli.process('help', 'COMMAND')
         self.assertEqual(mode, 'COMMAND')   # make sure we stay in command mode
         self.assertEqual(result, '')   # No result returned when showing help
         
-    def test_03_add(self):
-        result, mode = cli.process('add', 'COMMAND')
+    def test_04_enter_add_mode(self):
+        result, mode = self.cli.process('add', 'COMMAND')
         self.assertEqual(mode, 'ADD')  
         self.assertEqual(result, 'Entering Add mode') 
         
-        result, mode = cli.process('some raw data', 'ADD')
+    def test_05_add_data(self):
+        result, mode = self.cli.process('some raw data', 'ADD')
         self.assertEqual(mode, 'ADD')  
         self.assertEqual(result, 'Added some raw data') 
         
-        result, mode = cli.process('cmd', 'ADD')
+    def test_06_exit_add_mode(self):
+        result, mode = self.cli.process('cmd', 'ADD')
         self.assertEqual(mode, 'COMMAND')  
         self.assertEqual(result, '')    # No result when returning to command mode
     
-    def test_09_exit(self):
+    def test_09_exit_program(self):
         with self.assertRaises(SystemExit) as cm:
-            result, mode = cli.process('quit', 'COMMAND')
+            result, mode = self.cli.process('quit', 'COMMAND')
         self.assertEqual(cm.exception.code, 0)
          
     def test_11_full_sequence01(self):
-        result, mode = cli.process('add', 'COMMAND')
-        self.assertEqual(result, 'Entering Add mode') 
-        
-        result, mode = cli.process('The sky is blue', 'ADD')
-        result, mode = cli.process('The grass is green', 'ADD')
-        result, mode = cli.process('water flows down', 'ADD')
+        # individual functions have already passed tests
+        # so these sections test complex data scenarios.
+         
+        self.cli.process('The sky is blue', 'ADD')
+        self.cli.process('The grass is green', 'ADD')
+        self.cli.process('water flows down', 'ADD')
         
         
         
