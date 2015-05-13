fc9abeff2182fffcf1a510aeb9524cea70a79d5c
Duncan Murray <djmurray@acutesoftware.com.au>
2015-05-13 22:19:30 +0930

    having issues with download files in network_tools.py

--- aikif/toolbox/network_tools.py
+++ aikif/toolbox/network_tools.py
@@ -7,8 +7,11 @@ import aikif.config as mod_cfg
 try:
 	import urllib.request as request
 except:
-	import urllib2 as request
-	
+	#import urllib2 as request
+    print('cant import urllib')
+    exit(1)
+    
+#import urllib2 as request	
 import getpass
 import socket
 
@@ -34,12 +37,20 @@ def get_host_name():
 def download_file_no_logon(url, filename):
     """
     download a file from a public website with no logon required
-    """
+    
     output = open(filename,'wb')
     output.write(request.urlopen(url).read())
-    output.close()
+    output.close()    
+    """
+    import urllib.request
+    #url = "http://www.google.com/"
+    request = urllib.request.Request(url)
+    response = urllib.request.urlopen(request)
+    with open(filename,'wb') as f:
+        #print (response.read().decode('utf-8'))
+        f.write(response.read())
 
-def download_file(p_realm, p_url, p_op_file, p_username, p_password, p_logon_url=''):
+def download_file(p_realm, p_url, p_op_file, p_username, p_password):
     # https://docs.python.org/3/library/urllib.request.html#examples
     # Create an OpenerDirector with support for Basic HTTP Authentication...
     auth_handler = request.HTTPBasicAuthHandler()
@@ -50,10 +61,17 @@ def download_file(p_realm, p_url, p_op_file, p_username, p_password, p_logon_url
     opener = request.build_opener(auth_handler)
     # ...and install it globally so it can be used with urlopen.
     request.install_opener(opener)
-    if p_logon_url == '':
-        p_logon_url = p_url
-    print('p_logon_url = ', p_logon_url)
-    web = request.urlopen(p_logon_url)
+
+    web = request.urlopen(p_url)
     with open(p_op_file, 'w') as f:
         f.write(web.read().decode('utf-8'))
         
+def download_file_proxy(p_realm, p_url, p_op_file, p_username, p_password, proxies):
+    print('downloading file ', p_url)
+    print('with credentials : ', p_username, p_password)
+    r = requests.get(p_url, auth=(p_username, p_password), proxies=proxies)
+    print(r.status_code)
+    with open(p_op_file, 'wb') as fd:
+        for chunk in r.iter_content(chunk_size):
+            fd.write(chunk)    
+	# currently fails at work, 
