8246e87970ee0557a999c346638546ab5fa929c4
Duncan Murray <djmurray@acutesoftware.com.au>
2015-05-15 00:22:27 +0930

    wrapper for html extraction tools using BeautifulSoup

---
+++ aikif/toolbox/html_tools.py
@@ -0,0 +1,61 @@
+# coding: utf-8
+# html_tools.py
+
+import os
+import sys
+import aikif.toolbox.network_tools as mod_net
+from bs4 import BeautifulSoup
+
+def TEST():
+    print('html extraction tools')
+    url = 'http://xkcd.com/292'
+    links = extract_page_links(url, '')
+    for l in links:
+        print(l)
+    
+    print(extract_content(mod_net.get_web_page(url), ''))
+    
+def extract_page_links(url, searchText):	
+    links = []
+    rawText = mod_net.get_web_page(url)
+    soup = BeautifulSoup(rawText)
+    for link in soup.findAll('a'):
+        #print(link)
+        l = str(link.get('href'))
+        if searchText in l:
+            a = link.attrs
+            content = str(link.string)
+            t = str(link.get('title'))
+            if l != '/':
+                links.append(l)
+                #print(l)
+    return links	
+
+def extract_page_links_OLD(url, searchText):	
+    links = []
+    raw_text = mod_net.get_web_page(url)
+    soup = BeautifulSoup(raw_text)
+    for link in soup.findAll('a'):
+        #print(link)
+        l = str(link.get('href'))
+        if searchText in l:
+            a = link.attrs
+            content = str(link.string)
+            t = str(link.get('title'))
+            links.append({'name': content, 'url': l, 'title': t, 'txt': '', 'html': '', 'catList': []})
+    return links	
+
+   
+def extract_content(raw_text, divID):
+	html = ''
+	soup = BeautifulSoup(raw_text)
+	results = soup.find("div", {"id": divID})
+	txt = results.getText()  
+	print(str(len(txt)) + ' bytes read\n')
+	for line in results.contents:
+		html = html + str(line) + '\n'
+	return html, txt
+    
+if __name__ == '__main__':
+    TEST()    
+    
