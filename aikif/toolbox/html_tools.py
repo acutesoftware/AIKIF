8d23887db77d16120b11f778f22193dd42e67526
Duncan Murray <djmurray@acutesoftware.com.au>
2015-05-18 22:49:29 +0930

    html_tools.py has separate functions for get_text and find by div

--- aikif/toolbox/html_tools.py
+++ aikif/toolbox/html_tools.py
@@ -14,7 +14,7 @@ def TEST():
     for l in links:
         print(l)
     
-    print(extract_content(mod_net.get_web_page(url), ''))
+    print(extract_content(mod_net.get_web_page(url)))
     
 def extract_page_links(rawText, searchText):	
     links = []
@@ -46,16 +46,19 @@ def extract_page_links_OLD(url, searchText):
     return links	
 
    
-def extract_content(raw_text, divID):
-	html = ''
-	soup = BeautifulSoup(raw_text)
-	results = soup.find("div", {"id": divID})
-	txt = results.getText()  
-	print(str(len(txt)) + ' bytes read\n')
-	for line in results.contents:
-		html = html + str(line) + '\n'
-	return html, txt
+def extract_by_div(raw_text, divID):
+    html = ''
+    soup = BeautifulSoup(raw_text)
+    results = soup.find("div", {"id": divID})
+    txt = results.getText()  
+    print(str(len(txt)) + ' bytes read\n')
+    for line in results.contents:
+        html = html + str(line) + '\n'
+    return html, txt
     
+def extract_content(raw_text):
+    return BeautifulSoup(raw_text).get_text()
+     
 if __name__ == '__main__':
     TEST()    
     
