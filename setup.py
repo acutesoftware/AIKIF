5099b32607f65385693161124bbfa48f84567e7a
Duncan Murray <djmurray@acutesoftware.com.au>
2015-05-20 13:34:14 +0930

    adding pyaixi to setup as dependancy

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     long_description=open('README.txt').read(),
     install_requires=[
           'nose >= 1.0',
+          'pyaixi >= 1.0'
     ],
     classifiers = [
     'Development Status :: 2 - Pre-Alpha',
