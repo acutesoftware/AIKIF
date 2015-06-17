56a64cc364b555b1eb3a182f435486f3f74360ee
Duncan Murray <djmurray@acutesoftware.com.au>
2015-06-17 18:25:55 +0930

    import path to allow unit test to run properly

--- tests/test_agent_agg_context.py
+++ tests/test_agent_agg_context.py
@@ -2,8 +2,14 @@
 # test_agent_agg_context.py
 
 import os
+import sys
 import unittest
+root_fldr = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+print('Running unit test for - ' + root_fldr)
+sys.path.insert(0, root_fldr)
+
 import aikif.agents.aggregate.agg_context as mod_agg
+
 class TestAgentAggContext(unittest.TestCase):
     def test_01_(self):
         agentFileList = mod_agg.AggContext()
