a28bbe2ecf9b1175e6b2c42986bf83eb7e5d80f3
Duncan Murray <djmurray@acutesoftware.com.au>
2015-07-02 22:46:40 +0930

    stub for toolbox/file_tools which will use lib/file and lib/filelist

---
+++ aikif/toolbox/file_tools.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# coding: utf-8
+# file_tools.py
+
+import os
+import aikif.lib.cls_filelist as mod_fl
+import aikif.lib.cls_file as mod_file
+
+def TEST():
+    print('local testing of file_tools')
+    download('http://test.com/a.txt', '~/downloads')
+
+def download(url, dest_file):
+    """
+    downloads the file at url to dest_file 
+    """
+    print('downloading ' + url + ' to ' + dest_file)
+    
+def get_filelist(fldr):
+    """
+    extract a list of files from fldr
+    """
+    print('collecting filelist from ' + fldr)
+    
+
+if __name__ == '__main__':
+    TEST()    
