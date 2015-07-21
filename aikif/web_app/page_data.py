347aa785690912fd4c126ccef60d4227ff158eef
Duncan Murray <djmurray@acutesoftware.com.au>
2015-07-21 21:31:45 +0930

    page_data in web_app has cleaned up function names

--- aikif/web_app/page_data.py
+++ aikif/web_app/page_data.py
@@ -8,31 +8,30 @@ import aikif.web_app.web_utils as web
 cur_folder = os.path.dirname(os.path.abspath(__file__)) 
 aikif_folder = os.path.abspath(cur_folder + os.sep + ".."  )
 root_folder = os.path.abspath(aikif_folder + os.sep + '..')
+data_folder = root_folder + os.sep + 'data' + os.sep + 'core'
 print('page_data.py: root_folder = ', root_folder)
 
 #import aikif.AIKIF_utils as aikif
 import aikif.cls_file_mapping as filemap
 
-dataFolder = root_folder + os.sep + 'data' + os.sep + 'core'
 
 def get_page(dataFile=''):
 	txt = '<div id="content">\n'
 	txt += '<div id="table_list">\n'
-	txt += get_list_data_files()
+	txt += get_list_data_files(data_folder)
 	txt += '</div>\n'
 	txt += '<div id="table_data">\n'
 	if dataFile:
-		fullName = os.path.join(dataFolder , dataFile)
+		fullName = os.path.join(data_folder , dataFile)
 		print("\nREADING " + fullName + '\n')
-		txt += showDataFile(fullName)
+		txt += show_data_file(fullName)
 	else:
 		txt += get_aikif_structure()
-		#txt += get_ontology()   # no - this is the proposed file list
 	txt += '</div>\n'
 	txt += '</div>\n'
 	return txt
 	
-def showDataFile(fname):
+def show_data_file(fname):
 	""" shows a data file in CSV format - all files live in CORE folder """
 	txt = '<H2>' + fname + '</H2>'
 	print (fname)
@@ -52,17 +51,11 @@ def get_aikif_structure():
 	txt += '</TABLE><BR><BR>'
 	return txt
 	
-def get_list_data_files():
+def get_list_data_files(fldr):
 	txt = '<H3>Master File Mapping</H3>\n' # <TABLE width=100% border=0 align=centre>\n'
-	pth = os.path.abspath('..\\data\\core\\')
-	print( "get_list_data_files(): PTH = " + pth )
-	lst = web.GetFileList(pth, '*.CSV')
-	txt += web.filelist2html(lst, pth)
-	return txt
-	
-def get_ontology():
-	txt = '<H3>Master File Mapping</H3>\n<TABLE width=100% border=0 align=centre>\n'
-	lst = filemap.BuildMasterFileMapping('N')
-	txt += web.filelist2html(lst, '..\\..\\data\\')
+	print( "get_list_data_files(): fldr = " + fldr )
+	lst = web.GetFileList(fldr, '*.csv')
+    #print(lst)
+	txt += web.filelist2html(lst, fldr)
 	return txt
 	
