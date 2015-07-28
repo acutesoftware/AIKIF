057cfccec3c9ee2dfa14aa35b7244a0763fa6f76
Duncan Murray <djmurray@acutesoftware.com.au>
2015-07-28 21:37:01 +0930

    file_tools has copy files function and delete

--- aikif/toolbox/file_tools.py
+++ aikif/toolbox/file_tools.py
@@ -31,7 +31,28 @@ def get_filelist(fldr):
     print('collecting filelist from ' + fldr)
     lst = mod_fl.FileList([fldr], ['*.*'], [],  '')
     return lst.get_list()
+
+def delete_files_in_folder(fldr):
+    fl = glob.glob(fldr + os.sep + '*.*')
+    for f in fl:
+        os.remove(f)
  
+
+def copy_files_to_folder(src, dest):
+    """
+    copies all the files from src to dest folder
+    """
+    print('copying files from ' + src + '\nto ' + dest)
+    
+    all_files = glob.glob(src + os.sep + '*.txt')
+    for f in all_files:
+        print(' ... copying ' + os.path.basename(f))
+        try:
+            shutil.copy2(f , dest)
+        except Exception as ex:
+            print('ERROR copying ' + f + '\n to ' + dest + str(ex))
+  
+     
 
 if __name__ == '__main__':
     TEST()    
