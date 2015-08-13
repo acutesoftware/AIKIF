9c3b5d91f349e3fd550b428189b7f1bfad3cc68d
Duncan Murray <djmurray@acutesoftware.com.au>
2015-08-13 23:25:11 +0930

    test for config.py to call local functions for coverage

---
+++ tests/test_config.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# test_config.py
+import os
+import sys
+import unittest
+
+root_folder = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.sep + ".." ) 
+pth = root_folder + os.sep + 'aikif'
+sys.path.append(pth)
+
+import config
+
+
+class TestConfig(unittest.TestCase):
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+
+        
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+
+    
+    def test_01_param_version(self):
+        self.assertEqual(config.params['AIKIF_version'] > '0.0.8', True)
+    
+    def test_02_fldr_log_folder(self):
+        self.assertEqual(len(config.fldrs['log_folder']) > 5, True)
+
+    def test_03_read_credentials(self):
+        with open('config_credentials.txt', 'w') as f:
+            f.write('frank\nhunter2\n')
+        usr, pwd = config.read_credentials('config_credentials.txt')
+        self.assertEqual(usr, 'frank')
+        self.assertEqual(pwd, 'hunter2')
+
+    def test_04_show_config(self):
+        config.show_config()
+        self.assertEqual(1, 1)
+
+if __name__ == '__main__':
+    unittest.main()
