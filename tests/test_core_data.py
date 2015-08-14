a535654b9f93e10becfc83b2ccbbc5a30fab38e1
Duncan Murray <djmurray@acutesoftware.com.au>
2015-08-15 00:08:38 +0930

    testing save and Object contract for core_data

--- tests/test_core_data.py
+++ tests/test_core_data.py
@@ -10,7 +10,7 @@ pth = root_folder + os.sep + 'aikif'
 sys.path.append(pth)
 import core_data as mod_core
 
-test_fldr = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.sep + "test_results" + os.sep + "log")
+test_fldr = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.sep + "test_results")
 
 class CoreDataTest(unittest.TestCase):
     
@@ -32,6 +32,7 @@ class CoreDataTest(unittest.TestCase):
         self.assertEqual(str(f.drill_down()[0]) , 'Apples') 
         self.assertEqual(str(f.drill_down()[1]) , 'Chops') 
         self.assertEqual(str(f.drill_down()[2]) , 'Cheese') 
+        self.assertEqual(f.drill_up() , None) # TODO - keep track of location of graph 
 
     def test_03_events(self):
     
@@ -39,6 +40,7 @@ class CoreDataTest(unittest.TestCase):
         e = mod_core.Event('Sales Meeting', ['2015-04-11', 'Office', 'Meet with client to discuss custom software'])
         self.assertEqual(len(e.format_csv()), 85)
         self.assertEqual(len(e.format_dict()), 104)
+        self.assertEqual(str(e), 'Sales Meeting')
         
     def test_04_detailed_core_data_usage(self):
         root = mod_core.Object('Everything')
@@ -63,11 +65,37 @@ class CoreDataTest(unittest.TestCase):
         shelf = proj.get_child_by_name('Install Shelf')
         self.assertEqual(str(shelf), 'Install Shelf')
 
+        # try fake name 
+        rubbish = proj.get_child_by_name('Something that doesnt exist')
+        self.assertEqual(str(rubbish), 'None')
+
         # contract (or rollup - ie get parent) the shelf - NOT IMPLEMENTED
-        #self.assertEqual(str(shelf.contract()), 'Projects')
+        self.assertEqual(str(shelf.contract('Projects')), 'None')
         
-
-
+    def test_05_save_a_table(self):
+        try:
+            os.remove(test_fldr + os.sep + 'Events2015.user01')
+        except Exception:
+            print('file not found, but we dont care')
         
+        #ev = mod_core.CoreTable(config.fldrs['log_folder'], tpe='Events', user='user01', header=['date', 'category', 'details'])
+        ev = mod_core.CoreTable(test_fldr, tpe='Events', user='user01', header=['date', 'category', 'details'])
+        ev.add(mod_core.Event('Sales Meeting', ['2014-01-11', 'Office', 'Catchup with client']))
+        ev.add(mod_core.Event('Sales Meeting#3', ['2015-03-11', 'Office', 'Catchup with client']))
+        ev.add(mod_core.Event('DEV AIKIF - core data', ['2015-05-11', 'Software', 'update TEST - no test for CORE_DATA']))
+        ev.add(mod_core.Event('DEV LifePim - core data', ['2015-03-11', 'Software', 'use data for LifePim']))
+        ev.add(mod_core.Event('DEV AIKIF - data tools', ['2015-05-11', 'Software', 'fix data tools ']))
+        #print(ev)
+        ev.save()
+        with open(test_fldr + os.sep + 'Events2015.user01', 'r') as f:
+            txt = f.read()
+        self.assertEqual(len(txt), 353)
+        
+    def test_06_core_table(self):
+        ob = mod_core.CoreTable(test_fldr, tpe='Object', user='user03', header=['code', 'desc'])
+        print(ob) 
+        self.assertEqual(len(str(ob)), 86)
+        self.assertEqual(ob.get_filename('2999'), test_fldr + os.sep + 'Object2999.user03')
+         
 if __name__ == '__main__':
     unittest.main()
