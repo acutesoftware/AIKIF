106822f36fc0acdd288d61d7730e9c7faac5a055
acutesoftware <djmurray@acutesoftware.com.au>
2015-10-05 19:48:45 +1030

    run_agents has working list and tests for verify_agents

--- aikif/run_agents.py
+++ aikif/run_agents.py
@@ -1,4 +1,4 @@
-# run_agents.py	written by Duncan Murray  20/4/2014
+# run_agents.py written by Duncan Murray  20/4/2014
 # manages the agents schedules and calls programs as needed
 
 
@@ -7,34 +7,36 @@ import aikif.cls_log as mod_log
 
 schedule_types = ['once', 'year', 'month', 'fortnight', 'week', 'weekday', 'day', 'hour', 'minute', 'second', 'always']
 
+#print('run_agents imported')
+
 agentCodeFolder = os.path.dirname(os.path.abspath(__file__)) + os.sep 
 
 agent_list = [
     {'name': 'Browser history', 
-     'file': agentCodeFolder + 'agents\\gather\\log_browser_history.py', 
+     'file': os.path.join(agentCodeFolder,'agents','gather','log_browser_history.py'), 
      'schedule_type':'week'
     },
     {'name': 'PC Usage', 
-     'file': agentCodeFolder + 'agents\\gather\\log_PC_usage.py', 
-     'schedule_type':'minute'
+     'file': os.path.join(agentCodeFolder,'agents','gather','log_PC_usage.py'), 
+     'schedule_type':'second'
     },
-    {'name': 'Aggregate PC usage', 
-     'file': agentCodeFolder + 'agents\\aggregate\\load_PC_usage.py', 
+    #{'name': 'Aggregate PC usage', 
+    # 'file': os.path.join(agentCodeFolder,'agents','aggregate','load_PC_usage.py'), 
+    # 'schedule_type':'minute'
+    #},
+    {'name': 'File Usage', 
+     'file': os.path.join(agentCodeFolder,'agents','gather','agent_filelist.py'), 
      'schedule_type':'hour'
     },
-    {'name': 'File Usage', 
-     'file': agentCodeFolder + 'agents\\gather\\agent_filelist.py', 
-     'schedule_type':'minute'
-    },
-    {'name': 'Aggregate Context', 
-     'file': agentCodeFolder + 'agents\\aggregate\\agg_context.py', 
-     'schedule_type':'hour' 
-    }
+    #{'name': 'Aggregate Context', 
+    # 'file': os.path.join(agentCodeFolder,'agents','aggregate','agg_context.py'), 
+    # 'schedule_type':'minute' 
+    #}
 ]
 
         
 def main():
-    if verify_agents() == True:
+    if verify_agents(agent_list) == True:
         print('Agents verified')
     for i in agent_list:
         if is_agent_scheduled_to_start(i) is True:
@@ -44,9 +46,9 @@ def get_agent_list():
     print('RETURNING AGENT LIST\n\n')
     return agent_list
             
-def verify_agents():
+def verify_agents(l_agent_list):
     all_good = True
-    for i in agent_list:
+    for i in l_agent_list:
         if i['schedule_type'] not in schedule_types:
             all_good = False
             print('ERROR - ', i['name'], 'has invalid schedule type', i['schedule_type'])
@@ -69,7 +71,7 @@ def run(scriptFile, logUsage='Y'):
             if logUsage=='Y':
                 lg.record_result(scriptFile + ' success')
     except OSError as e:
-        #print("Execution failed:", e, file=sys.stderr)	
+        #print("Execution failed:", e, file=sys.stderr) 
         lg.record_result(scriptFile + ' error' + str(e))
     
             
@@ -82,7 +84,7 @@ def is_agent_scheduled_to_start(agt):
         return False
             
 if __name__ == '__main__':
-    main()	
+    main()  
     
 
 
--- tests/test_run_agents.py
+++ tests/test_run_agents.py
@@ -13,10 +13,19 @@ import run_agents as mod_agent
 class AgentTest(unittest.TestCase):
     
     def test_01_run_agents(self):
-        #print(str(self.agt))
         mod_agent.main()
         self.assertTrue(999 > 99)
 
+    def test_02_verify_agents(self):
+        dummy_agent = 'local_test_agent.py'
+        with open(dummy_agent, 'w') as f:
+            f.write('# dummy python agent\nprint("Hello world")\n')
+        ag_pass = mod_agent.verify_agents([{'name': 'TEST', 'file': dummy_agent, 'schedule_type':'hour'}])
+        self.assertTrue(ag_pass)
+
+    def test_03_verify_non_existent_agents(self):
+        ag_fail = mod_agent.verify_agents([{'name': 'FAIL', 'file': 'no_file.py', 'schedule_type':'hour'}])
+        self.assertFalse(ag_fail)
 
     
 if __name__ == '__main__':
