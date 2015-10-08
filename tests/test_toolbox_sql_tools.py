d9cff86336a614bdff5a37bc45a20db21e9d43b2
Duncan Murray <djmurray@acutesoftware.com.au>
2015-10-08 21:54:24 +1030

    sql_tools test for load_text_to_file

--- tests/test_toolbox_sql_tools.py
+++ tests/test_toolbox_sql_tools.py
@@ -25,6 +25,16 @@ class TestToolboxSqlTools(unittest.TestCase):
         lines = sql_tools.count_lines_in_file('test_toolbox_sql_tools.py' )
         self.assertEqual(lines > '29 recs read', True)
 
-
+    def test_02_load_txt_to_sql(self):
+        src_file = 'test_src_file.csv'
+        with open(src_file, 'w') as f:
+            f.write('col1,col2\nval1,val2\n')
+        sql_tools.load_txt_to_sql('tbl_testload', src_file, os.getcwd(), ['col1','col2'] )
+        self.assertTrue(os.path.exists('BACKOUT_tbl_testload.SQL'))
+        self.assertTrue(os.path.exists('CREATE_tbl_testload.SQL'))
+        self.assertTrue(os.path.exists('LOAD_tbl_testload.BAT'))
+        self.assertTrue(os.path.exists('tbl_testload.CTL'))
+        
+        
 if __name__ == '__main__':
     unittest.main()
