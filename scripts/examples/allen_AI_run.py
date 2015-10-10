d6a0bc20c70addc78089abf35d8fbb67d72d87b2
acutesoftware <djmurray@acutesoftware.com.au>
2015-10-10 23:04:40 +1030

    script to document and run the allen_AI competition entry

---
+++ scripts/examples/allen_AI_run.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+# coding: utf-8
+# Allen_AI_run.py      written by Duncan Murray 10th-Oct-2015
+
+import os
+import aikif.project as mod_prj
+import aikif.dataTools.cls_datatable as mod_dt
+
+root_folder = '/home/duncan/dev/src/python/kaggle/aicomp'
+src_data = root_folder + '/data'
+op_folder = root_folder + '/output'
+
+def main():
+    p = mod_prj.Project('Allen_AI', tpe='Software', fldr=op_folder , desc='Kaggle competetion entry for Allen_AI')
+    document_project(p)
+       
+       
+       
+def document_project(p):
+    """
+    adds notes and progress for the project
+    """
+    p.add_detail('kaggle_url', 'https://www.kaggle.com/c/the-allen-ai-science-challenge')
+    p.add_detail('files_root_folder', root_folder)
+    p.add_detail('files_src_data', src_data)
+    p.add_detail('files_op_folder', op_folder)
+    p.add_detail('date_last_ran', mod_dt.TodayAsString())
+
+
+    progress = mod_dt.DataTable('progress.csv', ',', col_names=['program', 'percent', 'details'])
+    p.log_table(progress)
+    p.record(progress, '', ['Source data download', '100%',  'download competition sample data'])
+    p.record(progress, '', ['Allen_AI_install.py', '0%',  'downloads lookup data, unzips'])
+    p.record(progress, '', ['Allen_AI_run.py', '2%',  'main script to run the program'])
+    p.record(progress, '', ['method1.py', '0%',  'method to answer questions using heuristic #1'])
+    p.record(progress, '', ['method2.py', '0%',  'method to answer questions using heuristic #2'])
+    p.record(progress, '', ['method3.py', '0%',  'method to answer questions using heuristic #3'])
+
+
+    p.build_report('allen_AI.rst', tpe='rst')
+    print('Done...')
+
+
+main()
