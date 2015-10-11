268ae099748a98a86af173d76081d10d346c0ec0
acutesoftware <djmurray@acutesoftware.com.au>
2015-10-11 11:35:26 +1030

    notes on generating interest graph to link and update bias network

---
+++ aikif/.z_prototype/interest_graph.py
@@ -0,0 +1,16 @@
+# interest_graph.py
+
+# need to map how interested a user is in the objects and concepts
+# - check facebook likes, tweets
+# - scan files on PC
+# - usage logging
+# - google docs
+# - email (lower rating for recieved)
+
+
+people = ['Frank', 'Jane', 'Pat']
+concepts = ['Movies', 'Music', 'Art', 'Travel', 'Science']
+objects = ['Die Hard', 'Mozart', 'Mona Lisa', 'France', 'Solar Power']
+
+raw_likes = {}
+raw_likes['Frank']['Die Hard'] = 1
