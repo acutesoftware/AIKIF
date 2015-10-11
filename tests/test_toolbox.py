fe3c76a1c417c20ec2f09214030aa1220a1755d3
acutesoftware <djmurray@acutesoftware.com.au>
2015-10-11 16:23:11 +1030

    test05 added to check external tools, but only runs locally if ext_path exists

--- tests/test_toolbox.py
+++ tests/test_toolbox.py
@@ -29,17 +29,29 @@ class LogTest(unittest.TestCase):
         self.assertEqual(str(self.tb), 'test_tool.py.sum_even_numbers\ntool2.py.do_stuff\n')
         
     def test_03_run_tool(self):
-        testResult = self.tb.run(self.tb.lstTools[0], [1,2,3,4,5,6,7], 'Y')
-        self.assertEqual(testResult, 12)
+        test_result = self.tb.run(self.tb.lstTools[0], [1,2,3,4,5,6,7], 'Y')
+        self.assertEqual(test_result, 12)
 
     def test_04_run_wrong_tool(self):
         suspect_tool = {'file':'ANOTHER_tool.py', 'function':'do_stuff', 'args':['list'], 'return':['int']}
         self.tb.add(suspect_tool)
         if self.tb.verify(suspect_tool):
-            testResult = self.tb.run(self.tb.lstTools[1], ['this', 'should', 'fail'], 'Y')
+            test_result = self.tb.run(self.tb.lstTools[1], ['this', 'should', 'fail'], 'Y')
         else:
-            testResult = 555
-        self.assertEqual(testResult, 555)
+            test_result = 555
+        self.assertEqual(test_result, 555)
+ 
+ 
+    def test_05_run_external_tool(self):
+        ext_tool = {'file':'method1.py', 'function':'solve_example', 'args':['list'], 'return':['int']}
+        ext_path = '/home/duncan/dev/src/python/kaggle/aicomp'
+        self.tb.add(ext_tool)
+        if os.path.exists(ext_path):  # dont test this on travis-CI, and DONT verify
+            test_result = self.tb.run(self.tb.lstTools[1], ['AAAA', 'BBB'], 'N', import_path=ext_path)
+        else:
+            test_result = 454
+ 
+        self.assertEqual(test_result, 454)
         
 if __name__ == '__main__':
     unittest.main()
