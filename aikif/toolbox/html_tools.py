6b5af888d3d32135ac81fc833ca29513691bd97f
acutesoftware <djmurray@acutesoftware.com.au>
2015-10-18 09:57:35 +1030

    removed test function and __main__ call

--- aikif/toolbox/html_tools.py
+++ aikif/toolbox/html_tools.py
@@ -4,18 +4,9 @@
 from bs4 import BeautifulSoup
 import aikif.toolbox.network_tools as mod_net
 
-def TEST():
-    print('html extraction tools')
+
     
-    url = 'http://xkcd.com/292'
-    raw_text = mod_net.get_web_page(url)
-    links = extract_page_links(raw_text, '')
-    for l in links:
-        print(l)
-    
-    print(extract_content(mod_net.get_web_page(url)))
-    
-def extract_page_links(rawText, searchText):	
+def extract_page_links(rawText, searchText):    
     links = []
     soup = BeautifulSoup(rawText)
     for link in soup.findAll('a'):
@@ -24,7 +15,7 @@ def extract_page_links(rawText, searchText):
         if searchText in l:
             if l != '/':
                 links.append(l)
-    return links	
+    return links    
 
  
 def extract_by_div(raw_text, divID):
@@ -39,7 +30,3 @@ def extract_by_div(raw_text, divID):
     
 def extract_content(raw_text):
     return BeautifulSoup(raw_text).get_text()
-     
-if __name__ == '__main__':
-    TEST()    
-    
