328cf4f12d4f0f0ac335253ae3770612e4faf660
acutesoftware <djmurray@acutesoftware.com.au>
2015-10-19 19:37:32 +1030

    tests for clear, save, load grid

--- tests/test_toolbox_grid.py
+++ tests/test_toolbox_grid.py
@@ -35,7 +35,60 @@ class ToolboxGridTest(unittest.TestCase):
         for i in range(50):
             grd.new_tile()
         self.assertEqual(grd.count_blank_positions(),10)
+        
+            
+    def test_05_clear(self):
+        grd.clear()
+        self.assertTrue(grd.is_empty(1,1))
+        self.assertEqual(grd.count_filled_positions(),0)
+        self.assertEqual(grd.count_blank_positions(),64)
+        grd.new_tile()
+        grd.new_tile()
+        self.assertEqual(grd.count_blank_positions(),62)
+        for i in range(50):
+            grd.new_tile()
+        self.assertEqual(grd.count_blank_positions(),12)
         print('\n', grd)
+        
+    def test_06_save_grid(self):
+        good_file = 'my_grid.txt'
+        crap_file = '/\/\_BAD_FILENAME.txt'
+        grd.save(good_file)
+        self.assertTrue(os.path.exists(good_file))    
+        grd.save(crap_file)
+        self.assertFalse(os.path.exists(crap_file))    
+        
+    def test_07_load_grid(self):
+        test_grid_data = """.XX.
+X.X.
+....
+XXXX"""
+        with open('test_grid.txt', 'w') as f:
+            f.write(test_grid_data)
+        self.assertTrue(os.path.exists('test_grid.txt'))
+        
+        # load the grid
+        grd.clear()
+        grd.load('test_grid.txt')
+        
+        # grid should have changed from 8x8 to 4x4
+        self.assertEqual(grd.get_grid_height(),4)
+        self.assertEqual(grd.get_grid_width(),4)
+        
+        # spot check new values
+        self.assertEqual(grd.count_blank_positions(),8)
+        self.assertEqual(grd.get_tile(0,0),'.')
+        self.assertEqual(grd.get_tile(0,1),'X')
+        self.assertEqual(grd.get_tile(0,2),'X')
+        self.assertEqual(grd.get_tile(0,3),'.')
+
+        self.assertEqual(grd.get_tile(3,0),'X')
+        self.assertEqual(grd.get_tile(3,1),'X')
+        self.assertEqual(grd.get_tile(3,2),'X')
+        self.assertEqual(grd.get_tile(3,3),'X')
+        
+        
+        
             
 if __name__ == '__main__':
     unittest.main()
