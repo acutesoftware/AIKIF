19948aebc80f6725d26307260e0eb20176564bb6
Duncan Murray <djmurray@acutesoftware.com.au>
2015-10-20 21:53:09 +1030

    cls_dataset test includes login creds

--- tests/test_cls_dataset.py
+++ tests/test_cls_dataset.py
@@ -1,32 +1,56 @@
-# test_cls_dataset.py	written by Duncan Murray 25/6/2014
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# test_cls_dataset.py
 
 import unittest
 import sys
-import aikif.dataTools.cls_dataset as cl
-					
+import os
+
+root_folder = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.sep + ".." ) 
+pth = root_folder + os.sep + 'aikif' + os.sep + 'dataTools' 
+
+sys.path.append(pth)
+
+
+import cls_dataset as cl
+                    
 class TestClassDataSet(unittest.TestCase):
  
-	def setUp(self):
-		pass
+    def setUp(self):
+        pass
 
-	def test_01_create_folder(self):
-		fldr = cl.DataSet('Folder of CSV Files', 'folder')
-		fldr.add('file1.csv')
-		fldr.add('file2.csv')
-		print("test 1 - folder")
-		self.assertEqual(len(fldr.datatables), 2) 
-		self.assertEqual(len(fldr.list_tables('\n')), 19) 		
-		
-	def test_02_create_database(self):
-		schema = cl.DataSet('schema', 'database')
-		schema.add('C_CUSTOMERS')
-		schema.add('C_PRODUCTS')
-		schema.add('C_SALES')
-		print("test 2 - database")
-		self.assertEqual(len(schema.datatables), 3)  
-		self.assertEqual(len(schema.list_tables('')), 28)  
+    def test_01_create_folder(self):
+        fldr = cl.DataSet('Folder of CSV Files', 'folder')
+        fldr.add('file1.csv')
+        fldr.add('file2.csv')
+        display_str = str(fldr)
+        print(display_str)
+        
+        self.assertEqual(len(fldr.datatables), 2) 
+        self.assertEqual(len(fldr.list_tables('\n')), 19)
+        self.assertEqual(display_str, """Folder of CSV Files contains the tables 
+file1.csv
+file2.csv""")
+        
+        
+    def test_02_create_database(self):
+        schema = cl.DataSet('schema', 'database')
+        schema.add('C_CUSTOMERS')
+        schema.add('C_PRODUCTS')
+        schema.add('C_SALES')
+        print("test 2 - database")
+        self.assertEqual(len(schema.datatables), 3)  
+        self.assertEqual(len(schema.list_tables('')), 28)  
 
-		
-		
+    def test_03_check_creds(self):
+        schema2 = cl.DataSet('schema', 'database', ['dbase_name', 'username', 'password'])
+        print(schema2)
+        
+    def test_04_creds_added_later(self):
+        schema3 = cl.DataSet('schema', 'database' )
+    
+        schema3.login('dbase_name', 'username', 'password')
+        schema3.logout()
+        
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
