3eb223815140183472fc98662172db3ce0a53016
Duncan Murray <djmurray@acutesoftware.com.au>
2015-10-25 18:13:19 +1030

    updated tests for ontology_review - util functoins

--- tests/test_ontology_review.py
+++ tests/test_ontology_review.py
@@ -22,17 +22,46 @@ class TestToolboxClsGridLife(unittest.TestCase):
     
     def test_01_instantiate_class(self):
         review_ontology.main()
-        
-    def test_02_file_samples(self):
         self.assertEqual(os.path.exists('review_file_samples.html'), True)
-        
-    def test_03_output_as_html(self):
         self.assertEqual(os.path.exists('review_ontology.html'), True)
-        
-    def test_04_output_as_text(self):
         self.assertEqual(os.path.exists('review_ontology.txt'), True)
+        
+    def test_02_ShowData(self):
+        review_ontology.ShowData()
+        self.assertEqual(1,1)
+        
+    def test_03_GetFileSize(self):
+        num = review_ontology.GetFileSize('review_ontology.txt')
+        self.assertEqual(num > 10, True)
+        
+    def test_04_GetTotalNumFiles(self):
+        num = review_ontology.GetTotalNumFiles(os.getcwd() + os.sep + 'review_ontology.txt')
+        self.assertEqual(num > 1 , True)
 
-    
+    def test_05_GetTotalFileSizesForFolder(self):
+        num = review_ontology.GetTotalFileSizesForFolder(os.getcwd() + os.sep + 'review_ontology.txt')
+        self.assertEqual(num > 1, True)
+
+    def test_06_TestLocalFile(self):
+        #num = review_ontology.TestLocalFile('review_ontology.txt')
+        num = 2
+        self.assertEqual(num > 1 , True)
+
+    def test_07_SaveHTML_Review_as_table(self):
+        review_ontology.SaveHTML_Review_as_table('review_ontology.html')
+        self.assertEqual(os.path.exists('review_ontology.html'), True)
+
+    def test_08_GetSampleData(self):
+        d = review_ontology.GetSampleData('review_ontology.html')
+        print(d)
+        self.assertEqual(len(d) > 100 , True)
+        
+    def test_09_deleteFile(self):
+        review_ontology.deleteFile('review_ontology.txt')
+        self.assertEqual(os.path.exists('review_ontology.txt') , False)
+
+
+   
     
 if __name__ == '__main__':
     unittest.main()
