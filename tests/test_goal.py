5b2b82546b909c7d06543c654b7c715b20797c4b
Duncan Murray <djmurray@acutesoftware.com.au>
2015-10-28 00:17:12 +1030

    goal.py fix tabs

--- tests/test_goal.py
+++ tests/test_goal.py
@@ -11,39 +11,39 @@ sys.path.insert(0, root_fldr)
 from aikif.lib.cls_goal import Goal
 from aikif.lib.cls_goal_time import GoalTime
 from aikif.lib.cls_goal_money import GoalMoney
-				
+                
 class GoalTest(unittest.TestCase):
 
-	def setUp(self):
-		""" called once at the start of this test class """
-		unittest.TestCase.setUp(self)
-		self.mygoal = Goal()
-		
-	def tearDown(self):
-		""" called once at the end of this test class """
-		unittest.TestCase.tearDown(self)
+    def setUp(self):
+        """ called once at the start of this test class """
+        unittest.TestCase.setUp(self)
+        self.mygoal = Goal()
+        
+    def tearDown(self):
+        """ called once at the end of this test class """
+        unittest.TestCase.tearDown(self)
 
-	""" 
-	tests for goals go below - use mygoal instantiated object
-	"""
-	def test_new_goal(self):
-		result = self.mygoal.get_name()
-		self.assertEqual(result, 'New Goal')
+    """ 
+    tests for goals go below - use mygoal instantiated object
+    """
+    def test_new_goal(self):
+        result = self.mygoal.get_name()
+        self.assertEqual(result, 'New Goal')
 
-	def test_success(self):
-		result = self.mygoal.check_for_success()
-		self.assertEqual(result, False)
+    def test_success(self):
+        result = self.mygoal.check_for_success()
+        self.assertEqual(result, False)
 
-	def test_time_goal(self):
-		timeGoal = GoalTime(True, 5,2)
-		result = timeGoal.check_for_success()
-		self.assertEqual(result, False)
-		
-	def test_money_goal(self):
-		moneyGoal = GoalMoney(True, 100,50)
-		result = moneyGoal.check_for_success()
-		self.assertEqual(result, False)
-		
-		
+    def test_time_goal(self):
+        timeGoal = GoalTime(True, 5,2)
+        result = timeGoal.check_for_success()
+        self.assertEqual(result, False)
+        
+    def test_money_goal(self):
+        moneyGoal = GoalMoney(True, 100,50)
+        result = moneyGoal.check_for_success()
+        self.assertEqual(result, False)
+        
+        
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
