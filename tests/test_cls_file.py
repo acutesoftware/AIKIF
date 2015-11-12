0a7823d28134d31f94ffc01f6843b70e945ff0ae
acutesoftware <djmurray@acutesoftware.com.au>
2015-11-12 22:49:48 +1030

    tests for exist, convert_csv, delete

--- tests/test_cls_file.py
+++ tests/test_cls_file.py
@@ -39,12 +39,44 @@ class TestClassFile(unittest.TestCase):
         lst = f.load_file_to_list()
         self.assertEqual(len(lst), 3) 
         
-    def test_05_file_delete(self):
+  
+    def test_06_launch(self):
         f = cl.File(self.fname)
-        
-        #f.delete()
-        
+        f.launch()    
 
+
+    def test_07_get_file_sample(self):
+        f = cl.TextFile(self.fname)
+        sample = f.get_file_sample(2)
+        self.assertEqual(len(sample.split('\n')) - 1, 2)
+
+    def test_08_convert_to_csv(self):
+        chr31_delimited_data = """D20130611000920130611PCFile0122 15UsageFacebook - Google Chrome
+D20130611001020130611PCFile0123 1UsageGoogle - Google Chrome
+D20130611001120130611PCFile0400 61UsageDesktop
+D20130611001220130611PCFile0500 60UsageDesktop
+"""
+        with open('chr31_delimited_data_file.dat', 'w') as d:
+            d.write(chr31_delimited_data)
+            
+        f = cl.TextFile('chr31_delimited_data_file.dat')
+        #self.assertEqual(f.get_file_sample(1), '00000 D20130611000920130611PCFile0122 15UsageFacebook - Google Chrome')
+        
+        f.convert_to_csv('chr31_delimited_data_file.csv', '')
+        
+        f_csv = cl.TextFile('chr31_delimited_data_file.csv')
+        self.assertEqual(f_csv.get_file_sample(1), '00000 "D201306110009","20130611","PCFile","0122"," 15","Usage","","","","","","","","","Facebook - Google Chrome",""\n')
     
+    def test_99_file_delete(self):
+        f = cl.File(self.fname)
+        self.assertEqual(f.exists(), True)
+        f.delete()
+        self.assertEqual(f.exists(), False)
+        
+        # try deleting a non existing file
+        f_no_file = cl.File('')
+        f_no_file.delete()
+        
+      
 if __name__ == '__main__':
     unittest.main()
