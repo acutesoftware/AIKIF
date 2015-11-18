6cc4d3a1ada316fb2a8000739323f88e49a494fe
Duncan Murray <djmurray@acutesoftware.com.au>
2015-11-18 21:12:26 +1030

    setup.py uses latest noise to work with python 3.5

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
           'requests >= 2.3',
           'beautifulsoup4 >= 1.0',
           'Pillow >= 1.0',
-          'noise >= 1.2.0',
+          'noise >= 1.2.2',
           'xlrd >= 0.9.0',
           'pyyaml >= 3.10',
           'psutil > 0.1.0'
