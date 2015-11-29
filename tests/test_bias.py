4cb667e999f01c3536783cbd6650b05d9166d6df
Duncan Murray <djmurray@acutesoftware.com.au>
2015-11-29 21:47:09 +1030

    test for bias using current values (untested so low result)

--- tests/test_bias.py
+++ tests/test_bias.py
@@ -8,37 +8,43 @@ sys.path.append(root_fldr)
 
 import bias as mod_bias
 
+test_metadata = [
+    {'label':'collection-method', 'value': 'website'},
+    {'label':'website', 'value': 'abs.gov.au'},
+    {'label':'source-type', 'value': 'published-data'},
+    {'label':'person-relationship', 'value': 'unknown'},
+]
+
+dummy_metadata = [  # source_area, source_type, source_website, source_person
+    {'label':'collection-method', 'value': 'AAA'},
+    {'label':'source-type', 'value': 'BBB'},
+    {'label':'website', 'value': 'CCC'},
+    {'label':'person-relationship', 'value': 'DDD'},
+]
+
+
 class BiasTest(unittest.TestCase):
 
     def test_01_str(self):
-        self.bias = mod_bias.Bias('gov.au', 'published_data', 'abs.gov.au', '')
-        self.assertEqual(len(str(self.bias)), 130)
-        print(self.bias)
+        self.bias = mod_bias.Bias(test_metadata)
+        self.assertEqual(len(str(self.bias)), 153)
+        #print(self.bias)
 
     def test_02_calc_bias(self):
-        bias = mod_bias.Bias('gov.au', 'published_data', 'abs.gov.au', '')
-        self.assertEqual(bias.get_bias_rating(), 0.863)
+        bias = mod_bias.Bias(test_metadata)
+        self.assertEqual(bias.get_bias_rating(),  5.9457650345124735e-09)
 
-    def test_03_get_source_area(self):
-        bias = mod_bias.Bias('AAA', 'BBB', 'CCC', 'DDD')
-        self.assertEqual(bias.get_source_area(), 'AAA')
-
-    def test_04_get_source_type(self):
-        bias = mod_bias.Bias('AAA', 'BBB', 'CCC', 'DDD')
-        self.assertEqual(bias.get_source_type(), 'BBB')
-
-    def test_05_get_source_website(self):
-        bias = mod_bias.Bias('AAA', 'BBB', 'CCC', 'DDD')
-        self.assertEqual(bias.get_source_website(), 'CCC')
-
-    def test_06_get_source_person(self):
-        bias = mod_bias.Bias('AAA', 'BBB', 'CCC', 'DDD')
-        self.assertEqual(bias.get_source_person(), 'DDD')
 
         
-
-
-
+    def test_07_sample_data(self):
+        dat = ['website:twitter,sender:@random,text:"Really love the new Star Wars film"',
+               'website:wikipedia,page:https://en.wikipedia.org/wiki/Exilioidea, text:"Exilioidea is a genus of sea snails, marine gastropod mollusks in the family Ptychatractidae"',
+               'source:person,actor:self,text:"I like cheese"',
+               ]
+        for d in dat:
+            #bias = mod_bias.Bias(d[0],d[1],d[2])
+            #print(d)
+            pass
  
  
  
