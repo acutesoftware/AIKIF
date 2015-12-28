8b2e219e49524ef691f74cd564f1e4d5b51fb65d
Duncan Murray <djmurray@acutesoftware.com.au>
2015-12-28 23:51:47 +1030

    stub for research method 1 for paper

---
+++ aikif/.z_prototype/research_core_data_method1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# research_core_data_method1.py
+
+import unittest
+import sys
+import os
+
+root_folder = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.sep + ".." ) 
+sys.path.append(root_folder)
+import core_data as mod_core
+
+def main():
+    print('\n==============================\nresearch_core_data_method1.py\n==============================\n')
+
+
+main()
