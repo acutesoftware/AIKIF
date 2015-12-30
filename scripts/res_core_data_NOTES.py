9788b44dcb23d1e3923b7971e4f28f302c4b2645
Duncan Murray <djmurray@acutesoftware.com.au>
2015-12-30 23:22:17 +1030

    imports header (abstract/introduction) and footer (results/bibliog) to paper

--- scripts/res_core_data_NOTES.py
+++ scripts/res_core_data_NOTES.py
@@ -10,6 +10,8 @@ root_folder = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.se
 sys.path.append(root_folder)
 import core_data as mod_core
 
+fname = 'res_core_data.rst'
+
 def main():
     """
     This generates the research document based on the results of 
@@ -17,15 +19,23 @@ def main():
     and summary
     """
     print("Generating research notes...")
-
-    # import header
-    
-    # call programs
-    import res_core_data_mthd1
-    res_core_data_mthd1.main()
-    
-    # import footer
-    
+    if os.path.exists(fname):
+       os.remove(fname)
+    with open(fname, 'w') as f:
+        f.write('================================================\n')
+        f.write('Comparison of Information Aggregation Techniques\n')
+        f.write('================================================\n\n')
+        f.write('.. contents::\n\n')
+       
+        # import header
+        f.write(open('res_core_data_HEADER.rst', 'r').read())
+        
+        # call programs
+        import res_core_data_mthd1
+        res_core_data_mthd1.main()
+        
+        # import footer
+        f.write(open('res_core_data_FOOTER.rst', 'r').read())    
     print("Done!")
     
 
