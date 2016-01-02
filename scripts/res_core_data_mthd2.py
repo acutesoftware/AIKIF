2cfde66b36aa4f37a7b8f4ccad215668637e1fce
Duncan Murray <djmurray@acutesoftware.com.au>
2016-01-03 00:38:27 +1030

    function stub for method2 for research testing

---
+++ scripts/res_core_data_mthd2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# res_core_data_mthd2.py
+
+import unittest
+import sys
+import os
+
+root_folder = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.sep + ".." + os.sep + "aikif" ) 
+sys.path.append(root_folder)
+import core_data as mod_core
+
+
+def run(fname, dat):
+    append_rst(fname, '- Data File : ' + dat + '\n')
+    append_rst(fname, '\nrunning source data ' + dat + ' .... \n\n')
+ 
+
+
+def append_rst(fname, txt):
+    with open(fname, 'a') as f:
+        f.write(txt)
