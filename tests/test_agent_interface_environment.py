6f3b01c7bf46d4be7d4c4c426a277bc84a7f42d4
Duncan Murray <djmurray@acutesoftware.com.au>
2016-01-26 00:06:46 +1030

    ensure tests for send_string fail if called from base class

--- tests/test_agent_interface_environment.py
+++ tests/test_agent_interface_environment.py
@@ -12,5 +12,34 @@ sys.path.append(pth)
 
 
 import agent_interface_env as mod_agt
-agt = mod_agt.AgentInterfaceEnv('Agent Enviroment Interface')
-print(agt)
+
+class TestAgentInterfaceEnvironment(unittest.TestCase):
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+
+    def test_01_instantiate_base_class(self):
+        res = mod_agt.AgentInterfaceEnv('Agent Enviroment Interface')
+        self.assertEqual(str(res),'Agent Enviroment Interface is RUNNING')
+        
+
+    def test_02_instantiate_env_win(self):
+        res = mod_agt.AgentInterfaceWindows('Windows Interface')
+        self.assertEqual(str(res),'Windows Interface is RUNNING')
+    
+
+    def test_10_send_key_via_baseclass(self):
+        res = mod_agt.AgentInterfaceEnv('Agent Enviroment Interface')
+        self.assertEqual(res.send_string('blah', 'some text'),'WARNING - dont run this from base class')
+
+    def test_11_send_key_via_env_windows(self):
+        res = mod_agt.AgentInterfaceWindows('Windows Interface')
+        self.assertEqual(res.send_string('blah', 'some text'),True)
+        print(res)
+
+
+        
+if __name__ == '__main__':
+    unittest.main()
