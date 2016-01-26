ef45dd16df2e0243f5472ff1ed1b546a9f3be344
Duncan Murray <djmurray@acutesoftware.com.au>
2016-01-26 20:44:38 +1030

    test to create a new text file via send_keys then cleanup

---
+++ tests/test_interface_windows_tools.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# test_interface_windows_tools.py
+import os
+import sys
+import unittest
+import time
+root_folder = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.sep + ".." ) 
+pth = root_folder + os.sep + 'aikif' + os.sep + 'toolbox' 
+
+sys.path.append(pth)
+
+
+import interface_windows_tools as mod_tool
+
+class TestAgentInterfaceEnvironment(unittest.TestCase):
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+
+    def test_01_send_text_to_notepad(self):
+        if os.path.exists('random_text.txt'):
+            os.remove('random_text.txt')
+        
+        self.assertFalse(os.path.exists('random_text.txt'))
+
+        
+        os.system('start notepad') # for linux, use "open [txtfile]"
+        time.sleep(1)
+        #hwnd = mod_tool.get_window_by_caption('Untitled - Notepad')
+        #print('Notepad window ID = ', hwnd)
+        #res = mod_tool.send_text(hwnd, 'hello')  # this fails
+        #self.assertTrue(hwnd > 0)
+        mod_tool.app_activate('Untitled - Notepad')
+        mod_tool.send_keys("^a{DELETE}hello this is some text with a //")
+        mod_tool.send_keys("^s") 
+        time.sleep(0.2) 
+        mod_tool.send_keys("random_text.txt{ENTER}")
+        time.sleep(.5) 
+        self.assertTrue(os.path.exists('random_text.txt'))
+        
+        mod_tool.send_keys('%{F4}') 
+        os.remove('random_text.txt')
+        self.assertFalse(os.path.exists('random_text.txt'))
+        
+        
+if __name__ == '__main__':
+    unittest.main()
+        
