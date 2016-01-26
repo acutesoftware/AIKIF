40c227f58670a24ed3618a723fd9de6842602d88
Duncan Murray <djmurray@acutesoftware.com.au>
2016-01-26 20:22:39 +1030

    added send_keys function that works

--- aikif/toolbox/interface_windows_tools.py
+++ aikif/toolbox/interface_windows_tools.py
@@ -9,6 +9,7 @@ import sys
 import win32gui
 import win32con
 import win32api
+import win32com.client
 
 root_folder =  os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.sep + "..") 
 print(root_folder)
@@ -30,5 +31,35 @@ def send_text(hwnd, txt):
             win32api.SendMessage(hwnd, win32con.WM_KEYUP, win32con.VK_RETURN, 0)
         else:
             win32api.SendMessage(hwnd, win32con.WM_CHAR, ord(c), 0)            
-            
-     
+
+def launch_app(app_path, params):
+    """
+    start an app
+    """
+    pass
+    
+def app_activate(caption):
+    """
+    use shell to bring the application with caption to front
+    """
+    shell = win32com.client.Dispatch("WScript.Shell")
+    shell.AppActivate(caption)
+   
+def close_app(caption):
+    """
+    close an app
+    """
+    pass
+              
+def send_keys(key_string):
+    """
+    sends the text or keys to the active application using shell
+    Note, that the imp module shows deprecation warning.
+    Examples:
+        shell.SendKeys("^a") # CTRL+A 
+        shell.SendKeys("{DELETE}") # Delete key
+        shell.SendKeys("hello this is a lot of text with a //") 
+    """
+    shell = win32com.client.Dispatch("WScript.Shell")
+    shell.SendKeys(key_string)
+
