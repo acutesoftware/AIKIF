bb5e9aa7d7db8fa86c0d5fd79b141601d41c574d
Duncan Murray <djmurray@acutesoftware.com.au>
2016-01-26 19:50:27 +1030

    initial stub for toolbox to interface with windows

---
+++ aikif/toolbox/interface_windows_tools.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# interface_windows_tools.py
+# collection of toolbox functions to send keys to windows apps
+# and capture results / screengrabs
+
+import os
+import sys
+import win32gui
+import win32con
+import win32api
+
+root_folder =  os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.sep + "..") 
+print(root_folder)
+
+def get_window_by_caption(caption):
+    """
+    finds the window by caption and returns handle (int)
+    """
+    hwnd = win32gui.FindWindow(None, caption)
+    return hwnd
+    
+def send_text(hwnd, txt):
+    """
+    sends the text 'txt' to the window handle hwnd using SendMessage
+    """
+    for c in txt:
+        if c == '\n':
+            win32api.SendMessage(hwnd, win32con.WM_KEYDOWN, win32con.VK_RETURN, 0)
+            win32api.SendMessage(hwnd, win32con.WM_KEYUP, win32con.VK_RETURN, 0)
+        else:
+            win32api.SendMessage(hwnd, win32con.WM_CHAR, ord(c), 0)            
+            
+     
