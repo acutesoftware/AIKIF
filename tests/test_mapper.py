9ae29c7caeed0b8e8228b906e6af100920c08fa8
Duncan Murray <djmurray@acutesoftware.com.au>
2016-02-11 21:09:40 +1030

    mapper tests for parser updated to account for passed lines

--- tests/test_mapper.py
+++ tests/test_mapper.py
@@ -89,7 +89,7 @@ class MapTest(unittest.TestCase):
         mapPC_Usage = mod_map.Mapper(os.path.join(fldr_raw_data, 'PC_Usage.map'))
         tot, vals = mapPC_Usage.process_raw_file(os.path.join(fldr_raw_data, 'sample_PC_usage.txt'))
         self.assertEqual(tot, 18)
-        self.assertEqual(len(vals), 7)
+        self.assertEqual(len(vals), 8)
         self.assertEqual(vals[0], 'C:\\Windows\\system32\\cmd.exe')
     
     def test_99(self):
