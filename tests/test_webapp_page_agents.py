b004950a585e7a64a749f7f00d296ce3e13514a1
Duncan Murray <djmurray@acutesoftware.com.au>
2016-02-13 10:05:54 +1030

    test for page_agents - stub so far

---
+++ tests/test_webapp_page_agents.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# test_webapp_page_agents.py
+import os
+import sys
+import unittest
+
+root_folder = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.sep + ".." ) 
+web_app_path = os.path.join(root_folder,'aikif','web_app')
+  
+sys.path.append(web_app_path)
+
+import page_agents
+
+
+class TestWebAppPageAgent(unittest.TestCase):
+
+    def test_01_get_list_of_files(self):
+        res = page_agents.get_page()
+        print(res)
+        self.assertEqual(len(res) > 40, True)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
