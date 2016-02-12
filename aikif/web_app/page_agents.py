8f4fa852aec5105d18d811840cf34a431dbf5217
Duncan Murray <djmurray@acutesoftware.com.au>
2016-02-12 21:38:08 +1030

    removed reference to run_agents

--- aikif/web_app/page_agents.py
+++ aikif/web_app/page_agents.py
@@ -1,4 +1,4 @@
-# page_agents.py	written by Duncan Murray 26/5/2014
+# page_agents.py
 # handles the agents display page for AIKIF web interface
 
 import aikif.web_app.web_utils as web
@@ -6,17 +6,9 @@ import aikif.web_app.web_utils as web
 import aikif.run_agents as agt
 
 def get_page():
-	txt = ''
-	txt += '<p>Page to show agents currently setup in AIKIF.</p>'
-	txt += '<p>Modify run_agents.py to update agent list.</p>'
-	txt += '<BR><BR>'
-	txt += get_agent_list()
-	return txt
-	
-def get_agent_list():
-	txt = '<TABLE width=100% border=0>'
-	for a in agt.get_agent_list():
-		txt += web.dict_to_htmlrow(a) 
-	txt += '</TABLE>\n\n'
-	return txt
-	
+    txt = ''
+    txt += '<p>Page to show agents currently setup in AIKIF.</p>'
+    txt += '<p>TODO</p>'
+    txt += '<BR><BR>'
+    return txt
+    
