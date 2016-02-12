a2f6dc2bfac1aa79d5a96bf06ddd8695dbebb92d
Duncan Murray <djmurray@acutesoftware.com.au>
2016-02-13 10:05:26 +1030

    page_agents shows list of agent names used

--- aikif/web_app/page_agents.py
+++ aikif/web_app/page_agents.py
@@ -1,12 +1,30 @@
 # page_agents.py
 # handles the agents display page for AIKIF web interface
 
-import aikif.web_app.web_utils as web
+import os
+import sys
+#import aikif.web_app.web_utils as web
+import web_utils as web
+
+root_fldr = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.sep + ".." )
+print(root_fldr)
 
 def get_page():
     txt = ''
     txt += '<p>Page to show agents currently setup in AIKIF.</p>'
-    txt += '<p>TODO</p>'
-    txt += '<BR><BR>'
+    txt += load_agent_list(os.path.join(root_fldr,"data"))
     return txt
     
+def load_agent_list(fldr):
+    """
+    reads the text file showing the list of agent classes used
+    and presents counts based on the agent usage
+    """
+    res = '\n'
+    with open(os.path.join(fldr, 'list_agent_names.txt'), 'r') as f:
+        for line in f:
+            res += line + '<BR>\n'
+    res += '<BR><BR>\n'
+    
+    
+    return res
