ba4191e2716fd2cb02ea1e0291314114acabf3b5
Duncan Murray <djmurray@acutesoftware.com.au>
2016-02-15 19:49:33 +1030

    tests for clear_surroundings

--- tests/test_agent_explore_grid.py
+++ tests/test_agent_explore_grid.py
@@ -6,30 +6,20 @@ import sys
 import unittest
 
 root_folder = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.sep + ".." ) 
-pth = root_folder + os.sep + 'aikif' + os.sep + 'agents' + os.sep + 'explore' 
+pth = os.path.join(root_folder,'aikif','agents','explore' )
+tool_folder = os.path.join(root_folder, 'aikif', 'toolbox')
 
 sys.path.append(pth)
 import agent_explore_grid as mod_agt
-agt = mod_agt.ExploreAgent('TEST - exploring_agent',  os.getcwd(), 1, True)
 
-tool_folder = os.path.join(root_folder, 'aikif', 'toolbox')
 sys.path.append(tool_folder)
 import cls_grid as mod_grid
+
+agt = mod_agt.ExploreAgent('TEST - exploring_agent',  os.getcwd(), 1, True)
 grd = mod_grid.Grid(grid_height=8, grid_width=8, pieces=['X', 'O'], spacing=1)   
 grd.new_tile()
 grd.new_tile()
-
 agt.set_world( grd, [3,4], [6,6])
-#agt.do_your_job()
-
-
-
-
-
-       #    start_y, start_x = myWorld.grd.find_safe_starting_point()
-            
-#    ag.set_world(myWorld.grd, [start_y, start_x], [target_coords[0], target_coords[1]])
- 
 
 
 
@@ -55,19 +45,46 @@ class TestAgentExploreGrid(unittest.TestCase):
 
     def test_04_do_your_job(self):
         txt_before = agt.show_status()
-        for i in range(0,100):
+        for i in range(0,99):
             agt.do_your_job()
         txt_after = agt.show_status()
-        self.assertFalse(agt.current_y == 3)  # we *assume* it manages to move
-        self.assertFalse(agt.current_x == 4)
+        self.assertTrue(agt.current_y != 3 or agt.current_x != 4)  # we *assume* it manages to move somewhere
 
-        
         #print(agt.grd)
         
     def test_08_clear_surroundings(self):
-        #agt.show_status()
-        self.assertEqual(1,1)
 
+        self.assertNotEqual(agt.grd.get_tile(agt.current_y - 1, agt.current_x),' ' )
+        self.assertNotEqual(agt.grd.get_tile(agt.current_y + 1, agt.current_x),' ' )
+        self.assertNotEqual(agt.grd.get_tile(agt.current_y, agt.current_x - 1),' ' )
+        self.assertNotEqual(agt.grd.get_tile(agt.current_y, agt.current_x + 1),' ' )
+
+        self.assertNotEqual(agt.grd.get_tile(agt.current_y - 1, agt.current_x - 1),' ' )
+        self.assertNotEqual(agt.grd.get_tile(agt.current_y + 1, agt.current_x - 1),' ' )
+        self.assertNotEqual(agt.grd.get_tile(agt.current_y - 1, agt.current_x - 1),' ' )
+        self.assertNotEqual(agt.grd.get_tile(agt.current_y - 1, agt.current_x + 1),' ' )
+
+        self.assertNotEqual(agt.grd.get_tile(agt.current_y - 1, agt.current_x + 1),' ' )
+        self.assertNotEqual(agt.grd.get_tile(agt.current_y + 1, agt.current_x + 1),' ' )
+        self.assertNotEqual(agt.grd.get_tile(agt.current_y + 1, agt.current_x - 1),' ' )
+        self.assertNotEqual(agt.grd.get_tile(agt.current_y + 1, agt.current_x + 1),' ' )
+    
+        agt.clear_surroundings()
+
+        self.assertEqual(agt.grd.get_tile(agt.current_y - 1, agt.current_x),' ' )
+        self.assertEqual(agt.grd.get_tile(agt.current_y + 1, agt.current_x),' ' )
+        self.assertEqual(agt.grd.get_tile(agt.current_y, agt.current_x - 1),' ' )
+        self.assertEqual(agt.grd.get_tile(agt.current_y, agt.current_x + 1),' ' )
+
+        self.assertEqual(agt.grd.get_tile(agt.current_y - 1, agt.current_x - 1),' ' )
+        self.assertEqual(agt.grd.get_tile(agt.current_y + 1, agt.current_x - 1),' ' )
+        self.assertEqual(agt.grd.get_tile(agt.current_y - 1, agt.current_x - 1),' ' )
+        self.assertEqual(agt.grd.get_tile(agt.current_y - 1, agt.current_x + 1),' ' )
+
+        self.assertEqual(agt.grd.get_tile(agt.current_y - 1, agt.current_x + 1),' ' )
+        self.assertEqual(agt.grd.get_tile(agt.current_y + 1, agt.current_x + 1),' ' )
+        self.assertEqual(agt.grd.get_tile(agt.current_y + 1, agt.current_x - 1),' ' )
+        self.assertEqual(agt.grd.get_tile(agt.current_y + 1, agt.current_x + 1),' ' )
 
 if __name__ == '__main__':
     unittest.main()
