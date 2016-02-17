5ad4d1f37aa1e7b8efffd98e6c9694a224ef2599
Duncan Murray <djmurray@acutesoftware.com.au>
2016-02-17 20:45:11 +1030

    web_page_programs tests for correct deletion of program_list.html

--- tests/test_webapp_page_programs.py
+++ tests/test_webapp_page_programs.py
@@ -14,34 +14,29 @@ import page_programs
 
 
 class TestWebAppPageData(unittest.TestCase):
-    def setUp(self):
-        unittest.TestCase.setUp(self)
-
-        
-    def tearDown(self):
-        unittest.TestCase.tearDown(self)
-
     
-    
-    def test_01_get_page_empty(self):
-        if os.path.exists('program_list.html'):
-            try:
-                os.remove('program_list.html')
-            except Exception:
-                pass
-    
-        pge = page_programs.get_page()
-        #print(pge)
-        self.assertEqual('<a href="/programs/rebuild">Rebuild Program List</a><BR>' in pge, True)
-        self.assertEqual(len(pge) < 100, True)
-
     def test_02_rebuild_list(self):
+        
         page_programs.rebuild()
         pge = page_programs.get_page()
+        self.assertTrue(os.path.exists('program_list.html'))
         #print(pge[0:1000])
         str_in_prog_list = '<TD>File Name</TD><TD>Size</TD><TD>Imports</TD><TD>List of Functions</TD>'
         self.assertEqual(str_in_prog_list in pge, True)
         self.assertEqual(len(pge) > 1000, True)
 
+        os.remove('program_list.html')
+        self.assertFalse(os.path.exists('program_list.html'))
+                
+    def test_03_get_page_empty(self):
+        if os.path.exists('program_list.html'):
+            os.remove('program_list.html')
+        pge = page_programs.get_page()
+        print(pge)
+        self.assertEqual('<a href="/programs/rebuild">Rebuild Program List</a><BR>' in pge, True)
+        self.assertEqual(len(pge) < 100, True)
+
+
+        
 if __name__ == '__main__':
     unittest.main()
