10f696beba0d284410877079b9bb4d5d522688ae
Duncan Murray <djmurray@acutesoftware.com.au>
2016-02-18 20:01:50 +1030

    fixed failing test after change page_agents html generation

--- tests/test_webapp_page_agents.py
+++ tests/test_webapp_page_agents.py
@@ -19,7 +19,7 @@ class TestWebAppPageAgent(unittest.TestCase):
         res = page_agents.get_page()
         #print(res)
         self.assertEqual(len(res) > 40, True)
-        self.assertEqual(res[0:36], '\n<TABLE><TR><TH>Class</TH><TH>Name</')
+        self.assertEqual(res[0:35], '\n<TABLE border=1 valign=top width=8')
 
 
 
