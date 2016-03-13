9fe26d6137f16889d36a8ddcf367b7df8ea6c840
Duncan Murray <djmurray@acutesoftware.com.au>
2016-03-13 21:52:01 +1030

    playing around with cls_collect and fixed test for prototype folder

--- aikif/.z_prototype/test_cls_collect.py
+++ aikif/.z_prototype/test_cls_collect.py
@@ -5,34 +5,34 @@ import unittest
 import os
 import sys
 import csv
-root_folder = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.sep + ".." + os.sep + 'aikif') 
-sys.path.append(root_folder)
-print(root_folder)
-import aikif.cls_collect_files as cl
-					
+import cls_collect_files as cl
+
+root_folder = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) ) 
+
+
 class TestClassCollect(unittest.TestCase):
  
     def setUp(self):
         pass
 
     def test_collect(self):
-        my_files = cl.clsCollectFiles(os.getcwd(), 'test_cls_collect.py')  # test_cls_collect
+        my_files = cl.clsCollectFiles(root_folder, 'test_cls_collect.py')  # test_cls_collect
         my_files.collect_filelist()
         self.assertEqual(len(my_files.get_filelist()), 1)  # should be one file (this one)
         
     def test_filesize_cwd(self):
-        my_files = cl.clsCollectFiles(os.getcwd(), '*.*')  # test_cls_collect
+        my_files = cl.clsCollectFiles(root_folder, '*.*')  # test_cls_collect
         my_files.collect_filelist()
-        self.assertEqual(my_files.get_tot_bytes() > 10000 , 1)  # should be at least 10k source code
+        self.assertEqual(my_files.get_tot_bytes() > 1000 , 1)  # should be at least 10k source code
         
     def test_folders_AI(self):
-        my_files = cl.clsCollectFiles("..\\aikif", '*.*')  # test_cls_collect
+        my_files = cl.clsCollectFiles(root_folder, '*.*')  # test_cls_collect
         my_files.collect_filelist()
-        self.assertEqual(len(my_files.get_folders()) > 11 , True)  # should be at least 11 folders
+        self.assertEqual(len(my_files.get_folders())> 0, True)  # should only be at least 1 folder in .z_prototype (pycache)
         
 
     def test_local_list_python_folder(self):
-        my_files = cl.clsCollectFiles("C:\Python34", '*.*')  # test_cls_collect
+        my_files = cl.clsCollectFiles("C:\\Python34", '*.*')  # test_cls_collect
         my_files.collect_filelist()
         self.assertEqual(my_files.get_tot_bytes() > 74913177, True ) 
         self.assertEqual(my_files.get_tot_files() > 4178, True ) 
@@ -40,4 +40,4 @@ class TestClassCollect(unittest.TestCase):
 
     
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
