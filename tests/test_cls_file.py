984d96dec466fa0055100313c0cf13139469d35c
Duncan Murray <djmurray@acutesoftware.com.au>
2016-03-25 15:16:42 +1030

    testing launch for cls_file trying to get exception to fail

--- tests/test_cls_file.py
+++ tests/test_cls_file.py
@@ -55,7 +55,11 @@ class TestClassFile(unittest.TestCase):
   
     def test_06_launch(self):
         f = cl.File(self.fname)
-        f.launch()    
+        self.assertTrue(f.launch())  
+        # test that a fake file returns an exception
+            
+        f_fail = cl.File('no such file.blah')
+        #self.assertFalse(f_fail.launch())
 
 
     def test_07_get_file_sample(self):
