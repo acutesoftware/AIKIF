d1527efbacbfe0a7157f48743952b57202bbed9e
Duncan Murray <djmurray@acutesoftware.com.au>
2016-04-28 00:04:14 +0930

    test for cls_data reading a CSV file

--- tests/test_cls_data.py
+++ tests/test_cls_data.py
@@ -17,21 +17,35 @@ class TestClassData(unittest.TestCase):
 
     def test_01_simple_string(self):
         d01 = cls_data.Data('hello')
-        print(d01)
         self.assertEqual(str(d01), 'hello') 
         self.assertEqual(str(cls_data.Data('1')), '1') 
 
-    def test_02_url(self):
-        d02 = cls_data.Data('https://github.com/acutesoftware/AIKIF/blob/master/README.txt')
-        print(d02)
-        self.assertTrue(len(str(d02)) > 25) 
-        #self.assertEqual(cls_data.Data('1'), '1') 
-
-    def test_03_number(self):
-        d03 = cls_data.Data(34.54)
-        print(d03)
-        self.assertEqual(str(d03), '34.54') 
+    def test_02_number(self):
+        d02 = cls_data.Data(34.54)
+        self.assertEqual(str(d02), '34.54') 
         self.assertEqual(str(cls_data.Data(0.5847593842)), '0.5847593842') 
+        
+    def test_03_url(self):
+        d03 = cls_data.Data('https://raw.githubusercontent.com/acutesoftware/AIKIF/master/README.txt')
+        self.assertTrue(len(str(d03)) > 25) 
+        self.assertEqual(str(d03)[0:63], 'AIKIF - Artificial Intelligence Knowledge Information Framework') 
+
+    def test_04_list(self):
+        d04 = cls_data.Data(['this', 'is', 'a', 'list', 'of', 'words'])
+        self.assertEqual(len(str(d04)), 42) 
+        self.assertEqual(str(d04), "['this', 'is', 'a', 'list', 'of', 'words']") 
+        
+        self.assertEqual(str(cls_data.Data(['aaa', 0.4, ['a','b','c']])), "['aaa', 0.4, ['a', 'b', 'c']]")
+
+    def test_05_csv(self):
+        with open('data.csv', "w") as f:
+            f.writelines(
+"""Name,Gender,tot2
+Frank,M,1866
+John,M,124
+Betty,F,67""")
+        d05 = cls_data.Data('data.csv')
+        self.assertEqual(str(d05), "[['Frank', 'M', '1866'], ['John', 'M', '124'], ['Betty', 'F', '67']]")
         
 if __name__ == '__main__':
     unittest.main()
