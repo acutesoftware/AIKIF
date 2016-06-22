db0d7d6df39a5e2373d7db168fa40eae87352e99
Duncan Murray <djmurray@acutesoftware.com.au>
2016-06-22 22:00:30 +0930

    example to generate events from various sources

---
+++ aikif/.z_prototype/event_aggregator.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# event_aggregator.py
+
+import os
+import sys
+import random
+
+root_fldr = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(1, root_fldr)
+root_path = root_fldr + os.sep + 'samples'
+
+import config
+import cls_log
+import core_data
+ 
+password_folder = config.fldrs['pers_credentials']
+log_folder = config.fldrs['log_folder'] 
+data_folder = config.fldrs['pers_data']
+#op_folder = os.path.join(config.fldrs['localPath'], '_PROD','data','core')
+op_folder = os.path.join(config.fldrs['localPath'], '_DEV','data','core')
+
+lg = cls_log.Log(log_folder)
+usr = 'djm'     # file extenstions for all data files    
+
+def main():
+    """
+    Generates selection of different events from various sources
+    
+    print('password_folder = ', password_folder)
+    print('log_folder      = ', log_folder)
+    print('data_folder     = ', data_folder)
+    print('op_folder       = ', op_folder)
+    """
+    if '_PROD' in op_folder:
+        correct = random.choice(['y', '1', ' ', '#'])
+        ans = input('Saving to PROD - press ' + correct + ' to continue')
+        if ans != correct:
+            lg.record_process('event_aggregator.py', 'abort process on startup')
+            sys.exit()
+            
+    lg.record_command('event_aggregator.py', 'generating events to op_folder=' + op_folder)        
+    add_manual_events()
+    
+
+def add_manual_events():
+    """
+    add events manually from code, and sample text files
+    """
+    hdr = ['date', 'category', 'details']
+    ev_man = core_data.CoreTable(op_folder, tpe='Events', user=usr, header=hdr)
+    op_file = ev_man.get_filename('Manual')
+    if os.path.exists(op_file):
+        os.remove(op_file)
+        
+    lg.record_process('event_aggregator.py', 'loading manual events')
+    ev_man.add(core_data.CoreDataWhen('Build Event Aggregator', ['2016-06-22', 'Dev', 'Started example to generate diary with AIKIF']))
+
+    ev_man.save('Manual')
+    #print(ev_man.get_filename('ManualTEST'))
+    #ev_man.generate_diary()    
+
+    
+    
+if __name__ == '__main__':
+    main()
+
