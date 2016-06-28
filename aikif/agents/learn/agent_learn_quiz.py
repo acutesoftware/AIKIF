7db31505194d5b0b9ba5fcf9517331bc36b28584
Duncan Murray <djmurray@acutesoftware.com.au>
2016-06-28 22:48:51 +0930

    sample agent stub for testing quiz answers

---
+++ aikif/agents/learn/agent_learn_quiz.py
@@ -0,0 +1,46 @@
+# agent_learn_quiz.py.py
+
+
+import os
+import random
+root_folder = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.sep + ".." + os.sep + ".." + os.sep + "..") 
+
+import aikif.agents.agent as agt
+
+
+class LearnAgentQuiz(agt.Agent):
+    """
+    sample agent that pretends to know information.
+    Use for trying out method of rating AI agents.
+    """
+    def __init__(self, name,  fldr, running, LOG_LEVEL):
+        agt.Agent.__init__(self, name,  fldr, running)
+        self.LOG_LEVEL = LOG_LEVEL
+
+        
+    def do_your_job(self):
+        print('Waiting for question...')
+ 
+    def show_status(self):
+        """
+        dumps the status of the agent
+        """
+        txt = 'Agent Status:\n'
+        print(txt)
+        return txt
+
+    def answer(self, question):
+        """
+        These are dummy answers to questions
+        """
+        words = [w.lower() for w in question.split(' ')]
+        if 'calc' in words:
+            return 'calculating maths result'
+        elif 'where' in words:
+            return "that's somewhere on Earth I think"
+        elif 'when' in words:
+            return "Last Wednesday"
+        elif 'why' in words:
+            return "Probably caused by lack of sunlight"
+        
+        
