f87b4235e029405f76f881e444acd33d3f3a5252
Duncan Murray <djmurray@acutesoftware.com.au>
2016-09-13 22:15:08 +0930

    program stub to work on aikif example on random info solving

---
+++ scripts/examples/random_info.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# random_info.py
+
+desc = """This project uses AIKIF to handle a random series of data collection
+tasks to solve an everyday problem.
+
+
+1. Request for solution
+Someome has a request to solve an issue
+- document details
+
+2. Identify Problem
+- map the reqest to a series of information that needs collecting
+
+3. Find Tools to solve the problem
+- work out what is needed for a solution
+
+4. Run the tools to find possible solutions
+- run the tools and keep all results
+
+
+5. Test results and pick most likely solution
+- for all the test results, find the most appropraite matching result
+
+
+"""
+
+
+print(desc)
