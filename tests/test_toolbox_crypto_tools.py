d02d64f902238cb75fc16afde57337338091bf3a
Duncan Murray <djmurray@acutesoftware.com.au>
2016-10-03 01:16:30 +1030

    problem with AES key length

--- tests/test_toolbox_crypto_tools.py
+++ tests/test_toolbox_crypto_tools.py
@@ -32,8 +32,8 @@ class TestTemplate(unittest.TestCase):
         wont work unless crypto installed
         """
         msg = 'blah blah'
-        secret = crypto_tools.encrypt_AES('key123', msg, 'ERTE66RTERTiv456')
-        result = crypto_tools.decrypt_AES('key123', secret, 'ERTE66RTERTiv456')
+        secret = crypto_tools.encrypt_AES('key123', msg, 'ERTE66TERTiv456')
+        result = crypto_tools.decrypt_AES('key123', secret, 'ERTE66TERTiv456')
         self.assertEqual(msg, result)
         print('original  = ' + msg)
         print('encrypted = ' + secret)
