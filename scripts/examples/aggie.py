30765c21a220840927e5d535ede7b5d9f7bba9c6
Duncan Murray <djmurray@acutesoftware.com.au>
2016-12-09 00:17:56 +1030

    initial commit of aggie.py - experiment

---
+++ scripts/examples/aggie.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# aggie.py
+
+import aikif.config as mod_cfg
+
+print('Hello, my name is Aggie.')
+print('base folder is ', mod_cfg.get_root_folder()[1])
