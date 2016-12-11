0d12d154879d78fbf1831bf41fca5c6e23594df8
Duncan Murray <djmurray@acutesoftware.com.au>
2016-12-11 23:38:55 +1030

    logging added to sample aggie script

--- scripts/examples/aggie.py
+++ scripts/examples/aggie.py
@@ -3,6 +3,12 @@
 # aggie.py
 
 import aikif.config as mod_cfg
+import aikif.cls_log as mod_log
 
-print('Hello, my name is Aggie.')
-print('base folder is ', mod_cfg.get_root_folder()[1])
+def main():
+    mod_log.record_source('aggie.py','Hello, my name is Aggie.')
+    mod_log.record_source('aggie.py','base folder is ', mod_cfg.get_root_folder()[1])
+    print('done')
+    
+main()    
+    
