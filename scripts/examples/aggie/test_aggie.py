f8f40262ab732b71dbbc57f50f78c16bf7778da7
Duncan Murray <djmurray@acutesoftware.com.au>
2016-12-21 00:09:02 +1030

    test for aggie with hard coded answers

--- scripts/examples/aggie/test_aggie.py
+++ scripts/examples/aggie/test_aggie.py
@@ -9,8 +9,12 @@ import os
 import aggie 
 
 class TestAggie(unittest.TestCase):
-    def test_01_import(self):
-        self.assertEqual(1,1)
+    def test_01_hard_coded_answers(self):
+        a = aggie.Aggie()
+        self.assertEqual(a.answer('when is my dentist appointment'),'next week')
+        self.assertEqual(a.answer('this is a contrived example'),'I dont'' know')
+        self.assertEqual(a.answer('what is the weather in Spain'),'sunny')
+        self.assertEqual(a.answer('where is the best picnic spot nearby'),'4km to the North')
 
 if __name__ == '__main__':
     unittest.main()
