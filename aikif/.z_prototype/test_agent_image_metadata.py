af69aa5836978d0e50371bfbf5d1f65b37152426
Duncan Murray <djmurray@gmail.com>
2017-01-29 16:38:40 +1030

    changing import path

--- aikif/.z_prototype/test_agent_image_metadata.py
+++ aikif/.z_prototype/test_agent_image_metadata.py
@@ -7,10 +7,12 @@ import unittest
 
 root_folder = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.sep + ".." ) 
 mod_path = root_folder + os.sep + 'aikif' + os.sep + 'agents' + os.sep + 'gather' 
-sys.path.append(mod_path)
 
+sys.path.append(mod_path)
 import agent_image_metadata as mod_img
-import aikif.lib.cls_filelist as fl 
+
+sys.path.append(root_folder + os.sep + 'aikif' + os.sep + 'lib')
+import cls_filelist as fl 
  
 """
     ip_file = cfg.fldrs['log_folder'] + os.sep + 'filelist' + os.sep + 'cur_files.txt'
