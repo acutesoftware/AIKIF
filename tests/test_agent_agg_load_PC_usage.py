06d6b744d93b260e8f429e69a0c2a6bee5e04ebe
Duncan Murray <djmurray@gmail.com>
2017-02-17 00:24:54 +1030

    test for load_PC_usage includes reading the mapping file

--- tests/test_agent_agg_load_PC_usage.py
+++ tests/test_agent_agg_load_PC_usage.py
@@ -11,21 +11,35 @@ sys.path.append(os.path.join(root_fldr, "agents", "aggregate"))
 import agent_pcusage as mod_usage
 
 diary_folder = os.getcwd() 
-op_file = os.getcwd() + os.sep + 'summary_diary.dat'
+ip_file = os.getcwd() + os.sep + 'diary_duncan.txt'
+op_file = os.getcwd() + os.sep + 'summary_diary.txt'
+
 
     
 class AgentAggLoadPCUsageTest(unittest.TestCase):
 
-    def test_01_process_all(self):
+    def test_01_instantiate(self):
         """
-        
+        load the class and read mapping file
         """
-        print('testing new version...')
-        #mod_usage.process_all(diary_folder, op_file)
-        
-        #self.assertEqual(os.path.exists(op_file), True)
+        p = mod_usage.PCUsageAgent('PCUsage', os.getcwd(), False, 3)
+        print(p)
+        self.assertEqual(str(p)[0:52], '\n--------- Agent Summary ---------\nName    : PCUsage')
         
 
+ 
+    def test_02_instant_map(self):
+        """
+        start the map_usage object
+        """
+        map_file = os.path.join(root_fldr, 'data', 'ref','map_pc_usage.csv')
+        print('loading map file ', map_file)
+        m = mod_usage.MapUsage(map_file)
+        print(m.maps)
+        self.assertEqual(len(m.maps), 36)
+        self.assertEqual(m.maps[1], ['Application', 'Right', ' - Message (HTML)', 'Outlook', 'Email', 'Work\n'])
+        self.assertEqual(m.maps[12], ['Application', 'Left', 'C:\\', 'Windows Explorer', 'File Mgt', 'Any\n'])
+        
  
 if __name__ == '__main__':
     unittest.main()
