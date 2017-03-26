330b69792ca89ef9d8802c435368da5b96c2f1e9
Duncan Murray <djmurray@gmail.com>
2017-03-26 21:03:39 +1030

    added doc string

--- aikif/__init__.py
+++ aikif/__init__.py
@@ -1,3 +1,15 @@
+'''
+    AIKIF
+    -----------
+    This is an information classification framework that maps structured or freeform data to a standard knowledge store.
+
+    Manages a dataset of your applications, the source data, parameters, runs and results and then uses your business rules to convert and store the information in a machine usable format.
+
+    Your AI software can link to AIKIF by setting up logging watch-points to define success / failure along with the range of input parameters. Goals and plans are defined by breaking them down to smaller tasks until the task can be run by a tool in the Toolbox.
+
+    A tool is any python wrapped function or application and is easily extensible.
+'''
+
 status='planning'
 author='djmurray@acutesoftware.com.au'
 
