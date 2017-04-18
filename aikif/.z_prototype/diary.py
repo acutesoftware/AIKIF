200385fa509d6e742802cfe6b89678aedb01d649
Duncan Murray <djmurray@gmail.com>
2017-04-18 23:02:42 +0930

    diary prototype uses correct file mapping module from local folder

--- aikif/.z_prototype/diary.py
+++ aikif/.z_prototype/diary.py
@@ -15,8 +15,11 @@ Example of using AIKIF to record diary events.
    external sources.
    
 """
-import aikif.cls_file_mapping as mod_filemap
-import aikif.core_data as c
+import sys
+sys.path.append('..')
+
+import cls_file_mapping as mod_filemap
+import core_data as c
 
 def main():
     """
@@ -24,15 +27,16 @@ def main():
     NLP splitting as it is better to use the function 
     via script (and later with web service)
     """
-    diary_file = mod_filemap.FileMap('.').get_full_filename('EVENT', 'INFO-PIM-DIARY')
+    dataSubjectAreas = ['log events', 'reminders', 'todo']
+    diary_file = mod_filemap.FileMap([],[]).get_full_filename('EVENT', 'INFO-PIM-DIARY')
     print('Diary file = ', diary_file)
-    today = '20150411'
+    today = '20170418'
     add_event(diary_file, today, 'Testing aikif.examples.diary.py')
     add_event(diary_file, today, 'random diary entry')
-    add_event(diary_file, '2015-05-17', 'deposit bank cheque for custom software', cat='tax')
-    add_event(diary_file, '2015-10-22', 'Book car in for major service', remindme='20151021')
+    add_event(diary_file, '2016-06-04', 'deposit bank cheque for custom software', cat='tax')
+    add_event(diary_file, '2013-11-12', 'Book car in for major service', remindme='20131111')
     #  add_event(diary_file, today, 'Remember to fix shelf', remindme='20150422')
-    
+
 def add_event(fname, dte, details, remindme='', cat='Diary'):
     """
     function to take a diary / calendar or reminder note and
@@ -40,10 +44,23 @@ def add_event(fname, dte, details, remindme='', cat='Diary'):
     """
     print("Adding event:", dte, remindme, details)
     
-    e = c.Event('Diary', [dte, cat, details])
+    e = c.CoreDataWhen('Diary', [dte, cat, details])
     
     
     with open(fname, "a") as f:
         f.write(e.format_csv())
+
+
+def parse_csv_file_to_events(fname, date_col_num, details_col_num):
+    """
+    read a CSV file and parse to diary events
+    """
+    with open(fname, 'r') as f:
+        for line in f:
+            print(line)
+            # add_event ...
     
+
+
+        
 main()
