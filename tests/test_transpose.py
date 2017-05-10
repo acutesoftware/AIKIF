eb81ebb333072804cc82dd90891e2e8f64b78910
Duncan Murray <djmurray@gmail.com>
2017-05-10 21:59:24 +0930

    initial test for transpose module

---
+++ tests/test_transpose.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# test_transpose.py
+
+import unittest
+import sys
+import os
+
+root_folder = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.sep + ".." ) 
+pth = root_folder + os.sep + 'aikif'
+
+sys.path.append(pth)
+import transpose
+
+test_fldr = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.sep + "test_results")
+
+test_list = [['col1','col2','col3'],['r1c1','r1c2','r1c3'],['r2c1','r2c2','r2c3'],['r3c1','r3c2','r3c3'],['r4c1','r4c2','r4c3']]
+
+class TransposeTest(unittest.TestCase):
+    
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+        self.obj_transpose = transpose.Transpose(test_list) 
+        
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+        self.obj_transpose = None
+
+    def test_01_instantiate(self):
+        print(self.obj_transpose)
+        self.assertEqual(len(self.obj_transpose.data) , 5) 
+        
+        
+        
+        
+ 
+if __name__ == '__main__':
+    unittest.main()
