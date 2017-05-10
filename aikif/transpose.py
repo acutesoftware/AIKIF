4be7e2e389bf79827df3e6786a146afcd5e018ab
Duncan Murray <djmurray@gmail.com>
2017-05-10 22:52:26 +0930

    added key value transform to transpose.py

--- aikif/transpose.py
+++ aikif/transpose.py
@@ -11,7 +11,38 @@ class Transpose(object):
     main transpose object
     """
     def __init__(self, data):
-        self.data = data
+        self.ip_data = data
+        self.op_data = []
     
     def __str__(self):
-        return str(self.data)
+        return str(self.op_data)
+        
+    def pivot(self):
+        """
+        transposes rows and columns
+        """
+        self.op_data = [list(i) for i in zip(*self.ip_data)]
+        
+    def key_value_pairs(self):
+        """
+        convert list to key value pairs
+        
+        This should also create unique id's to allow for any
+        dataset to be transposed, and then later manipulated
+        r1c1,r1c2,r1c3
+        r2c1,r2c2,r2c3
+        
+        should be converted to 
+        ID  COLNUM  VAL
+        r1c1, 
+        """
+        self.op_data = []
+        hdrs = self.ip_data[0]
+        for row in self.ip_data[1:]:
+            id_col = row[0]
+            for col_num, col in enumerate(row):
+                self.op_data.append([id_col, hdrs[col_num], col])
+        
+            
+        
+        
