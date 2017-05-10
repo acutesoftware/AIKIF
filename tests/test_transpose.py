06aa9f5a95a05751283607a456538c669b75cea1
Duncan Murray <djmurray@gmail.com>
2017-05-10 23:08:43 +0930

    test for key value transform

--- tests/test_transpose.py
+++ tests/test_transpose.py
@@ -28,11 +28,21 @@ class TransposeTest(unittest.TestCase):
 
     def test_01_instantiate(self):
         print(self.obj_transpose)
-        self.assertEqual(len(self.obj_transpose.data) , 5) 
+        self.assertEqual(len(self.obj_transpose.ip_data) , 5) 
         
+    def test_02_pivot(self):
+        #print(self.obj_transpose.ip_data)
         
+        self.assertEqual(self.obj_transpose.ip_data, [['col1', 'col2', 'col3'], ['r1c1', 'r1c2', 'r1c3'], ['r2c1', 'r2c2', 'r2c3'], ['r3c1', 'r3c2', 'r3c3'], ['r4c1', 'r4c2', 'r4c3']])
+        self.obj_transpose.pivot()
+        #print(self.obj_transpose.op_data)
+        self.assertEqual(self.obj_transpose.op_data, [['col1', 'r1c1', 'r2c1', 'r3c1', 'r4c1'], ['col2', 'r1c2', 'r2c2', 'r3c2', 'r4c2'], ['col3', 'r1c3', 'r2c3', 'r3c3', 'r4c3']])
         
+
+    def test_03_key_value_pairs(self):
+        self.obj_transpose.key_value_pairs()
+        #print(self.obj_transpose.op_data)
+        self.assertEqual(self.obj_transpose.op_data, [['r1c1', 'col1', 'r1c1'], ['r1c1', 'col2', 'r1c2'], ['r1c1', 'col3', 'r1c3'], ['r2c1', 'col1', 'r2c1'], ['r2c1', 'col2', 'r2c2'], ['r2c1', 'col3', 'r2c3'], ['r3c1', 'col1', 'r3c1'], ['r3c1', 'col2', 'r3c2'], ['r3c1', 'col3', 'r3c3'], ['r4c1', 'col1', 'r4c1'], ['r4c1', 'col2', 'r4c2'], ['r4c1', 'col3', 'r4c3']])
         
- 
 if __name__ == '__main__':
     unittest.main()
