105003f3884aea44176409e3eb48a7af2ba9d824
Duncan Murray <djmurray@gmail.com>
2017-05-13 13:36:10 +0930

    initial commit. standard mapping of base actions

---
+++ aikif/data/core/base_actions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# actions.py
+
+fun  = {    'name':'Fun',
+           'desc':'Have fun and enjoy life',
+           'modifier_key': 1,
+           'modifier_val':2
+       }
+
+job = {    'name':'Job',
+           'desc':'Work in a job',
+           'modifier_key': 0,
+           'modifier_val':100
+           }
+
+training = {'name':'Training',
+           'desc':'learn or improve a skill',
+           'modifier_key': 4,
+           'modifier_val':10
+           }
+           
+prison = {'name':'Prison',
+           'desc':'Go to Prison',
+           'modifier_key': 0,
+           'modifier_val':-100
+           }           
