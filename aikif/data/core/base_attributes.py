ed47927a6f7e8b07f15986c4ea801297c777dc38
Duncan Murray <djmurray@gmail.com>
2017-05-13 18:09:21 +0930

    initial commit for attributes

---
+++ aikif/data/core/base_attributes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# atrributes.py
+
+# base attributes
+intelligence = {'name':'Intelligence', 'base':100  }
+strength = {'name':'Strength', 'base':100, 'inc_per_yr': -0.2  }
+charisma = {'name':'Charisma', 'base':100  }
+constitution = {'name':'Constitution', 'base':100, 'inc_per_yr': -0.3  }
+wisdom = {'name':'Wisdom', 'base':100, 'inc_per_yr': 0.2 }
+
+
+# personality traits
+
+positive_traits = [
+'compassionate'
+'loving',
+'caring',
+'loyalty',
+'considerate',
+'determined',
+'kind',
+'faithfullness',
+'sincerity',
+'committment',
+'honesty',
+]
