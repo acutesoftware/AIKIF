56347e0b4012628001ac835fbe3b9b8482b6a056
Duncan Murray <djmurray@gmail.com>
2017-05-13 13:36:34 +0930

    initial commit. standard mapping of base emotions that modify results

---
+++ aikif/data/core/base_emotions.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# emotions.py
+
+sadness = {'name':'Sadness',
+           'desc':'You feel unhappy',
+           'modifier_key': 0,
+           'modifier_val':0
+           }
+           
