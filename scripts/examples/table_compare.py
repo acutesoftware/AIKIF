80d40a01755d1e86a89fdbf041aa00d2e7b53f69
Duncan Murray <djmurray@gmail.com>
2017-05-15 21:45:33 +0930

    example using dataTable to compare files

---
+++ scripts/examples/table_compare.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# table_compare.py
+
+import unittest
+import os
+import sys
+
+root_folder = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.sep + ".." + os.sep + ".." + os.sep + 'aikif') 
+sys.path.append(root_folder + os.sep + 'dataTools')
+
+import cls_datatable as cl
+
+
+with open('dummy_table1.csv', "w") as f:
+    f.writelines(
+"""TERM,GENDER,ID,tot1,tot2,tot3
+5300,F,00078,18,66,45
+7310,M,00078,16,465,64
+7310,F,00078,30,2,3
+7310,F ,00016,25,12,2
+5320,M,00016,31,0,66
+7310,F,00016,67,873,2""")
+
+with open('dummy_table2.csv', "w") as f:
+    f.writelines(
+"""TERM,Gender,ID,tot1,tot2
+5300,F,00078,18,66
+7310,M,00078,18,465
+7310,M,00078,10,12
+7310,F,00078,30,2
+7310,F,00016,25,12
+5300,M,00016,31,0 
+7310,F,00016,67,873""")
+
+old_table = 'dummy_table1.csv'
+new_table = 'dummy_table2.csv'
+
+
+def main():
+
+    if len(sys.argv) != 3:
+        print('usage:')
+        print('table_compare.py "TABLE1.CSV" "TABLE2.CSV":')
+        old_table = 'dummy_table1.csv'
+        new_table = 'dummy_table2.csv'
+    else:
+        old_table = sys.argv[1]
+        new_table = sys.argv[2]
+    
+
+    t_old = cl.DataTable(old_table, ',')
+    t_old.load_to_array()
+    t_old.describe_contents()
+    
+    t_new = cl.DataTable(new_table, ',')
+    t_new.load_to_array()
+    t_new.describe_contents()
+    
+    print('Comparing tables ' )
+
+    print(check_col_names(t_old, t_new))
+    print(check_rows(t_old, t_new))
+
+    
+    
+def check_col_names(t_old, t_new):
+    res = '\n -- Column Name check -- \n'
+
+    for col_num, c in enumerate(t_old.header):
+        if len(t_new.header) >= col_num + 1:
+            if c == t_new.header[col_num]:
+                res += c + ' ok\n'
+            else:
+                res += c + ' different name (now ' + t_new.header[col_num] + ')\n'
+        else:
+            res += c + ' is missing in new table\n'
+    return res
+    
+    
+def check_rows(t_old, t_new):
+    res = '\n -- Row Name check#1 -- \n'
+
+    for row_num, r in enumerate(t_old.arr):
+        if len(t_new.arr) >= row_num + 1:
+            if r == t_new.arr[row_num]:
+                res += str(row_num) + ' ok\n'
+            else:
+                res += str(r) + ' different values (now ' + str(t_new.arr[row_num]) + ')\n'
+        else:
+            res += r + ' is missing in new table\n'
+    return res
+    
+   
+
+if __name__ == '__main__':
+    main()    
+
