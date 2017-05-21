0f30ed9c8b73200f0b0cb534d65ce8884e7afa58
Duncan Murray <djmurray@gmail.com>
2017-05-21 23:31:03 +0930

    parsing data source into keywords

--- scripts/examples/random_info.py
+++ scripts/examples/random_info.py
@@ -40,27 +40,38 @@ from dataTools import cls_datatable as dt
 def main():
 
     # define the project
-    all_projects = prj.Projects()
-    all_projects.add_ontology('researcher')
     prj_random = prj.Project(name='Random Information Request', desc=desc, fldr=proj_fldr)
-    all_projects.add_project(prj_random)
-    print(all_projects)
-
+    
     # add sources of data
+    prj_random.add_source('Root Ontology', os.path.join(src_fldr, 'data','ref', 'ontology.yaml'))
+    prj_random.add_source('Toolboxes', os.path.join(src_fldr, 'data','ref', 'toolbox.csv'))
+    
     
     # parse the request for possible toolboxes to use based on local ontology
-    
+    keywords = []
+    for s in prj_random.data_sources:
+        with open(s[1], 'r') as f:
+            for line in f:
+                print(s[1], ' = ' , line)
+                for word in line:
+                    keywords.append(word)
     
     # a. split request into words
     # b. lookup local ontology of words to concepts
     # c. lookup toolboxes that match the concepts
-    
+    print(list(set(keywords)))
     # test the matching toolboxes on the source data
     # a. run the test on each toolboxes
     # b. compare test results and attempt to rank results
     
     
+    all_projects = prj.Projects()
+    all_projects.add_ontology('researcher')
+    all_projects.add_project(prj_random)
     
+    
+    print(prj_random)
+
 if __name__ == '__main__':
     main()
 
