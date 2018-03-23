a140d30b346c81dcd7cc10b7bfced1552ab94113
acutesoftware <djmurray@acutesoftware.com.au>
2018-03-23 23:05:01 +1030

    test_comms checks Channel and init CommsManager

--- tests/test_comms.py
+++ tests/test_comms.py
@@ -21,11 +21,21 @@ class CommsTest(unittest.TestCase):
         unittest.TestCase.tearDown(self)
 
 
-    def test_01_begin(self):
-        #self.comms = mod_bias.Bias(test_metadata)
-        mod_comms.TEST()
-        self.assertEqual(1,1)
 
+    def test_01_channel(self):
+        #self.comms = mod_bias.Bias(test_metadata)
+        ch = mod_comms.Channel('audio', 'F57gj3thddj')
+        #cm.add_channel(Channel('TCP', 'Jgfdedfsweewr54'), 'Jgfdedfsweewr54')
+        #print(ch)
+
+        self.assertTrue('audio : 0 incoming messages' in str(ch))
+
+    def test_02_comms_manager(self):
+        #self.comms = mod_bias.Bias(test_metadata)
+        cm = mod_comms.CommsManager()
+        self.assertTrue('---- CommsManager ----' in str(cm))
+        cm.add_channel(mod_comms.Channel('text', '12345'), '12345')
+        self.assertTrue('channel : text : 0 incoming messages' in str(cm))
 
 
 if __name__ == '__main__':
